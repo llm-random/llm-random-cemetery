aa8f5ab972e5db4327b052dc8a04c0efb621eebe
Simontwice <szymon.antoniak1@gmail.com>
2023-09-16 20:21:28 +0200

    Versioning code

--- research/conditional/moe_layers/continuous_moe.py
+++ research/conditional/moe_layers/continuous_moe.py
@@ -5,11 +5,9 @@ import itertools
 import einops
 import numpy as np
 import torch
-from plotly import express as px
 
 from lizrd.core import misc, nn
-from lizrd.support.logging import make_histogram
-from research.conditional.utils.misc_tools import stable_softmax_temperature, entropy
+from research.conditional.utils.misc_tools import stable_softmax_temperature
 from research.conditional.utils.layer_manager import LoggingLayer, measure_time
 
 
@@ -84,9 +82,16 @@ class ContinuousMoeBaseClass(LoggingLayer):
         return merge_weights, merge_weights
 
     def merge_map_emit(self, x, merge_weights, emit_weights):
-        self.merge_process_profiling(x, merge_weights, emit_weights)
+        # Reorder the dimensions so that the calculations are faster
+        input_order = "B S g d"
+        output_order = "g d (B S)"
+        sig = f"{input_order} -> {output_order}"
+        output_order = output_order.replace("(B S)", "S")
+        x = einops.rearrange(
+            x, sig
+        )
         x = misc.einsum(
-            "B S c d, B S e c, d e f -> B S e f",
+            f"{output_order}, B S e c, d e f -> B S e f",
             x,
             merge_weights,
             self.lin1,
@@ -129,140 +134,11 @@ class ContinuousMoeBaseClass(LoggingLayer):
             misc.get_init_weight((self.dm, self.n_experts), fan_in=self.dm)
         )
 
-    def merge_process_profiling(self, x, merge_weights, emit_weights):
-        # Reorder the dimensions so that the calculations are faster
-        input_order_1 = "B S g d"
-        input_order_2 = "B s e c"
-        input_order_3 = "d e f"
-        outpt_order = "B s e f"
-        copied_x = x.clone()
-
-        with measure_time(self, f"mp_default"):
-            perm_x = misc.einsum(
-                f"{input_order_1}, {input_order_2}, {input_order_3} -> {outpt_order}",
-                copied_x,
-                merge_weights,
-                self.lin1,
-                use_opt_einsum=self.use_opt_einsum,
-            )
-            del perm_x
-        inp1s = itertools.permutations(input_order_1.split())
-        inp2s = itertools.permutations(input_order_2.split())
-        inp3s = itertools.permutations(input_order_3.split())
-        outps = itertools.permutations(outpt_order.split())
-        for inp1_ in inp1s:
-            for inp2_ in inp2s:
-                for inp3_ in inp3s:
-                    for outp_ in outps:
-                        inp1 = " ".join(inp1_)
-                        inp2 = " ".join(inp2_)
-                        inp3 = " ".join(inp3_)
-                        outp = " ".join(outp_)
-
-                        inp1_clean = "_".join(inp1_)
-                        inp2_clean = "_".join(inp2_)
-                        inp3_clean = "_".join(inp3_)
-                        outp_clean = "_".join(outp_)
-
-                        perm_2 = einops.rearrange(
-                            merge_weights, f"{input_order_2} -> {inp2}"
-                        ).contiguous()
-                        perm_3 = einops.rearrange(
-                            self.lin1, f"{input_order_3} -> {inp3}"
-                        ).contiguous()
-
-                        logging_name = f"merge_and_process{inp1_clean}{inp2_clean}{inp3_clean}{outp_clean}"
-                        # with measure_time(self, logging_name):
-                        perm_1 = einops.rearrange(
-                            copied_x, f"{input_order_1} -> {inp1}"
-                        ).contiguous()
-                        with measure_time(self, logging_name):
-                            perm_1 = misc.einsum(
-                                f"{inp1},{inp2},{inp3}->{outp}",
-                                perm_1,
-                                perm_2,
-                                perm_3,
-                                use_opt_einsum=self.use_opt_einsum,
-                            )
-                        del perm_1, perm_2, perm_3
-
     def log_light(self):
         return {}
 
     def log_heavy(self):
         log = {}
-        if self.group_size == 1:
-            return log
-        merge_weights = torch.flatten(
-            self.logging_cache["merge_weights"], start_dim=0, end_dim=-2
-        )
-        merge_logits = torch.flatten(
-            self.logging_cache["merge_logits"], start_dim=0, end_dim=-2
-        )
-        sample_weight_distros = merge_weights[:5]
-        sample_logits_distros = merge_logits[:5]
-
-        for i, sample in enumerate(sample_weight_distros):
-            sample = sample.sort(descending=True).values
-            sample = sample.tolist()
-            fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
-            log[f"merge_weights/sample_{i}"] = fig
-
-        for i, sample in enumerate(sample_logits_distros):
-            sample = sample.sort(descending=True).values
-            sample = sample.tolist()
-            fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
-            log[f"merge_logits/sample_{i}"] = fig
-
-        ent = entropy(merge_weights)
-        max_entropy = np.log(self.group_size)
-        normalised_ent = ent / max_entropy
-        log["merge_weights/normalised_entropy"] = make_histogram(
-            normalised_ent, title="merge logits entropy (normalised to [0,1])"
-        )
-
-        # make bar plot of values cached in forward with measure_time
-        instr_names = list(self.logging_cache["time"].keys())
-        instr_times = list(self.logging_cache["time"].values())
-        merge_maps = []
-        merge_map_names = []
-        for instr_name, instr_time in zip(instr_names, instr_times):
-            if "merge_and_process" in instr_name:
-                merge_maps.append(instr_time)
-                merge_map_names.append(instr_name)
-
-        # print(merge_maps, merge_map_names)
-        merge_best_id = np.argmin(merge_maps)
-        merge_best = merge_maps[merge_best_id]
-        merge_best_signature = merge_map_names[merge_best_id]
-        merge_worst_id = np.argmax(merge_maps)
-        merge_worst = merge_maps[merge_worst_id]
-        merge_worst_signature = merge_map_names[merge_worst_id]
-
-        log[f"merge_and_process/best_{merge_best_signature}"] = merge_best
-        log[f"merge_and_process/worst_{merge_worst_signature}"] = merge_worst
-        default_time = self.logging_cache["time"]["mp_default"]
-        log[f"merge_and_process/default_time"] = default_time
-        log[f"merge_and_process/best_to_default_ratio"] = merge_best / default_time
-        log[f"merge_and_process/best_to_worst_ratio"] = merge_best / merge_worst
-
-        times_fig = px.bar(x=instr_names, y=instr_times)
-        log["forward_pass_times"] = times_fig
-
-        # log process_and_emit time
-        # log["process_and_emit_time"] = self.logging_cache["time"][
-        #     "process_and_emit_B_S_g_d"
-        # ]
-        # log["best_merge_to_emit_ratio"] = (
-        #     merge_best / self.logging_cache["time"]["process_and_emit_B_S_g_d"]
-        # )
-        # log["worst_merge_to_emit_ratio"] = (
-        #     merge_worst / self.logging_cache["time"]["process_and_emit_B_S_g_d"]
-        # )
-
-        # times_fig = px.bar(x=instr_names, y=instr_times)
-        # log["forward_pass_times"] = times_fig
-
         return log
 
 
