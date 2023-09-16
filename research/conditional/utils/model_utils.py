e813d2119c32c62cd11ae6195b0944257c042fc7
Simontwice <szymon.antoniak1@gmail.com>
2023-09-16 23:40:41 +0200

    Versioning code

--- research/conditional/utils/model_utils.py
+++ research/conditional/utils/model_utils.py
@@ -9,17 +9,17 @@ from lizrd.core.llm import Parallel
 from research.conditional.moe_layers.cont_moe_designs.common_weighted_parameter_matrices import (
     ContinuousMoECommonWeightedParameters,
 )
-from research.conditional.moe_layers.cont_moe_designs.entropy_loss import (
-    ContinuousMoEEntropyLoss,
-)
+# from research.conditional.moe_layers.cont_moe_designs.entropy_loss import (
+#     ContinuousMoEEntropyLoss,
+# )
 from research.conditional.moe_layers.cont_moe_designs.random_grouping import (
     ContinuousMoERandomGroups,
 )
 from research.conditional.moe_layers.cont_moe_designs.learn_temp_and_common_base import (
     ContinuousMoEFinal,
 )
-from research.conditional.moe_layers.cont_moe_designs.learnable_temperature import (
-    ContinuousMoEAdaTemp,
+from research.conditional.moe_layers.cont_moe_designs.learnable_temperature_positive import (
+    ContinuousMoEAdaTempPositive,
 )
 from research.conditional.moe_layers.cont_moe_designs.add_layernorms import (
     ContinuousMoELayernorm,
@@ -333,7 +333,7 @@ def get_ff_layer(args):
     elif args.ff_mode == "cont_moe_nosoft":
         return_fn = lambda: ContinuousMoENosoftmax(**default_contmoe_args)
     elif args.ff_mode == "cont_moe_adatemp":
-        return_fn = lambda: ContinuousMoEAdaTemp(
+        return_fn = lambda: ContinuousMoEAdaTempPositive(
             **default_contmoe_args,
             separate_temp_for_experts=args.separate_temp_for_experts,
             separate_temp_for_emit_merge=args.separate_temp_for_emit_merge,
@@ -357,8 +357,8 @@ def get_ff_layer(args):
         return_fn = lambda: ContinuousMoESeparateWeightedParameters(
             **default_contmoe_args
         )
-    elif args.ff_mode == "cont_moe_entropy_loss":
-        return_fn = lambda: ContinuousMoEEntropyLoss(**default_contmoe_args)
+    # elif args.ff_mode == "cont_moe_entropy_loss":
+    #     return_fn = lambda: ContinuousMoEEntropyLoss(**default_contmoe_args)
     elif args.ff_mode == "cont_moe_pass_through_temp":
         return_fn = lambda: ContinuousMoEPassThroughTemp(**default_contmoe_args)
     elif args.ff_mode == "expert_choice":
