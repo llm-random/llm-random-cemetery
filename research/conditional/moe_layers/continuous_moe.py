5c3f945d430736308a495cec85765208cec6ee7c
Simontwice <szymon.antoniak1@gmail.com>
2023-09-27 13:12:37 +0200

    Versioning code

--- research/conditional/moe_layers/continuous_moe.py
+++ research/conditional/moe_layers/continuous_moe.py
@@ -50,10 +50,14 @@ class ContinuousMoeBaseClass(LoggingLayer):
         self.init_parameters()
 
     def forward(self, x):
-        x = self.reshape_into_token_groups(x)
-        merge_weights, emit_weights = self.get_merge_and_emit_weights(x)
-        x = self.separate_map_emit(x, merge_weights, emit_weights)
-        x = self.reshape_into_original(x)
+        with measure_time(self, "reshape_into_token_groups"):
+            x = self.reshape_into_token_groups(x)
+        with measure_time(self, "get_merge_and_emit_weights"):
+            merge_weights, emit_weights = self.get_merge_and_emit_weights(x)
+        with measure_time(self, "merge_map_emit"):
+            x = self.merge_map_emit(x, merge_weights, None)
+        with measure_time(self, "reshape_into_original"):
+            x = self.reshape_into_original(x)
         return x
 
     def reshape_into_token_groups(self, x):
@@ -74,50 +78,65 @@ class ContinuousMoeBaseClass(LoggingLayer):
 
     def get_merge_and_emit_weights(self, x):
         merge_logits = misc.einsum("B S g d, d e -> B S e g", x, self.controller)
-        self.update_cache_for_logging("merge_logits", merge_logits)
+        # self.update_cache_for_logging("merge_logits", merge_logits)
         merge_weights = stable_softmax_temperature(merge_logits, self.temperature)
-        self.update_cache_for_logging("merge_weights", merge_weights)
+        # self.update_cache_for_logging("merge_weights", merge_weights)
         return merge_weights, merge_weights
 
     def merge_map_emit(self, x, merge_weights, emit_weights):
-        with measure_time(self, "merge_process"):
-            x = misc.einsum(
-                "B S c d, B S e c, d e f -> B S e f",
-                x,
-                merge_weights,
-                self.lin1,
-                use_opt_einsum=self.use_opt_einsum,
-            )
-        with measure_time(self, "relu"):
-            x = torch.relu_(x)
-        with measure_time(self, "emit_process"):
-            x = misc.einsum(
-                "B S e f, d e f, B S e c -> B S c d",
-                x,
-                self.lin2,
-                emit_weights,
-                use_opt_einsum=self.use_opt_einsum,
-            )
-        return x
-
-    def separate_map_emit(self, x, merge_weights, emit_weights):
+        # with measure_time(self, "merge_process"):
+        #     x = misc.einsum(
+        #         "B S c d, B S e c, d e f -> B S e f",
+        #         x,
+        #         merge_weights,
+        #         self.lin1,
+        #         use_opt_einsum=self.use_opt_einsum,
+        #     )
+        # with measure_time(self, "relu"):
+        #     x = torch.relu_(x)
+        # with measure_time(self, "emit_process"):
+        #     x = misc.einsum(
+        #         "B S e f, d e f, B S e c -> B S c d",
+        #         x,
+        #         self.lin2,
+        #         emit_weights,
+        #         use_opt_einsum=self.use_opt_einsum,
+        #     )
         x = misc.einsum("B S c d, B S e c -> B S e d", x, merge_weights)
         S = x.shape[1]
         x = einops.rearrange(x, "a b c d -> c (a b) d")
-        with profile(
-            activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
-            record_shapes=True,
-            profile_memory=True,
-            use_cuda=True,
-        ) as prof:
-            with record_function("cont_moe"):
-                x = torch.bmm(x, self.lin1)
-                x = torch.relu_(x)
-                x = torch.bmm(x, self.lin2)
-        print(prof.key_averages().table(sort_by="cuda_time_total"))
-        with open("profiling.txt", "w") as f:
-            f.write(prof.key_averages().table())
-            print(f"the full path to the file is {os.path.abspath(f.name)}")
+        x = torch.bmm(x, self.lin1)
+        x = torch.relu_(x)
+        x = torch.bmm(x, self.lin2)
+        x = einops.rearrange(x, "c (a b) d -> a b c d", b=S)
+        x = misc.einsum("B S e d, B S e c -> B S c d", x, merge_weights)
+        return x
+
+    def separate_map_emit(self, x, merge_weights, emit_weights):
+        print(
+            f" for {self.n_experts} experts, the shape of x is {x.shape}, and merge are {merge_weights.shape}"
+        )
+        x = misc.einsum("B S c d, B S e c -> B S e d", x, merge_weights)
+        S = x.shape[1]
+        x = einops.rearrange(x, "a b c d -> c (a b) d")
+        # with profile(
+        #     activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+        #     record_shapes=True,
+        #     profile_memory=True,
+        #     use_cuda=True,
+        # ) as prof:
+        #     with record_function("cont_moe"):
+        print(
+            f" for {self.n_experts} experts, the shape of x is {x.shape} and lin1, lin2 are {self.lin1.shape}, {self.lin2.shape}"
+        )
+        x = torch.bmm(x, self.lin1)
+        print(f" for {self.n_experts} experts, post lin1 the shape of x is {x.shape}")
+        x = torch.relu_(x)
+        x = torch.bmm(x, self.lin2)
+        # print(prof.key_averages().table(sort_by="cuda_time_total"))
+        # with open("profiling.txt", "w") as f:
+        #     f.write(prof.key_averages().table())
+        #     print(f"the full path to the file is {os.path.abspath(f.name)}")
         x = einops.rearrange(x, "c (a b) d -> a b c d", b=S)
         x = misc.einsum("B S e d, B S e c -> B S c d", x, emit_weights)
         return x
@@ -134,22 +153,17 @@ class ContinuousMoeBaseClass(LoggingLayer):
     def init_parameters(self):
         # lin1 is parameter, one dimension for experts of size dmodel to dff/n_experts
 
-        self.lin2 = nn.Parameter(
-            misc.get_init_weight(
-                (self.n_experts, self.expert_size, self.dm), fan_in=self.expert_size
-            )
-        )
-        self.lin3 = nn.Parameter(
-            misc.get_init_weight(
-                (self.n_experts, self.expert_size, self.dm), fan_in=self.expert_size
-            )
-        )
         self.lin1 = nn.Parameter(
             misc.get_init_weight(
                 (self.n_experts, self.dm, self.expert_size), fan_in=self.dm
             )
         )
 
+        self.lin2 = nn.Parameter(
+            misc.get_init_weight(
+                (self.n_experts, self.expert_size, self.dm), fan_in=self.expert_size
+            )
+        )
         # controller: send a token of size dmodel to n_experts scalars
         self.controller = nn.Parameter(
             misc.get_init_weight((self.dm, self.n_experts), fan_in=self.dm)
@@ -162,36 +176,203 @@ class ContinuousMoeBaseClass(LoggingLayer):
         log = {}
         if self.group_size == 1:
             return log
-        merge_weights = torch.flatten(
-            self.logging_cache["merge_weights"], start_dim=0, end_dim=-2
-        )
-        merge_logits = torch.flatten(
-            self.logging_cache["merge_logits"], start_dim=0, end_dim=-2
-        )
-        sample_weight_distros = merge_weights[:5]
-        sample_logits_distros = merge_logits[:5]
+        # merge_weights = torch.flatten(
+        #     self.logging_cache["merge_weights"], start_dim=0, end_dim=-2
+        # )
+        # merge_logits = torch.flatten(
+        #     self.logging_cache["merge_logits"], start_dim=0, end_dim=-2
+        # )
+        # sample_weight_distros = merge_weights[:5]
+        # sample_logits_distros = merge_logits[:5]
+        #
+        # for i, sample in enumerate(sample_weight_distros):
+        #     sample = sample.sort(descending=True).values
+        #     sample = sample.tolist()
+        #     fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
+        #     log[f"merge_weights/sample_{i}"] = fig
+        #
+        # for i, sample in enumerate(sample_logits_distros):
+        #     sample = sample.sort(descending=True).values
+        #     sample = sample.tolist()
+        #     fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
+        #     log[f"merge_logits/sample_{i}"] = fig
+        #
+        # ent = entropy(merge_weights)
+        # max_entropy = np.log(self.group_size)
+        # normalised_ent = ent / max_entropy
+        # log["merge_weights/normalised_entropy"] = make_histogram(
+        #     normalised_ent, title="merge logits entropy (normalised to [0,1])"
+        # )
 
-        for i, sample in enumerate(sample_weight_distros):
-            sample = sample.sort(descending=True).values
-            sample = sample.tolist()
-            fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
-            log[f"merge_weights/sample_{i}"] = fig
-
-        for i, sample in enumerate(sample_logits_distros):
-            sample = sample.sort(descending=True).values
-            sample = sample.tolist()
-            fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
-            log[f"merge_logits/sample_{i}"] = fig
-
-        ent = entropy(merge_weights)
-        max_entropy = np.log(self.group_size)
-        normalised_ent = ent / max_entropy
-        log["merge_weights/normalised_entropy"] = make_histogram(
-            normalised_ent, title="merge logits entropy (normalised to [0,1])"
-        )
+        if "time" not in self.logging_cache:
+            return log
+        instr_names = list(self.logging_cache["time"].keys())
+        instr_times = list(self.logging_cache["time"].values())
+        times_fig = px.bar(x=instr_names, y=instr_times)
+        log["time"] = times_fig
 
         return log
 
 
 class ContinuousMoE(ContinuousMoeBaseClass):
     pass
+
+
+class SpeedtestContMoE(nn.Module):
+    def __init__(
+        self,
+        dm: int,
+        dff: int,
+        n_experts: int,
+        group_size: int,
+        sparsity_dim: int,
+        temperature: float,
+        expert_size: Union[int, None],
+        use_opt_einsum: bool = False,
+        flop_matched: bool = False,
+    ):
+        super().__init__()
+        self.dm = dm
+        self.dff = dff
+        self.n_experts = n_experts
+        self.group_size = group_size
+        self.sparsity_dim = sparsity_dim
+        self.temperature = temperature
+        self.expert_size = expert_size
+        self.use_opt_einsum = use_opt_einsum
+        self.flop_matched = flop_matched
+        if self.flop_matched:
+            assert (
+                self.dff == 4 * self.dm
+            ), f"dff = {self.dff} is not equal to 4*dm = {4*self.dm} as in vanilla transformer"
+            self.dff *= self.group_size
+        if self.expert_size is None:
+            assert (
+                self.dff % self.n_experts == 0
+            ), f"dff = {self.dff} is not divisible by n_experts = {self.n_experts}"
+            print(
+                f"expert_size is None, setting it to dff // n_experts = {self.dff // self.n_experts}"
+            )
+            self.expert_size = self.dff // self.n_experts
+
+        self.lin1 = nn.Parameter(
+            misc.get_init_weight(
+                (
+                    self.n_experts,
+                    self.dm,
+                    self.expert_size,
+                ),
+                fan_in=self.dm,
+            )
+        )
+
+        self.lin2 = nn.Parameter(
+            misc.get_init_weight(
+                (self.n_experts, self.expert_size, self.dm), fan_in=self.expert_size
+            )
+        )
+
+        # print(
+        #     f"n experts: {self.n_experts}, lin1 shape: {self.lin1.shape}, lin2 shape: {self.lin2.shape}"
+        # )
+        # # controller: send a token of size dmodel to n_experts scalars
+        # self.controller = nn.Parameter(
+        #     misc.get_init_weight((self.dm, self.n_experts), fan_in=self.dm)
+        # )
+
+    def forward1(self, x, w=None, postmerge=None):
+        # merged version
+        x = misc.einsum(
+            "B S c d, B S e c, e d f -> B S e f",
+            x,
+            w,
+            self.lin1,
+        )
+
+    def forward2(self, x, w=None, postmerge=None):
+        # this alone takes linearly more time with more experts, which makes perfect sense
+        x = misc.einsum("B S c d, B S e c -> B S e d", x, w)
+        print(f" for {self.n_experts} experts, the shape of x post merge is {x.shape}")
+
+    def forward3(self, x, w=None, postmerge=None):
+        # this takes disproportionately more time with more experts, which is weird
+        x = misc.einsum("B S c d, B S e c -> B S e d", x, w)
+        x = einops.rearrange(x, "a b c d -> c (a b) d")
+        x = torch.bmm(x, self.lin1)
+        x = torch.relu_(x)
+        x = torch.bmm(x, self.lin2)
+        return x
+
+    def forward4(self, x, w=None, postmerge=None):
+        # this takes disproportionately more time with more experts, which is weird
+        x = misc.einsum("B S c d, B S e c -> B S e d", x, w)
+        x = einops.rearrange(x, "a b c d -> c (a b) d")
+        x = torch.bmm(x, self.lin1)
+        return x
+
+    def forward5(self, x, w=None, postmerge=None):
+        # this is just the bmm, and it is constant time, as expected
+        x = torch.bmm(x, self.lin1)
+        return x
+
+    def forward6(self, x, w=None, postmerge=None):
+        # this is the whole bmm relu bmm, and it is constant time, as expected
+        x = torch.bmm(x, self.lin1)
+        x = torch.relu_(x)
+        x = torch.bmm(x, self.lin2)
+        return x
+
+    def forward7(
+        self,
+        x,
+        w,
+        postmerge,
+        postrearrange,
+        postlin1,
+        postlin2,
+        postrearrange2,
+        postemit,
+    ):
+        # the original code - also takes disproportionately more time with more experts, which is weird
+        x = misc.einsum("B S c d, B S e c -> B S e d", x, w)
+        # print(f" for {self.n_experts} experts, the shape of x post merge is {x.shape}")
+
+        S = x.shape[1]
+        x = einops.rearrange(x, "a b c d -> c (a b) d")
+        # print(f" for {self.n_experts} experts, the shape of x post rearrange is {x.shape}")
+        x = torch.bmm(x, self.lin1)
+        # print(f" for {self.n_experts} experts, the shape of x post lin1 is {x.shape}")
+        x = torch.relu_(x)
+        x = torch.bmm(x, self.lin2)
+        # print(f" for {self.n_experts} experts, the shape of x post lin2 is {x.shape}")
+        x = einops.rearrange(x, "c (a b) d -> a b c d", b=S)
+        # print(f" for {self.n_experts} experts, the shape of x post rearrange2 is {x.shape}")
+        x = misc.einsum("B S e d, B S e c -> B S c d", x, w)
+        # print(f" for {self.n_experts} experts, the shape of x post emit is {x.shape}")
+        return x
+
+    def forward8(
+        self,
+        x,
+        w,
+        postmerge,
+        postrearrange,
+        postlin1,
+        postlin2,
+        postrearrange2,
+        postemit,
+    ):
+        x = misc.einsum(
+            "B S c d, B S e c, e d f -> B S e f",
+            x,
+            w,
+            self.lin1,
+        )
+        x = torch.relu_(x)
+        x = misc.einsum(
+            "B S e f, e f d, B S e c -> B S c d",
+            x,
+            self.lin2,
+            w,
+        )
+        return x
