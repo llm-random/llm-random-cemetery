d181b2bc9e843a5d430e9991fc270e58a2f857f5
Kuba Krajewski <gim.jakubk@gmail.com>
2023-10-20 08:50:42 +0200

    Versioning code

--- research/conditional/utils/model_utils.py
+++ research/conditional/utils/model_utils.py
@@ -67,7 +67,7 @@ def chungized_llm_loss(
 
     def make_custom_forward():
         def custom_forward(*inputs):
-            output = model.head(inputs[0])
+            output = model.module.head(inputs[0])
             with torch.autocast(device_type="cuda", enabled=False, dtype=torch.float16):
                 gt = inputs[1]
                 mask = inputs[2]
@@ -91,7 +91,7 @@ def chungized_llm_loss(
     with torch.autocast(
         device_type="cuda", enabled=mixed_precision, dtype=torch.float16
     ):
-        encoder_output = model.encoder(input_tokens)
+        encoder_output = model.module.encoder(input_tokens)
         chunged_inputs = torch.chunk(encoder_output, n_chungs, dim=0)
         chunged_non_masked_inputs = torch.chunk(gt_tokens, n_chungs, dim=0)
         chunged_non_masked_masks = torch.chunk(mask, n_chungs, dim=0)
