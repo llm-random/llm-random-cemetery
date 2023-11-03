5ec99630629e98682e98cabee9f65e25a494aa83
Michał Krutul <michal.krutul@gmail.com>
2023-11-03 23:02:42 +0100

    Versioning code

--- lizrd/support/misc.py
+++ lizrd/support/misc.py
@@ -84,3 +84,14 @@ def create_zipped_model_fits_params(args):
     params = args.model_fits_params.split(",")
     values = [str(getattr(args, param)) for param in params]
     return list(zip(params, values))
+
+def merge_dicts(a: dict, b: dict, path=[]):
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge_dicts(a[key], b[key], path + [str(key)])
+            elif a[key] != b[key]:
+                raise Exception('Conflict at ' + '.'.join(path + [str(key)]))
+        else:
+            a[key] = b[key]
+    return a
