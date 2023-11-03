60712b1695585e4e0e4495db6b3369d3534eae87
Michał Krutul <michal.krutul@gmail.com>
2023-11-03 23:21:07 +0100

    Versioning code

--- research/conditional/utils/check_model_fits.py
+++ research/conditional/utils/check_model_fits.py
@@ -1,9 +1,9 @@
 import json
 
 
-def record_gpu_ram_availability(filename, params, value):
+def record_gpu_ram_availability(filename: str, params: [str], value: bool):
     """
-    This function is used to mark whether a model fits a certain condition in a json file.
+    This function is used to records whether a model with given params fits in gpu.
     """
 
     if filename is None or params is None:
@@ -12,26 +12,20 @@ def record_gpu_ram_availability(filename, params, value):
     try:
         with open(filename, "r") as file:
             data = json.load(file)
-            print(f"Found file with data: {data}")
     except (FileNotFoundError, json.JSONDecodeError):
         data = {}
-        print(f"Did not find file with data")
+        print(f"File not found or could not decode it, creating new dict.")
 
     current = data
-    for param_name, param_value in params[:-1]:
-        if param_name not in current:
-            current[param_name] = {}
-        current = current[param_name]
-        if param_value not in current:
-            current[param_value] = {}
-        current = current[param_value]
-    last_param_name, last_param_value = params[-1]
-
-    if last_param_name not in current:
-        current[last_param_name] = {}
-    current = current[last_param_name]
-
-    current[last_param_value] = value
+    for param in params[:-1]:
+        if param not in current:
+            current[param] = {}
+        current = current[param]
+    
+    last_param = params[-1]
+    if last_param not in current:
+        current[last_param] = {}
+    current[last_param] = value
 
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
