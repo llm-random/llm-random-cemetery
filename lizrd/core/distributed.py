3285128dd90d28e1cede044cf7113abea6dc2bd3
Kuba Krajewski <gim.jakubk@gmail.com>
2023-11-10 15:09:41 +0100

    Versioning code

--- lizrd/core/distributed.py
+++ lizrd/core/distributed.py
@@ -1,13 +1,23 @@
 from typing import Optional
+from functools import partial
 
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp import MixedPrecision, CPUOffload
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.nn as nn
 import torch
+from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
 
 from lizrd.core.misc import Noop
 
+def custom_auto_wrap_policy(
+     module: nn.Module,
+     recurse: bool,
+     nonwrapped_numel: int,
+     # Additional custom arguments
+     min_num_params: int = int(1e8),
+ ) -> bool:
+     return nonwrapped_numel >= min_num_params
 
 def wrap_in_fsdp(
     module: nn.Module,
@@ -28,6 +38,7 @@ def wrap_in_fsdp(
                 cast_forward_inputs=cast_inputs,
             ),
             cpu_offload=CPUOffload(offload_params=offload_params),
+            # auto_wrap_policy=partial(custom_auto_wrap_policy, min_num_params=int(1e5))
         )
 
     if output_cast_dtype is not None:
