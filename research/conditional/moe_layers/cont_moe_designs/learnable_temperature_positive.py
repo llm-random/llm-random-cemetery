75c8a1a98b8ab2cdc59d45daf27a83f6d855d510
Simontwice <szymon.antoniak1@gmail.com>
2023-11-28 16:07:27 +0100

    Versioning code

--- research/conditional/moe_layers/cont_moe_designs/learnable_temperature_positive.py
+++ research/conditional/moe_layers/cont_moe_designs/learnable_temperature_positive.py
@@ -16,6 +16,19 @@ class ContinuousMoEAdaTempPositive(ContinuousMoEAdaTemp):
     """
 
     def get_temperature(self):
+        if self.logging_switch:
+            self.update_cache_for_logging("temperature_merge", (self.temperature_merge -1.0).data.clone().detach())
+            self.update_cache_for_logging("temperature_emit", (self.temperature_emit -1.0).data.clone().detach())
         return torch.exp(self.temperature_merge - 1.0), torch.exp(
             self.temperature_emit - 1.0
         )
+
+    def log_heavy(self):
+        log = super().log_heavy()
+        log[
+            "log_merge_temperature"
+        ] = self.logging_cache["temperature_merge"].flatten().tolist()
+        log[
+            "log_emit_temperature"
+        ] = self.logging_cache["temperature_emit"].flatten().tolist()
+        return log
