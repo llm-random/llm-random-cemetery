33b62168562be8781e4f3a1ca2d8f512bdc8e432
Simontwice <szymon.antoniak1@gmail.com>
2023-11-28 14:44:55 +0100

    Versioning code

--- research/conditional/utils/argparse.py
+++ research/conditional/utils/argparse.py
@@ -145,6 +145,7 @@ def introduce_parser_arguments(
     # paremeters for specific experiments
 
     ## used by MoE (common)
+    parser.add_argument("--general_ff_layer_config", type=str, default=None)
     parser.add_argument(
         "--eval_dynamic_groupsize",
         action="store_true",
@@ -167,6 +168,7 @@ def introduce_parser_arguments(
 
     parser.add_argument("--eval_discrete_mot", action="store_true")
     parser.add_argument("--emit_softmax_over_experts", action="store_true")
+    parser.add_argument("--use_layer_norm_for_update", action="store_true")
     parser.add_argument("--steps_until_start_temperature_learn", type=int, default=0)
     parser.add_argument("--n_experts", type=int)
     parser.add_argument("--group_size", type=int)
