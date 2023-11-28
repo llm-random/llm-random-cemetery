f04f6b75f6c377cc0dac9add8513592ea621fd0b
Simontwice <szymon.antoniak1@gmail.com>
2023-11-28 14:46:58 +0100

    Versioning code

--- research/conditional/moe_layers/cont_moe_designs/learnable_temperature.py
+++ research/conditional/moe_layers/cont_moe_designs/learnable_temperature.py
@@ -17,6 +17,9 @@ class ContinuousMoEAdaTemp(ContinuousMoeBaseClass):
     share_by_experts: bool = True
     share_by_emit_merge: bool = True
 
+    def get_temperature(self):
+        return self.temperature_merge, self.temperature_emit
+
     def init_additional_parameters(self):
         if self.share_by_experts:
             if self.share_by_emit_merge:
@@ -35,6 +38,8 @@ class ContinuousMoEAdaTemp(ContinuousMoeBaseClass):
 
     def log_heavy(self):
         log = super().log_heavy()
+        print("temperature_merge", self.temperature_merge.cpu())
+        print("temperature_emit", self.temperature_emit.cpu())
         log[
             "merge_weights/merge_temperature"
         ] = self.temperature_merge.data.flatten().tolist()
