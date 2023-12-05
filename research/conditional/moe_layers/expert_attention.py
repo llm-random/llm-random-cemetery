cb01fa59be8afc1f79c4757d3741b3014d68cfd6
Krystian-Krol <krystian.kytek@gmail.com>
2023-12-05 11:09:04 +0100

    Versioning code

--- research/conditional/moe_layers/expert_attention.py
+++ research/conditional/moe_layers/expert_attention.py
@@ -57,7 +57,9 @@ class ExpertChoiceAttention(LoggingLayer):
         self.output_projection = nn.Parameter(
             get_init_weight(
                 (self.n_experts * self.expert_dhead, self.dmodel),
-                fan_in=self.n_experts * self.expert_dhead // self.topk_denominator,
+                fan_in=self.n_experts
+                * self.expert_dhead
+                // (2 * self.topk_denominator),
                 init_type=init_type,
                 scale=init_scale,
             ),
