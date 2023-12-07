c2f7bb881f2ca96ec2932e3c5a55a49f9f22ac00
Simontwice <szymon.antoniak1@gmail.com>
2023-12-07 15:21:48 +0100

    Versioning code

--- lizrd/scripts/grid.py
+++ lizrd/scripts/grid.py
@@ -83,19 +83,19 @@ if __name__ == "__main__":
 
     check_for_argparse_correctness(grid)
 
-    if not CLUSTER_NAME == MachineBackend.LOCAL:
-        if not interactive_debug_session:
-            user_input = input(
-                f"Will run {total_n_experiments} experiments, using up {total_minutes} minutes, i.e. around {round(total_minutes / 60)} hours\n"
-                f"Continue? [Y/n]"
-            )
-        else:
-            user_input = input(
-                "Will run an INTERACTIVE experiment, which will be the first one from the supplied configs. \nContinue? [Y/n]"
-            )
-        if user_input.lower() not in ("", "y", "Y"):
-            print("Aborting...")
-            exit(1)
+    # if not CLUSTER_NAME == MachineBackend.LOCAL:
+    #     if not interactive_debug_session:
+    #         user_input = input(
+    #             f"Will run {total_n_experiments} experiments, using up {total_minutes} minutes, i.e. around {round(total_minutes / 60)} hours\n"
+    #             f"Continue? [Y/n]"
+    #         )
+    #     else:
+    #         user_input = input(
+    #             "Will run an INTERACTIVE experiment, which will be the first one from the supplied configs. \nContinue? [Y/n]"
+    #         )
+    #     if user_input.lower() not in ("", "y", "Y"):
+    #         print("Aborting...")
+    #         exit(1)
 
     if CLUSTER_NAME != MachineBackend.LOCAL:
         first_exp_training_args, _ = grid[0]
@@ -164,7 +164,7 @@ if __name__ == "__main__":
         elif CLUSTER_NAME == MachineBackend.IDEAS:
             subprocess_args = [
                 slurm_command,
-                f"--gres=gpu:{setup_args['n_gpus']}",
+                f"--gres=gpu:ampere:{setup_args['n_gpus']}",
                 f"--cpus-per-gpu={setup_args['cpus_per_gpu']}",
                 f"--job-name={job_name}",
                 f"--time={setup_args['time']}",
@@ -208,9 +208,9 @@ if __name__ == "__main__":
             raise ValueError(f"Unknown cluster name: {CLUSTER_NAME}")
         print(f"running experiment {i} from {job_name}...")
         PROCESS_CALL_FUNCTION(subprocess_args, env)
-        sleep(5)
-        if interactive_debug_session or CLUSTER_NAME == MachineBackend.LOCAL:
-            print(
-                "Ran only the first experiment in (interactive mode or local run). Aborting..."
-            )
-            break
+        # sleep(5)
+        # if interactive_debug_session or CLUSTER_NAME == MachineBackend.LOCAL:
+        #     print(
+        #         "Ran only the first experiment in (interactive mode or local run). Aborting..."
+        #     )
+        #     break
