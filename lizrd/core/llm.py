dc78442f6ab873d7415f000897385757b35787f9
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2023-12-12 12:22:55 +0100

    Versioning code

--- lizrd/core/llm.py
+++ lizrd/core/llm.py
@@ -297,7 +297,7 @@ class Attention(LoggingLayer):
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, dmodel, eps=1e-8):
+    def __init__(self, dmodel, eps=1e-5):
         super().__init__()
         self.eps = eps
 
@@ -311,7 +311,7 @@ class RMSNorm(nn.Module):
 
 
 class LearnableRMSNormCoarse(LoggingLayer):
-    def __init__(self, dmodel, eps=1e-8, init_p=0):
+    def __init__(self, dmodel, eps=1e-5, init_p=0):
         super().__init__()
         self.eps = eps
 
@@ -331,7 +331,7 @@ class LearnableRMSNormCoarse(LoggingLayer):
 
 
 class LearnableRMSNormFine(LoggingLayer):
-    def __init__(self, dmodel, eps=1e-8, init_p=0):
+    def __init__(self, dmodel, eps=1e-5, init_p=0):
         super().__init__()
         self.eps = eps
 
@@ -346,6 +346,8 @@ class LearnableRMSNormFine(LoggingLayer):
 
     def log_heavy(self):
         return {
+            "normalization/p-max": torch.max(self.ps),
+            "normalization/p-min": torch.min(self.ps),
             "normalization/p-mean": torch.mean(self.ps),
             "normalization/p-std": torch.std(self.ps),
         }
