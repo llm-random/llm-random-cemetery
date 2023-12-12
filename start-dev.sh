4cf7010e01ff6854ffcb42bcf63f4150e824b063
Maciej Pióro <m@ciekpio.ro>
2023-12-12 19:18:25 +0100

    Versioning code

--- start-dev.sh
+++ start-dev.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
-
 # setup git hooks
 git config --local core.hooksPath .githooks
 
-# setup the virtual environment
-if [ ! -d "venv" ]; then
-    python3 -m venv venv
-    . venv/bin/activate
-    ./install_requirements.sh
+# is the user using conda?
+if [ -n "$CONDA_DEFAULT_ENV" ]; then
+    CONDA_ENV="llm-random"
+    echo "[start-dev.sh] You're using conda - will create a conda environment"
+    conda create --name $CONDA_ENV python=3.10
+    conda activate $CONDA_ENV
+    $SHELL install_requirements.sh
+    fi
 else
-    echo "[start-dev.sh] I don't know Walt, seems kind of sus to me. You've already got a venv folder, do you really want to make another one? Try again."
+    # setup the virtual environment
+    if [ ! -d "venv" ]; then
+        python3 -m venv venv
+        . venv/bin/activate
+        ./install_requirements.sh
+    else
+        echo "[start-dev.sh] I don't know Walt, seems kind of sus to me. You've already got a venv folder, do you really want to make another one? Try again."
+    fi
 fi
