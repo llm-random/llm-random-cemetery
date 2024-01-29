dbb3df4410017a53665ae1178e494079d1526fa6
Maciej Pióro <m@ciekpio.ro>
2024-01-29 13:14:17 +0100

    Versioning code

--- lizrd/scripts/grid.py
+++ lizrd/scripts/grid.py
@@ -145,6 +145,7 @@ if __name__ == "__main__":
                 f"--gres=gpu:{setup_args['n_gpus']}",
                 "--partition=plgrid-gpu-a100",
                 f"--cpus-per-gpu={setup_args['cpus_per_gpu']}",
+                "--mem=512G",
                 "--account=plgplggllmeffi-gpu-a100",
                 f"--job-name={job_name}",
                 f"--time={setup_args['time']}",
