8a4ae2b47792e35d2bf07c9e5612626a180c7bab
Krystian-Krol <krystian.kytek@gmail.com>
2024-03-06 10:24:53 +0100

    Versioning code

--- research/conditional/moe_layers/token_choice.py
+++ research/conditional/moe_layers/token_choice.py
@@ -51,6 +51,8 @@ class TokenChoiceRouter(LoggingLayer):
 
     def forward(self, x: torch.Tensor):
         # x is (batch, seq_len, dmodel)
+        if not self.training:
+            self.routing_top_k *= 2
         batch_size, seq_len, _ = x.shape
         n_tokens = batch_size * seq_len
         self.update_cache_for_logging("n_tokens", torch.Tensor([n_tokens]))
@@ -167,6 +169,9 @@ class TokenChoiceRouter(LoggingLayer):
             with measure_time(self, "assign_tokens_to_input"):
                 for i, indices in enumerate(indices_of_tokens_for_expert):
                     experts_input[i, : len(indices)] = x[indices]
+
+        if not self.training:
+            self.routing_top_k = self.routing_top_k // 2
         if self.vectorize:
             return experts_input, top_tokens_per_expert_indices, masked_expert_gate
         else:
