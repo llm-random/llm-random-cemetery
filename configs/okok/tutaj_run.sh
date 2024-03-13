6c4c32488828efaf24ea9e37ecaa184f67045f5b
Michał Krutul <crewtoolcompany@gmail.com>
2024-03-13 17:24:03 +0100

    Versioning code

---
+++ configs/okok/tutaj_run.sh
@@ -0,0 +1,8 @@
+parent: configs/hej_test.yaml
+md5_parent_hash: 9c8eef6eddd5f9def9fd31abcb4c06d9
+# hf_datasets_cache: /raid/NFS_SHARE/home/kamil.ciebiera/datasets/datasets
+time: "3:00:00"
+n_gpus: 1
+
+params:
+    name: "mkrun"
