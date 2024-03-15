7b4bb14330b9ebf45439cbe5e3500234a9ccd872
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-03-15 17:43:17 +0100

    Versioning code

--- research/conditional/utils/model_utils.py
+++ research/conditional/utils/model_utils.py
@@ -398,7 +398,11 @@ def get_expert_choice_args(args):
     expert_inner_function = partial(
         get_inner_expert(args), use_topk_initialization=use_topk_initialization
     )
-    args = get_expert_choice_args_old(args)
+    args = dict(
+        **get_expert_choice_args_old(args),
+        get_router_values_from=args.get_router_values_from,
+        moe_values_exp=args.moe_values_exp
+    )
     del args["use_full_einsum"]  # this is no longer compatible
     del args["expert_size"]
     return args, expert_inner_function
@@ -609,6 +613,8 @@ def get_ff_layer(args):
             routing_top_k=args.routing_top_k,
             init_scale=args.init_scale,
             init_type=args.init_type,
+            get_router_values_from=args.get_router_values_from,
+            moe_values_exp=args.moe_values_exp
         )
     elif args.ff_mode == "token_choice_old":
         args = determine_moe_args(args)
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/routing_weights/weightless_gating.yaml --git_branch=weightless_gating_2024-03-15_17-43-17 --skip_copy_code
