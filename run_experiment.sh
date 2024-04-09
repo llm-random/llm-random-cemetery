c6963fa9cfc560403b8e88a7ca06f175ae93dc32
Michał Krutul <crewtoolcompany@gmail.com>
2024-04-09 11:29:43 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/token_dropping/configs/medium.yaml --git_branch=small_baseline_ckpt_old_with_dataseed_21_2024-04-09_11-29-43 --skip_copy_code
