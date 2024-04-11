73d9e7339bba6abb5de9655f5e60a23a4cfd1eb5
Michał Krutul <crewtoolcompany@gmail.com>
2024-04-11 13:50:18 +0200

    Versioning code

--- research/conditional/utils/layer_manager.py
+++ research/conditional/utils/layer_manager.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 from lizrd.support.logging import get_current_logger
 from functools import wraps
+from research.speedtest.utils.layer_manager import LoggingLayer as Inny
 
 
 def get_registered_name(name):
@@ -96,7 +97,7 @@ class LayerManager:
                         )
         if should_clean_up:
             for _, layer in self._logable_layers:
-                if isinstance(layer, LoggingLayer):
+                if isinstance(layer, LoggingLayer) or isinstance(layer, Inny):
                     layer.clean_up_after_logging()
 
     def manage_learnable_temperature(self, step):
@@ -219,21 +220,22 @@ def measure_time(layer: LoggingLayer, instruction_name: str):
         layer: The LoggingLayer object that will be used to cache the time.
         instruction_name: The name of the instruction that is being measured.
     """
-    if layer.logging_switch:
-        if torch.cuda.is_available():
-            start = torch.cuda.Event(enable_timing=True)
-            end = torch.cuda.Event(enable_timing=True)
-            start.record()
-        else:
-            start = time.time()
     yield
-    if layer.logging_switch:
-        if torch.cuda.is_available():
-            end.record()
-            torch.cuda.synchronize()
-            layer.update_cache_for_logging(
-                "time", {instruction_name: start.elapsed_time(end)}
-            )
-        else:
-            end = time.time()
-            layer.update_cache_for_logging("time", {instruction_name: end - start})
+    # if layer.logging_switch:
+    #     if torch.cuda.is_available():
+    #         start = torch.cuda.Event(enable_timing=True)
+    #         end = torch.cuda.Event(enable_timing=True)
+    #         start.record()
+    #     else:
+    #         start = time.time()
+    # yield
+    # if layer.logging_switch:
+    #     if torch.cuda.is_available():
+    #         end.record()
+    #         torch.cuda.synchronize()
+    #         layer.update_cache_for_logging(
+    #             "time", {instruction_name: start.elapsed_time(end)}
+    #         )
+    #     else:
+    #         end = time.time()
+    #         layer.update_cache_for_logging("time", {instruction_name: end - start})
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/token_dropping/configs/medium.yaml --git_branch=no_layer_man_2024-04-11_13-50-18 --skip_copy_code
