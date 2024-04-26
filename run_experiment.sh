fe8e4584f46e8fec0d75146147505b53d67a45ea
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-04-26 18:50:02 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/weightless_cluster/weightless_gating_clustered.yaml --git_branch=weightless_gating_2024-04-26_18-50-02 --skip_copy_code
