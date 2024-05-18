2b6b9cb5969474809e5c4d6113e5360bf39da141
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-05-18 16:31:35 +0200

    Versioning code

--- research/conditional/moe_layers/expert_types.py
+++ research/conditional/moe_layers/expert_types.py
@@ -72,6 +72,126 @@ class ExpertFF(LoggingLayer):
         return experts_output
 
 
+class ExpertProjectedFF(LoggingLayer):
+    def __init__(
+        self,
+        dmodel: int,
+        n_experts: int,
+        expert_size: int,
+        init_type: str,
+        init_scale: float,
+        use_einsum: bool = False,
+        doutput: Optional[int] = None,
+        activation_name: str = "relu",
+        topk: int = 1,
+        use_topk_initialization: bool = False,
+        weights_base_relative: float = 1.,
+        use_layer_norm: bool = False,
+        project_only_one_dim: bool = False,
+        project_skip_softmax: bool = False,
+    ):
+        super().__init__()
+        self.dmodel = dmodel
+        self.doutput = dmodel if doutput is None else doutput
+        self.n_experts = n_experts
+        self.use_einsum = use_einsum
+        self.expert_size = expert_size
+        self.activation = resolve_activation_name(activation_name)
+        self.n_neurons = expert_size*n_experts
+        self.use_layer_norm = use_layer_norm
+        self.project_only_one_dim = project_only_one_dim
+        self.project_skip_softmax = project_skip_softmax
+        weights_base_size = int(self.n_neurons * weights_base_relative)
+
+        if self.use_layer_norm:
+            self.layer_norm_1 = torch.nn.LayerNorm(self.dmodel)
+            self.layer_norm_2 = torch.nn.LayerNorm(self.expert_size)
+
+        fan_in_factor = topk if use_topk_initialization else n_experts
+        self.init_fun = get_init_fun(init_type=init_type, init_scale=init_scale)
+        if self.project_only_one_dim:
+            weights_base_size = int(weights_base_size/expert_size)
+            self.lin1_weight = self.init_fun(
+                shape=(expert_size, weights_base_size, dmodel), fan_in=dmodel
+            )
+            self.lin2_weight = self.init_fun(
+                shape=(expert_size, weights_base_size, self.doutput),
+                fan_in=int(fan_in_factor * expert_size),
+            )
+            self.neuron_selector = self.init_fun(
+                shape=(n_experts, expert_size, weights_base_size),
+                fan_in=weights_base_size,
+            )
+        else:
+            self.lin1_weight = self.init_fun(
+                shape=(weights_base_size, dmodel), fan_in=dmodel
+            )
+            self.lin2_weight = self.init_fun(
+                shape=(weights_base_size, self.doutput),
+                fan_in=int(fan_in_factor * expert_size),
+            )
+
+            self.neuron_selector = self.init_fun(
+                shape=(n_experts, expert_size, weights_base_size),
+                fan_in=weights_base_size,
+            )
+        self.selector_act = torch.nn.Softmax(dim=2)
+
+    @time_measured("calculate_experts")
+    def calculate_experts(self):
+        selector = self.neuron_selector if self.project_skip_softmax else self.selector_act(self.neuron_selector)
+        if self.project_only_one_dim:
+            lin1 = einsum("n_experts expert_size weights_base, expert_size weights_base dmodel "
+                          "-> n_experts dmodel expert_size",
+                          selector, self.lin1_weight)
+            lin2 = einsum("n_experts expert_size weights_base, expert_size weights_base doutput "
+                          "-> n_experts expert_size doutput",
+                          selector, self.lin2_weight)
+        else:
+            lin1 = einsum("n_experts expert_size weights_base, weights_base dmodel -> n_experts dmodel expert_size",
+                          selector, self.lin1_weight)
+            lin2 = einsum("n_experts expert_size weights_base, weights_base doutput -> n_experts expert_size doutput",
+                          selector, self.lin2_weight)
+        if self.use_layer_norm:
+            lin1 = self.layer_norm_1(lin1.permute(0, 2, 1)).permute(0, 2, 1)
+            lin2 = self.layer_norm_2(lin2.permute(0, 2, 1)).permute(0, 2, 1)
+        return lin1, lin2
+
+    def forward(self, x: torch.Tensor):
+        n_experts, capacity, dmodel = x.shape
+
+        assert n_experts == self.n_experts
+        assert dmodel == self.dmodel
+        lin1, lin2 = self.calculate_experts()
+
+        experts_output = self.calculate_ff(capacity, n_experts, x, lin1, lin2)
+        return experts_output
+
+    @time_measured("process_by_experts")
+    def calculate_ff(self, capacity, n_experts, x, lin1, lin2):
+        # maybe remove these einsums that just multiply two tensors? This will never be faster than torch.matmul
+        # unlike of course einsums with >2 tensors
+        if self.use_einsum:
+            experts_output = einsum(
+                "n_experts capacity dmodel, n_experts dmodel expert_size -> n_experts capacity expert_size",
+                x,
+                lin1,
+            )
+        else:
+            experts_output = torch.matmul(x, lin1)
+        experts_output = self.activation(experts_output)
+        if self.use_einsum:
+            experts_output = einsum(
+                "n_experts capacity expert_size, n_experts expert_size doutput -> n_experts capacity doutput",
+                experts_output,
+                lin2,
+            )
+        else:
+            experts_output = torch.matmul(experts_output, lin2)
+        assert experts_output.shape == (n_experts, capacity, self.doutput)
+        return experts_output
+
+
 class ExpertGated(ExpertFF):
     def __init__(
         self,
