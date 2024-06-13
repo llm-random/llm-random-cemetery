6965f0867a31f4d11ce4569ff5e0edac70873222
Michał Krutul <crewtoolcompany@gmail.com>
2024-06-13 19:00:36 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/token_reduction/configs/512/small_finetuning.yaml --git_branch=baseline_small_finetuning_cutoff_512_2024-06-13_19-00-36 --skip_copy_code
