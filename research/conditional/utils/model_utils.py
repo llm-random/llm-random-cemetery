92a089eb4dbe7b47bb29efec2cb93280e7cc9a46
Janek <jan.malasnicki@gmail.com>
2024-06-21 18:33:45 +0200

    Versioning code

--- research/conditional/utils/model_utils.py
+++ research/conditional/utils/model_utils.py
@@ -555,6 +555,14 @@ def get_ff_layer(args):
         return_fn = lambda: kan.MlpKan(
             args.dmodel, args.dff, init_type=args.init_type, init_scale=args.init_scale
         )
+    elif args.ff_mode == "mlp_kan_norelu":
+        return_fn = lambda: kan.MlpKan_norelu(
+            args.dmodel, args.dff, init_type=args.init_type, init_scale=args.init_scale
+        )
+    elif args.ff_mode == "kan_latent":
+        return_fn = lambda: kan.KanLatent(
+            args.dmodel, args.dff, init_type=args.init_type, init_scale=args.init_scale
+        )
     elif args.ff_mode == "kan_mlp":
         return_fn = lambda: kan.KanMlp(
             args.dmodel, args.dff, init_type=args.init_type, init_scale=args.init_scale
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/test/kan_vanilla.yaml --git_branch=kanmlp_2024-06-21_18-33-45 --skip_copy_code
