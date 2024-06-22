463125ee4af553ec36bc369f62e5968cb5aa482c
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-06-22 18:07:41 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_test_0_learning_rate_RYT3Z9QN.yaml --git_branch=local_search_test_0_learning_rate_RYT3Z9QN_2024-06-22_18-07-41 --skip_copy_code --skip_confirmation
