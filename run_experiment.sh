f76abc6d4790b39f5b938dce07e6ca522d9132e3
Michał Krutul <crewtoolcompany@gmail.com>
2024-06-22 13:28:55 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/token_reduction/configs/1024/base_dropping.yaml --git_branch=baseline_base_dropping_reduced_to_1024_2024-06-22_13-28-55 --skip_copy_code
