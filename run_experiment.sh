455b8696524c60b4ffba847b09b96d878f283a3f
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-06-22 18:07:47 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_test_0_learning_rate_CVPPVBDF.yaml --git_branch=local_search_test_0_learning_rate_CVPPVBDF_2024-06-22_18-07-47 --skip_copy_code --skip_confirmation
