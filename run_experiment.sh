455fe7b95831ed35b861a5a4deed12c1d4e32fc8
Michał Krutul <crewtoolcompany@gmail.com>
2024-06-23 12:24:33 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/token_reduction/configs/1024/small_merging.yaml --git_branch=small_merging_reduced_to_1024_2024-06-23_12-24-33 --skip_copy_code
