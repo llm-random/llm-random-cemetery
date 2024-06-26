6eaa61d3f558ee159a37829aff39d4f2838e4f18
Maciej Pióro <m@ciekpio.ro>
2024-06-26 11:55:49 +0200

    Versioning code

--- research/subtoken/text/packers.py
+++ research/subtoken/text/packers.py
@@ -10,6 +10,7 @@ from torch.utils.data import IterableDataset
 from lizrd.text.datasets import AbstractDataset
 from lizrd.text.data import LLMExample
 from lizrd.text.tokenizers import AbstractTokenizer, BertTokenizer, GPTTokenizer
+from research.subtoken.text.data import SubtokenLLMExample
 
 
 def take_circular(iterable, start, stop):
@@ -216,3 +217,171 @@ class GPTPacker(
         calculate_loss = [1] * len(target_ids)
 
         return LLMExample(input_ids, target_ids, calculate_loss)
+
+
+def token_ids_to_strings(token_ids: List[int], tokenizer) -> List[str]:
+    return tokenizer.batch_decode(
+        [[tid] for tid in token_ids], skip_special_tokens=True
+    )
+
+
+def string_to_bytes_list(s: str) -> List[int]:
+    return list(s.encode("utf-8"))
+
+
+class SubtokenGPTPacker(
+    AbstractPacker,
+):
+    FORBIDDEN_TOKENS = [45706]
+    SPECIAL_TOKENS = [50256]
+    TOKEN_REPLACEMENTS = {
+        15171: [220, 16733, 7992],
+        20727: [220, 403, 18789],
+        27473: [220, 46813, 20860],
+        28719: [220, 6381, 1676, 16864, 1286],
+        29760: [220, 29609, 420, 28018],
+        29789: [220, 25413, 4625, 5646],
+        30210: [220, 48317, 13739, 3118, 18143],
+        30213: [220, 22615, 2514, 27881, 10049],
+        30982: [220, 6381, 1676, 16864, 378],
+        32799: [220, 22769, 1634],
+        34400: [220, 1783],
+        36174: [220, 29531, 34832, 35992],
+        36573: [220, 24588, 19541],
+        36658: [220, 4770, 28],
+        37389: [220, 12100, 12100],
+        40586: [220, 38986, 282, 1023],
+        40800: [220, 9979, 2738, 453],
+        40887: [220, 70, 459, 305, 36387],
+        42045: [220, 298, 7537, 72, 16607],
+        43453: [220, 46933, 42202],
+        43649: [220, 521, 396, 41726],
+        44436: [220, 298, 10406, 1834, 1056],
+        44713: [220, 4181],
+        45545: [220, 26503, 44686, 42983],
+        46674: [220, 24588, 27781],
+        47757: [220, 259, 785, 3866, 5135, 856],
+        48667: [220, 31709, 20860],
+        16529: [220, 1783, 1783, 1783, 1783],
+        20368: [220, 1783, 1783],
+        38093: [220, 4770, 4770, 4770, 4770, 28],
+        41436: [220, 35937, 35937],
+        41906: [220, 8412, 8412],
+        46111: [220, 4770, 4770, 28],
+        30899: [21018, 30898],
+        39177: [7449, 39142],
+        39753: [39752, 10493],
+        39755: [39714, 39655],
+        39756: [24807, 31208],
+        39757: [17620, 29841],
+        40242: [39693, 40241],
+        41380: [21353, 41215],
+        30906: [30905, 21018, 30898],
+        31576: [22615, 31573],
+        3880: [1783, 1783],
+        8864: [4181, 4181],
+        10052: [4770, 4770],
+        10097: [1783, 1783, 1783, 1783],
+        10221: [4841, 4841],
+        14950: [8184, 8184],
+        17174: [8412, 8412],
+        19351: [35937, 35937],
+        22369: [10541, 10541],
+        23193: [4181, 4181, 4181, 4181],
+        14827: [9364, 9364],
+        23090: [9364, 9364, 9364, 9364],
+        23926: [4770, 4770, 4770, 4770],
+        27006: [15243, 15243],
+        27193: [4841, 4841, 4841, 4841],
+        27754: [2109, 2109, 2109],
+        49129: [16317, 16317, 16317],
+        28542: [16068, 16068],
+        29113: [14468, 14468],
+        29146: [15864, 15864],
+        30542: [8184, 8184, 8184, 8184],
+        32941: [2602, 2602, 2602],
+        49527: [20503, 20503],
+        49704: [27246, 27246],
+        35496: [9364, 9364, 9364, 9364, 9364, 9364, 9364, 9364],
+        43801: [26171, 26171, 26171, 26171],
+        47232: [1783, 1783, 1783],
+        39172: [17811, 17811],
+    }
+
+    def __init__(
+        self,
+        sequence_length: int,
+        dataset_maker: AbstractDataset,
+        tokenizer_maker: Callable[[], GPTTokenizer],
+        seed: Optional[int] = None,
+    ):
+        super().__init__(
+            sequence_length,
+            dataset_maker,
+            tokenizer_maker,
+            seed=seed,
+        )
+
+    def get_sample(self) -> SubtokenLLMExample:
+        """
+        Sample examples from the dataset until we reach the desired sequence length.
+        """
+        eot_id = self.tokenizer.eot_id
+        assert eot_id is not None
+
+        buffer: List[int] = []
+        calculate_loss: List[int] = []
+        document_lengths: List[int] = []
+
+        while True:
+            document = self.dataset.get_document()
+            tokens = self.tokenizer.text_to_ids(document)
+            tokens_with_replacement = []
+            skip_document = False
+            for t in tokens:
+                if t in self.FORBIDDEN_TOKENS:
+                    # raise ValueError(
+                    #     "Token cannot be in FORBIDDEN_TOKENS"
+                    # )  # TODO: somehow the forbidden tokens still show up :/ debug that
+                    skip_document = True
+                    break
+                elif t in self.TOKEN_REPLACEMENTS:
+                    tokens_with_replacement.extend(self.TOKEN_REPLACEMENTS[t])
+                else:
+                    tokens_with_replacement.append(t)
+            if skip_document:
+                continue
+            assert len(tokens_with_replacement) >= len(tokens)
+            tokens = tokens_with_replacement
+
+            buffer.extend(tokens + [eot_id])
+
+            document_lengths.append(len(tokens) + 1)
+            if (sum(document_lengths) - max(document_lengths)) > self.sequence_length:
+                break
+
+        sample_start = self.py_rng.randint(0, len(buffer) - 1)
+        sample_end = sample_start + self.sequence_length
+
+        input_ids = list(take_circular(buffer, sample_start, sample_end))
+        input_bytes = []
+        for token in input_ids:
+            if token != self.tokenizer.eot_id:
+                # bytes_ = list(
+                #     bytes(self.tokenizer.token_id_to_text[token].encode("utf-8"))
+                # )
+                # bytes_ = self.tokenizer.tokenizer.convert_tokens_to_string(
+                #     self.tokenizer.tokenizer.convert_ids_to_tokens(token)
+                # ).encode("utf-8")
+                bytes_ = list(self.tokenizer.token_id_to_text[token].encode("utf-8"))
+                assert len(bytes_) <= self.tokenizer.MAX_BYTES_PER_TOKEN
+                input_bytes.append(
+                    bytes_ + [-1] * (self.tokenizer.MAX_BYTES_PER_TOKEN - len(bytes_))
+                )
+            else:
+                input_bytes.append([-1] * self.tokenizer.MAX_BYTES_PER_TOKEN)
+        target_ids = list(take_circular(buffer, sample_start + 1, sample_end + 1))
+        calculate_loss = [1] * len(target_ids)
+        # is_special_token = [t in self.SPECIAL_TOKENS for t in input_ids]
+
+        return SubtokenLLMExample(input_ids, input_bytes, target_ids, calculate_loss)
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/subtoken/gpt2medium.yaml --git_branch=subtoken_2024-06-26_11-55-48 --skip_copy_code
