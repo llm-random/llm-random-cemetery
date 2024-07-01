813d55b2f769038576a46f71b1744234be0f5317
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-07-01 19:33:21 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_medium_truncated_0_weight_decay_775OQ34N.yaml --git_branch=local_search_medium_truncated_0_weight_decay_775OQ34N_2024-07-01_19-33-21 --skip_copy_code --skip_confirmation
