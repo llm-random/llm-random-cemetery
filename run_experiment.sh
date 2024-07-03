399d16d390cdcebdabad0d634676e412a20ce3e8
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-07-03 05:37:12 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_medium_relative_lr_1_init_scale_PKTDIM2T.yaml --git_branch=local_search_medium_relative_lr_1_init_scale_PKTDIM2T_2024-07-03_05-37-12 --skip_copy_code --skip_confirmation
