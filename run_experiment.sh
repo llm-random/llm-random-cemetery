ada750d162c516b1707ae358b5879ef466344b4e
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-07-03 03:51:40 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_medium_relative_lr_1_relative_lr_embedding_layer_LZ4IR01L.yaml --git_branch=local_search_medium_relative_lr_1_relative_lr_embedding_layer_LZ4IR01L_2024-07-03_03-51-40 --skip_copy_code --skip_confirmation
