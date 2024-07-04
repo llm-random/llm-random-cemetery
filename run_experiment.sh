182ce0eee19b3073435d091100e157b88ff36756
Michał Krutul <crewtoolcompany@gmail.com>
2024-07-04 19:43:30 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/token_reduction/configs/sota_base_seqlen1024.yaml --git_branch=base_merging_scheduled_1.2_2024-07-04_19-43-30 --skip_copy_code
