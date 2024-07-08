36770844da9c1b05ff66d29efd7b3ef9df083335
Janek <jan.malasnicki@gmail.com>
2024-07-08 03:11:15 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/test_scheduler_fraction.yaml --git_branch=relative_lr_medium_scheduler_cos-like-const_2024-07-08_03-11-15 --skip_copy_code
