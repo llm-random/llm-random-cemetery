84401068cb82079628e18d293b66f1777542b557
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-07-08 15:06:56 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_moe_0_init_scale_OCALQLFR.yaml --git_branch=local_search_relative_moe_0_init_scale_OCALQLFR_2024-07-08_15-06-56 --skip_copy_code --skip_confirmation
