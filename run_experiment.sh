0c55d427979b0d4796885c1c11e17a1d07ffde8a
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-07-09 05:31:36 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_moe_short_2_relative_lr_gating_N68RT380.yaml --git_branch=local_search_relative_moe_short_2_relative_lr_gating_N68RT380_2024-07-09_05-31-36 --skip_copy_code --skip_confirmation
