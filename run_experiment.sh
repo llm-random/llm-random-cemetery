259b31a1951f8ddb9665af371f8586a25023d3d8
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-07-09 14:52:59 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/token_reduction/configs/rnn_base_seqlen1024.yaml --git_branch=base_token_rnn_merging_2024-07-09_14-52-59 --skip_copy_code --skip_confirmation
