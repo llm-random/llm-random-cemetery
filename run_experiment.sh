09b3d516ee54e0bdb4e8636f4a5a6692fdba8ed9
localsearch <search@local.com>
2024-07-10 23:59:42 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_moe_short_1_learning_rate_LYAEKWKA.yaml --git_branch=local_search_relative_moe_short_1_learning_rate_LYAEKWKA_2024-07-10_23-59-42 --skip_copy_code --skip_confirmation
