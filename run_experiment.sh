0d760ecfd574182abf8bb159df05f7344bb3b546
localsearch <search@local.com>
2024-07-17 21:50:48 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_moe_short_1_final_lr_fraction_KNZAGSEC.yaml --git_branch=local_search_relative_moe_short_1_final_lr_fraction_KNZAGSEC_2024-07-17_21-50-48 --skip_copy_code --skip_confirmation
