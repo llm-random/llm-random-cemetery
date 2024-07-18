3df289c9570fc0f4677276eb152a07140285ae35
localsearch <search@local.com>
2024-07-18 23:53:35 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_moe_short_4_relative_lr_expert_inner_function_TFCWCGAY.yaml --git_branch=local_search_relative_moe_short_4_relative_lr_expert_inner_function_TFCWCGAY_2024-07-18_23-53-35 --skip_copy_code --skip_confirmation
