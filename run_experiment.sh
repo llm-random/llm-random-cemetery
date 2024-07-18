68699c41a6777330a936bfc4cadd5dcbecbcdf92
localsearch <search@local.com>
2024-07-18 07:36:42 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_moe_short_2_relative_scheduler_fraction_gating_5AP63TV9.yaml --git_branch=local_search_relative_moe_short_2_relative_scheduler_fraction_gating_5AP63TV9_2024-07-18_07-36-42 --skip_copy_code --skip_confirmation
