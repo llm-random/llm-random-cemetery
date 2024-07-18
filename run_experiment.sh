3e305edeba937de8ae327b947205dba053df2451
localsearch <search@local.com>
2024-07-18 18:11:17 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_moe_short_3_final_lr_fraction_4QMINFJ5.yaml --git_branch=local_search_relative_moe_short_3_final_lr_fraction_4QMINFJ5_2024-07-18_18-11-17 --skip_copy_code --skip_confirmation
