acd2dcd74115ef6224034410ae2cfa0a93a84852
localsearch <search@local.com>
2024-07-18 08:25:40 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_moe_short_2_relative_scheduler_fraction_head_CMTMPT7T.yaml --git_branch=local_search_relative_moe_short_2_relative_scheduler_fraction_head_CMTMPT7T_2024-07-18_08-25-40 --skip_copy_code --skip_confirmation
