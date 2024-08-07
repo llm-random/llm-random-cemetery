307802cc5c029902286b0641ccac62adcd8e4985
Maciej Pióro <m@ciekpio.ro>
2024-08-07 09:10:45 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/mot/lots_of_evals/stays/dense_medium_from_neptune.yaml --git_branch=mot_medium_vanilla_baseline_2024-08-07_09-10-45 --skip_copy_code
