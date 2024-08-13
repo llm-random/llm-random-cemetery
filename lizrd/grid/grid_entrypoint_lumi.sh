d058a7e938cf119162d67f9f6eed5924adde2f9a
Maciej Pióro <m@ciekpio.ro>
2024-08-13 16:58:32 +0200

    Versioning code

---
+++ lizrd/grid/grid_entrypoint_lumi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+module CUDA/11.7.0
+echo "Will run the following command:"
+echo "$@"
+echo "==============================="
+# conda activate /scratch/project_465001227/llm-random-group/env
+conda activate /scratch/project_465001227/llm-random-group/conda3.11
+$@
