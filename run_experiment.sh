11335e7b4c06b259a790778eaeac38850e8ea9a5
Michał Krutul <crewtoolcompany@gmail.com>
2024-08-14 17:50:08 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/token_reduction/configs/taki_duzy_longer_aws.yaml --git_branch=large_merging_scheduled_1_1_2024-08-14_17-50-08 --skip_copy_code
