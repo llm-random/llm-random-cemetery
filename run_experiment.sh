cbec02d49fd28fca974339d580c4a426fa472eee
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-08-14 14:17:17 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/85M/expansion_rate/4.yaml --git_branch=constrained_scaling_grid_2024-08-14_14-17-17 --skip_copy_code
