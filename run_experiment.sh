d1dd2584e54ff91d7a38194135c7cbabaa1bc57f
Maciej Pióro <m@ciekpio.ro>
2024-08-22 09:54:20 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/justification/medium_relative_all.yaml --git_branch=relative_all_medium_2024-08-22_09-54-20 --skip_copy_code
