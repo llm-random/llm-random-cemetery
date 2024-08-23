a711559f006ff43168ef859b4088c138515d48c0
Maciej Pióro <m@ciekpio.ro>
2024-08-23 19:09:20 +0200

    Versioning code

--- lizrd/core/distributed.py
+++ lizrd/core/distributed.py
@@ -18,6 +18,32 @@ def wrap_in_ddp(
     return DDP(module=module.to(f"cuda:{rank}"), device_ids=[rank])
 
 
+def module_whitelist_wrap_policy(
+    module: torch.nn.Module,
+    recurse: bool,
+    nonwrapped_numel: int,
+    # Additional custom arguments
+    modules_to_wrap: tuple[Type[nn.Module]],
+) -> bool:
+    if recurse:
+        print("Returning True, because we are recursing")
+    elif isinstance(module, modules_to_wrap):
+        print("Returning True, because module is in the whitelist")
+    else:
+        print("Returning False, because module is not in the whitelist")
+    return recurse or isinstance(module, modules_to_wrap)
+
+
+def module_size_based_wrap_policy(
+    module: torch.nn.Module,
+    recurse: bool,
+    nonwrapped_numel: int,
+    # Additional custom arguments
+    min_num_params: int,
+) -> bool:
+    return nonwrapped_numel >= min_num_params
+
+
 def wrap_in_fsdp(
     module: nn.Module,
     rank: Optional[int],
@@ -33,6 +59,16 @@ def wrap_in_fsdp(
     assert (modules_to_wrap is None and min_num_params is not None) or (
         modules_to_wrap is not None and min_num_params is None
     ), "The FSDP arguments `modules_to_wrap` and `min_num_params` are mutually exclusive. Either supply one, or the other."
+
+    # print("modules_to_wrap:", modules_to_wrap)
+    # if modules_to_wrap is not None:
+    #     wrap_policy = partial(
+    #         module_whitelist_wrap_policy, modules_to_wrap=modules_to_wrap
+    #     )
+    # else:
+    #     wrap_policy = partial(
+    #         module_size_based_wrap_policy, min_num_params=min_num_params
+    #     )
 
     if modules_to_wrap is not None:
         wrap_policy = ModuleWrapPolicy(modules_to_wrap)
