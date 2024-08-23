6641b6db90dd251058277d81e51a6d1f986ac19d
Janek <jan.malasnicki@gmail.com>
2024-08-23 15:52:16 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/large_extrapolation.yaml --git_branch=large_extrapolation_baseline_lr_tune_seqlen1024_debug_2024-08-23_15-52-16 --skip_copy_code
