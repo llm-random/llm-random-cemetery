56c7f967141dc790a1af2e827188b68fbc2aaa8a
Janek <jan.malasnicki@gmail.com>
2024-08-24 13:22:04 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/large_extrapolation.yaml --git_branch=large_extrapolation_new_code_debug_2024-08-24_13-22-04 --skip_copy_code
