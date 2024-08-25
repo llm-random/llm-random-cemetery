8dfd65f82bd646fcfc118500183caf841ecd3e49
Janek <jan.malasnicki@gmail.com>
2024-08-25 10:59:31 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/medium_relative_attn_start_tune.yaml --git_branch=relative_medium_attn_grid_2024-08-25_10-59-31 --skip_copy_code
