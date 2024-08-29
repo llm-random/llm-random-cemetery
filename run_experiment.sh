a247acd58448e003c478f995fd78b2679ed52f2d
localsearch <search@local.com>
2024-08-29 01:39:43 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_dense_0_relative_scheduler_fraction_projection_GTV7UOPR.yaml --git_branch=local_search_relative_medium_dense_0_relative_scheduler_fraction_projection_GTV7UOPR_2024-08-29_01-39-43 --skip_copy_code --skip_confirmation
