64b07316dd8afe79d6e57ad77997d51b5c76b2d5
localsearch <search@local.com>
2024-08-30 00:08:06 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_dense_3_relative_lr_projection_XUWI9G77.yaml --git_branch=local_search_relative_medium_dense_3_relative_lr_projection_XUWI9G77_2024-08-30_00-08-06 --skip_copy_code --skip_confirmation
