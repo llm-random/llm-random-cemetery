e4bc4fa7b08a61e923a1b016b652b08bbe4d12ab
localsearch <search@local.com>
2024-08-30 04:02:38 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_dense_3_relative_lr_layer_feedforward_Q88PSDB5.yaml --git_branch=local_search_relative_medium_dense_3_relative_lr_layer_feedforward_Q88PSDB5_2024-08-30_04-02-38 --skip_copy_code --skip_confirmation
