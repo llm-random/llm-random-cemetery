21d47a4952beef00636c20d90c18d3609582b661
Szymon Sadkowski <szysad@air.home>
2024-08-30 14:31:40 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/grad_norm/grid/exp_0_all_v1.yaml --git_branch=grad_norm_formulas_exp_0_all_v1_2024-08-30_14-31-40 --skip_copy_code
