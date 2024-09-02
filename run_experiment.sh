1f6dbe8dfb1ae1328e2ccdd78fc57c782e828dd2
Janek <jan.malasnicki@gmail.com>
2024-09-02 10:53:54 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/midlarge_moe_extrapolation.yaml --git_branch=midlarge_extrapolation_relative_2024-09-02_10-53-54 --skip_copy_code
