fe1658d956e7bb1762512a8549c02513eae1f268
Janek <jan.malasnicki@gmail.com>
2024-09-02 15:41:45 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/3M/expansion_rate/1.yaml --git_branch=neptune_is_stupiiid_2024-09-02_15-41-45 --skip_copy_code
