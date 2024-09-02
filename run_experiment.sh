530cb0ef770ce0aaa37a37f23aa1e9f1810a1c6c
m1kush <m1kush@disroot.org>
2024-09-02 21:27:22 +0200

    Versioning code

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m lizrd.grid --config_path=configs/experiments/multinode/multinode_1x1.yaml --git_branch=multinode_1x1_2024-08-20_21-06-10 --skip_copy_code
+python3 -m lizrd.grid --config_path=configs/baselines/gpt/dense/small.yaml --git_branch=baseline_2024-09-02_21-27-22 --skip_copy_code
