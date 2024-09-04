11806a101d4721a65154532c362c054c78c7892d
Janek <jan.malasnicki@gmail.com>
2024-09-04 17:37:02 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/base_local_minimum/start_expert_inner_function.yaml --git_branch=base_extrapolation_local_min_2024-09-04_17-37-02 --skip_copy_code
