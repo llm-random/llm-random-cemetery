d9266a88a76b150266161cd97dcf14762ab52598
Janek <jan.malasnicki@gmail.com>
2024-09-05 20:21:17 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/medium_local_minimum/end_embedding_layer.yaml --git_branch=medium_local_min_2024-09-05_20-21-17 --skip_copy_code
---
+++ run_exps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Folder containing the config files
+CONFIG_FOLDER="configs/experiments/relative_lr/medium_local_minimum/"
+
+# Loop through all config files in the folder
+for config in "$CONFIG_FOLDER"*
+do
+  # Execute the python script with the current config file
+  python submit_experiment.py --host athena --config "$config"
+  
+  # Optionally, you can add a sleep interval to avoid overwhelming the system with too many requests at once
+  # sleep 1
+done
