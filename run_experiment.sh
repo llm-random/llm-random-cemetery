2c6e5b8ba3353b246b6d43a5d80e0a926172af9c
Janek <jan.malasnicki@gmail.com>
2024-09-05 19:05:45 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/base_local_minimum/start_projection.yaml --git_branch=base_extrapolation_local_min_2024-09-05_19-05-45 --skip_copy_code
