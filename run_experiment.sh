39b8081d3eff48fce47a020060943c220f348da6
Janek <jan.malasnicki@gmail.com>
2024-09-05 18:31:16 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/base_extrapolation.yaml --git_branch=relative_base_component_contribution_2024-09-05_18-31-16 --skip_copy_code
