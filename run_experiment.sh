23217472456632195910ef933121a65690227d81
Janek <jan.malasnicki@gmail.com>
2024-09-05 19:13:36 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/base_local_minimum/end_gating.yaml --git_branch=base_extrapolation_local_min_2024-09-05_19-13-36 --skip_copy_code
