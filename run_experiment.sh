088c77575fd8a16069b5feb818ad4dabda535bbb
Janek <jan.malasnicki@gmail.com>
2024-09-05 20:33:01 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/medium_local_minimum/start_gating.yaml --git_branch=medium_local_min_2024-09-05_20-33-01 --skip_copy_code
