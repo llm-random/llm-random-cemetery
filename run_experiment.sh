c3a638c746d06d640c0a7d997be195382b3f1410
Janek <jan.malasnicki@gmail.com>
2024-09-05 18:28:54 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/medium_relative.yaml --git_branch=relative_medium_component_contribution_2024-09-05_18-28-54 --skip_copy_code
