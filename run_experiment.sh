a8b7a1cffc5d32ba98dd59af03df01115c6b4c26
Janek <jan.malasnicki@gmail.com>
2024-09-06 21:53:11 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/25M/test_saving.yaml --git_branch=test_checkpoint_saving_2024-09-06_21-53-11 --skip_copy_code
