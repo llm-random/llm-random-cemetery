bdafb27cf3b7ddeb2b0513ece13715a2440b6574
Kuba Krajewski <gim.jakubk@gmail.com>
2024-09-06 14:58:06 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/3M/expansion_rate/1.yaml --git_branch=constrained_scaling_grid_fixed_2024-09-06_14-58-06 --skip_copy_code
