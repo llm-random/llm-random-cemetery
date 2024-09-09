ccc3fe2894541ef5ee32f8eb85ea989a5e08c311
Szymon Sadkowski <szysad@air.home>
2024-09-09 11:24:03 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/grad_norm/grid/exp_10_post_add_v2.yaml --git_branch=grad_norm_formulas_exp_10_post_add_v2_2024-09-09_11-24-03 --skip_copy_code
