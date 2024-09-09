4d34a68162472d91248b4dd7228c8eaa657cdc9c
Szymon Sadkowski <szysad108@gmail.com>
2024-09-09 17:28:03 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/grad_norm/grid/exp_8_post_norm_v3.yaml --git_branch=grad_norm_formulas_exp_8_post_norm_v3_2024-09-09_17-28-03 --skip_copy_code
