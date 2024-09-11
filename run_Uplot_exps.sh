b478488e0834805dcdf665eca0f7640c834c7830
Janek <jan.malasnicki@gmail.com>
2024-09-12 01:28:32 +0200

    Versioning code

---
+++ run_Uplot_exps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Folder containing the config files
+CONFIG_FOLDER="configs/experiments/relative_lr/medium_local_minimum/"
+
+# Initialize a counter
+counter=1
+
+# Loop through all config files in the folder
+for config in "$CONFIG_FOLDER"*
+do
+  # Print the loop number
+  echo "Loop number: $counter"
+  
+  # Execute the python script with the current config file
+  echo $config
+  python submit_experiment.py --host athena --config "$config"
+  
+  # Increment the counter
+  ((counter++))
+
+  # Optionally, you can add a sleep interval to avoid overwhelming the system with too many requests at once
+  # sleep 1
+done
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/medium_local_minimum/end_projection.yaml --git_branch=medium_local_min_std_2024-09-12_01-28-32 --skip_copy_code
