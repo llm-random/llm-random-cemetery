2b9e284186a91e2d6d50663d4e5a9eaa77cc8fde
Janek <jan.malasnicki@gmail.com>
2024-09-12 01:04:26 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/medium_local_minimum/start_embedding_layer.yaml --git_branch=medium_local_min_std_2024-09-12_01-04-26 --skip_copy_code
