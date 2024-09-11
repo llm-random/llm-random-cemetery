4f6553d8ad9884cd7224f575b81557549f0d599c
Szymon Sadkowski <szysad108@gmail.com>
2024-09-11 14:35:04 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/grad_norm/std_norm_grid/sanity_check.yaml --git_branch=std_grad_norm_sanity_check_2024-09-11_14-35-04 --skip_copy_code
