89006b19733ffdf7fc8882fbca5c5661f3eaf1ec
Janek <jan.malasnicki@gmail.com>
2024-09-12 00:02:49 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/component_ablation/embedding_layer.yaml --git_branch=relativity_component_ablations_2024-09-12_00-02-49 --skip_copy_code
