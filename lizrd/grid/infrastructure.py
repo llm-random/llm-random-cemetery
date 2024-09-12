13f7351a1ef0b5ddb2a3654a7297072c419d686e
madragonse <madragonse@gmail.com>
2024-09-12 14:07:05 +0200

    Versioning code

--- lizrd/grid/infrastructure.py
+++ lizrd/grid/infrastructure.py
@@ -267,7 +267,7 @@ class WriterBackend(MachineBackend):
         return [
             slurm_command,
             f"--gres=gpu:a100:{setup_args['n_gpus']}",
-            f"--array=0-{n_consecutive-1}%1",
+            f"--array=0-{n_consecutive}%1",
             f"--cpus-per-gpu={setup_args['cpus_per_gpu']}",
             f"--mem={max(125, setup_args['mem_per_gpu']*setup_args['n_gpus'])}G",
             f"--job-name={training_args['name']}",
