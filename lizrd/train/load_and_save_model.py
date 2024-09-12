2947538c6cbadb1ea279bb7dd61d5a5b5c61e8ac
madragonse <madragonse@gmail.com>
2024-09-12 17:00:43 +0200

    Versioning code

--- lizrd/train/load_and_save_model.py
+++ lizrd/train/load_and_save_model.py
@@ -1,4 +1,5 @@
 import pathlib
+import re
 from typing import Union, Optional
 import os
 
@@ -11,6 +12,17 @@ from torch.distributed.fsdp import (
 
 from lizrd.support.misc import generate_random_string
 
+def get_latest_model(dir_path) -> pathlib.Path:
+    dir_path = pathlib.Path(dir_path)
+    all_models_paths = dir_path.glob(r"[0-9]*.pt")
+    latest_model = None
+    latest_step = 0
+    for mp in all_models_paths:
+        c_step = int(re.findall(r'\d+', mp.name)[0])
+        if c_step >= latest_step:
+            latest_model = mp
+            latest_step = c_step
+    return latest_model
 
 def get_checkpoint_from_path(load_weights_path: str, repeater_mode: bool) -> str:
     assert os.path.exists(load_weights_path), f"Path {load_weights_path} does not exist"
@@ -18,12 +30,12 @@ def get_checkpoint_from_path(load_weights_path: str, repeater_mode: bool) -> str
         load_weights_path = pathlib.Path(load_weights_path)
 
         if load_weights_path.is_dir():
-            load_weights_path = load_weights_path/"repeater_save.pt" #dev highest number TODO
-
-        if load_weights_path.exists():
-            print(f"Repeater file ({load_weights_path}) does not exist, starting new training.")
-            return None
-
+            latest_model = get_latest_model(load_weights_path)
+            if not latest_model:
+                print(f"No model yet saved in ({load_weights_path}), starting new training.")
+                return None
+            load_weights_path = load_weights_path/latest_model.name
+        
     print(f"Loading checkpoint from {load_weights_path}...")
     checkpoint = torch.load(str(load_weights_path))
     print(f"Checkpoint loaded")
@@ -69,8 +81,8 @@ def prepare_save_weights_path(path_to_dir: Optional[str], is_repeater:bool=False
     # we need a random dir because we can be running a whole grid from the same directory
     if not is_repeater:
         random_dirname = f"{generate_random_string(10)}"
-        save_weights_path = os.path.join(path_to_dir, random_dirname)
-    save_weights_path = os.path.abspath(save_weights_path)
+        path_to_dir = os.path.join(path_to_dir, random_dirname)
+    save_weights_path = os.path.abspath(path_to_dir)
     os.makedirs(save_weights_path, exist_ok=True)
     return save_weights_path
 
