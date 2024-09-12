5862b4223b46f3d716a4557f28ca19724bd5d9be
Szymon Sadkowski <szysad108@gmail.com>
2024-09-12 14:55:09 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/grad_norm/std_norm_grid/c_grid/post_attn_and_ff_v1/exp_3_c_0_2.yaml --git_branch=gn_c_grid_exp_0_post_attn_and_ff_v1_c_0_2_2024-09-12_14-55-09 --skip_copy_code
