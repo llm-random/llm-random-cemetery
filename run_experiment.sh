00d78b252f4bf883736eb43dace494db6de7f8ff
Szymon Sadkowski <szysad108@gmail.com>
2024-09-12 09:54:34 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/grad_norm/std_norm_grid/c_grid/post_add_v1/exp_4_c_neg_0_1.yaml --git_branch=gn_c_grid_exp_4_post_add_v1_c_neg_0_1_2024-09-12_09-54-34 --skip_copy_code
