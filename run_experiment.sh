5b57ac298deb578d58642334271767829c3d64ac
Janek <jan.malasnicki@gmail.com>
2024-09-13 21:16:29 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/520M/16B.yaml --git_branch=constrained_scaling_grid_fixed_2024-09-13_21-16-29 --skip_copy_code
