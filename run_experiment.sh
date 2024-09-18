6e999785fba7c5076b8f90b021f7a11fa651651d
madragonse <madragonse@gmail.com>
2024-09-18 10:11:03 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/base_local_minimum/start_embedding_layer.yaml --git_branch=base_extrapolation_local_min_2024-09-18_10-11-03 --skip_copy_code
