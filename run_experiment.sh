1fc0e5a0d3d9703056074c74b229696dade4ef3f
madragonse <madragonse@gmail.com>
2024-09-18 12:46:41 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/520M/expansion_rate/32.yaml --git_branch=constrained_scaling_grid_fixed_2024-09-18_12-46-41 --skip_copy_code
