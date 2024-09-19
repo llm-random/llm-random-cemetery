46220ccb56d2ed7d6af29427e40c9ac0f02c14d5
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-09-19 18:17:23 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/large_extrapolation.yaml --git_branch=large_extrapolation_relative_tune_2024-09-19_18-17-23 --skip_copy_code
