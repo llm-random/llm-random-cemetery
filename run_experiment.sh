334b1442cb6621850c954dda6b223dafb627b387
madragonse <madragonse@gmail.com>
2024-09-19 18:46:36 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/base_local_minimum/start_gating_plus.yaml --git_branch=base_extrapolation_local_min_2024-09-19_18-46-36 --skip_copy_code
