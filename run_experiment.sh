f9a8b6332784f54aa33cc0e7cd9889dddc9dac82
Szymon Sadkowski <szysad108@gmail.com>
2024-09-19 15:18:04 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/grad_norm/std_norm_grid/small_c_lr_tune_grid_reduced_bn.yaml --git_branch=small_c_lr_tune_exp_0_post_attn_and_ff_layer_type_v1_lr_div100_2024-09-19_15-18-04 --skip_copy_code
