110583518fa1856e71d85f4440300cac44846921
localsearch <search@local.com>
2024-09-20 23:50:43 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_tuning_0_relative_lr_gating_ULGQI689.yaml --git_branch=local_search_relative_medium_tuning_0_relative_lr_gating_ULGQI689_2024-09-20_23-50-43 --skip_copy_code --skip_confirmation
