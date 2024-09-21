4f3bdf0504809110ab903cbbd35595b1e0439697
localsearch <search@local.com>
2024-09-21 22:50:36 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_tuning_0_init_scale_OWVVAJT7.yaml --git_branch=local_search_relative_medium_tuning_0_init_scale_OWVVAJT7_2024-09-21_22-50-36 --skip_copy_code --skip_confirmation
