481d1253c4895ed1c63ff12cac3f21fdc85980d5
localsearch <search@local.com>
2024-09-21 23:18:47 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_0_init_scale_WEV9MAEL.yaml --git_branch=local_search_relative_medium_0_init_scale_WEV9MAEL_2024-09-21_23-18-47 --skip_copy_code --skip_confirmation
