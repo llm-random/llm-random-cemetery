dee2f78a53371730c310a8aae1e90d4c667ebff1
localsearch <search@local.com>
2024-09-21 22:50:58 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_tuning_0_init_scale_8SGE8R7X.yaml --git_branch=local_search_relative_medium_tuning_0_init_scale_8SGE8R7X_2024-09-21_22-50-58 --skip_copy_code --skip_confirmation
