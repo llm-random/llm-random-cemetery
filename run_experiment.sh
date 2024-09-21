62be93d63bad002073897555d94fbd94d84c5c91
localsearch <search@local.com>
2024-09-21 09:14:53 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_1_load_balancing_loss_weight_W2MJUCFE.yaml --git_branch=local_search_relative_medium_1_load_balancing_loss_weight_W2MJUCFE_2024-09-21_09-14-53 --skip_copy_code --skip_confirmation
