6906eb3cda1c8d705a5d4d3a0e5e8c051d1a53b4
localsearch <search@local.com>
2024-09-22 13:48:12 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_2_weight_decay_VXPMHLSM.yaml --git_branch=local_search_relative_medium_2_weight_decay_VXPMHLSM_2024-09-22_13-48-12 --skip_copy_code --skip_confirmation
