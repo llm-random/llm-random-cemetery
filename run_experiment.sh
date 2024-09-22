3550572f32592c7d41df2b5552d229762ff190e7
localsearch <search@local.com>
2024-09-22 13:20:54 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_tuning_0_relative_scheduler_fraction_gating_GTI8N21C.yaml --git_branch=local_search_relative_medium_tuning_0_relative_scheduler_fraction_gating_GTI8N21C_2024-09-22_13-20-54 --skip_copy_code --skip_confirmation
