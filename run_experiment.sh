204a81035c84d4ea1ebfa5303b90a8da52d0064f
localsearch <search@local.com>
2024-09-22 01:22:56 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_1_learning_rate_UFEDE7LF.yaml --git_branch=local_search_relative_medium_1_learning_rate_UFEDE7LF_2024-09-22_01-22-56 --skip_copy_code --skip_confirmation
