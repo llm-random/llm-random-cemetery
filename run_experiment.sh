d68da5dc462a503defb1e8472e6f32fe1a706de3
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-09-22 04:13:10 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/base_extrapolation.yaml --git_branch=base_extrapolation_2024-09-22_04-13-10 --skip_copy_code --skip_confirmation
