73b1686068a23908ddb107293a67073c3d8ce49f
localsearch <search@local.com>
2024-09-23 10:04:59 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_tuning_0_relative_lr_gating_B30S63PB.yaml --git_branch=local_search_relative_medium_tuning_0_relative_lr_gating_B30S63PB_2024-09-23_10-04-59 --skip_copy_code --skip_confirmation
