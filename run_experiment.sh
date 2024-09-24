fc4fb69b6048bcd7b4fff0bfcb2701a60163b05c
madragonse <madragonse@gmail.com>
2024-09-24 17:51:27 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/u_plots/medium/gating_end.yaml --git_branch=medium_uplot_gating_end_2024-09-24_17-51-27 --skip_copy_code --skip_confirmation
