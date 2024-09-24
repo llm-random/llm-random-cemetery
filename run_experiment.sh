63237395a49cf0df93d4495f566fae2893e8fa9d
localsearch <search@local.com>
2024-09-24 10:49:42 +0000

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=local_search_configs/local_search_relative_medium_tuning_1_weight_decay_JRF1MYMP.yaml --git_branch=local_search_relative_medium_tuning_1_weight_decay_JRF1MYMP_2024-09-24_10-49-42 --skip_copy_code --skip_confirmation
