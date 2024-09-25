8a2b986ce05f9f2caa2af6ba73b69eaf5b7a5107
Maciej Stefaniak <madragonse@gmail.com>
2024-09-25 09:13:12 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/u_plots/base/gating_end.yaml --git_branch=base_uplot_gating_end_2024-09-25_09-13-12 --skip_copy_code --skip_confirmation
