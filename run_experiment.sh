3e0ee78a85154b148f3609d5b25f395d2afb6748
madragonse <madragonse@gmail.com>
2024-09-25 21:16:43 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/contribute/base/expert_if.yaml --git_branch=base_contribution_expertif_2024-09-25_21-16-43 --skip_copy_code --skip_confirmation
