31c37e67e3f218d8c9d6dc09c790341e6ab766e3
madragonse <madragonse@gmail.com>
2024-09-25 20:59:33 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/removal/base/gating.yaml --git_branch=base_removal_gating_2024-09-25_20-59-33 --skip_copy_code --skip_confirmation
