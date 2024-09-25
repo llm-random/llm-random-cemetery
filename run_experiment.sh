98f5774e3dfc69124022f04b396f80e3433bb5bd
Janek <jan.malasnicki@gmail.com>
2024-09-25 14:37:59 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/extrapolations/base_baseline.yaml --git_branch=test_athena_2024-09-25_14-37-59 --skip_copy_code --skip_confirmation
