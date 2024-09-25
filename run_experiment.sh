6f27276002987c5598861087ae57f6297bc88c24
madragonse <madragonse@gmail.com>
2024-09-25 20:57:43 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/removal/base/embedding.yaml --git_branch=base_removal_embedding_2024-09-25_20-57-43 --skip_copy_code --skip_confirmation
