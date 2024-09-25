90de098cded8cdd35d23a5bcfd970b7b5b8192ed
Maciej Stefaniak <madragonse@gmail.com>
2024-09-25 09:12:37 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/u_plots/base/expert_if_end.yaml --git_branch=base_uplot_expertif_end_2024-09-25_09-12-37 --skip_copy_code --skip_confirmation
