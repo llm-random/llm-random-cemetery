025491d70bd0ee19b75c5277f363fa3ee3b67b92
Szymon Sadkowski <szysad108@gmail.com>
2024-09-26 10:31:26 +0200

    Versioning code

--- generate_grid.py
+++ generate_grid.py
@@ -6,7 +6,7 @@ import yaml
 
 from lizrd.grid.prepare_configs import get_yaml_md5
 
-OUTPUT_FILE = "configs/experiments/grad_norm/std_norm_grid/small_c_lr_tune_grid_reduced_bn.yaml"
+OUTPUT_FILE = "configs/experiments/grad_norm/std_norm_grid/post_add_c_lr_grid_reduced_bn.yaml"
 BASELINE_INPUT = "configs/experiments/grad_norm/medium_reduced_bs.yaml"
 
 
@@ -25,43 +25,43 @@ BASELINE_GRAD_MODIF_PLACEMENT = (
     ["post_attn", "post_attn_norm", "post_attn_add", "post_ff", "post_ff_norm", "post_ff_add"],
     "all",
 )
-BASELINE_STD_NORM_MODIF_PARAMS = (["layer_type=v1", "c=0.0", "eps=0.0"], "baseline")
 
 LR_MULTIPLIERS: List[Tuple[float, str]] = [
     (1 / 100, "div100"),
-    (1 / 30, "div30"),
     (1 / 10, "div10"),
-    (1 / 3, "div3"),
     (1, "mul1"),
-    (3, "mul3"),
     (10, "mul10"),
+    (100, "mul100"),
 ]
 
-NAME_PREFIX = "small_c_lr_tune"
+C_GRID: List[Tuple[float, str]] = [
+    (0, "c_0"),
+    (1 / 5**5, "c_exp-5"),
+    (1 / 5**4, "c_exp-4"),
+    (1 / 5**3, "c_exp-3"),
+    (1 / 5**2, "c_exp-2"),
+    (1 / 5, "c_exp-1"),
+    (1, "c_exp0")
+]
+
+NAME_PREFIX = "post_add_c_lr_grid"
 
 
 def main():
     parent_md5_hash = get_yaml_md5(BASELINE_INPUT)
     configs = []
 
-    grad_configs = itertools.chain(
-        itertools.product(GRAD_MODIF_PLACEMENT_COMBINATIONS, STD_NORM_MODIF_PARAMS_1),
-        ((BASELINE_GRAD_MODIF_PLACEMENT, BASELINE_STD_NORM_MODIF_PARAMS),),
-    )
-
-    for i, (((grad_placement, tag1), (layer_type, tag2)), (lr_multiplier, lr_tag)) in enumerate(
-        itertools.product(grad_configs, LR_MULTIPLIERS)
-    ):
-        config_name = f"exp_{i}_{tag1}_{tag2}_lr_{lr_tag}"
+    for i, ((lr_multiplier, lr_tag), (c, c_tag)) in enumerate(itertools.product(LR_MULTIPLIERS, C_GRID)):
+        config_name = f"exp_{i}_lr_{lr_tag}_{c_tag}"
 
         config = {
             "parent": BASELINE_INPUT,
             "time": "0-04:00:00",
             "md5_parent_hash": parent_md5_hash,
             "params": {
-                "grad_modif_placement": grad_placement,
-                "grad_modif_params": layer_type,
-                "tags": [tag1, tag2, f"lr_{lr_tag}", "std_norm", "small_c_lr_tune", "grad_norm"],
+                "grad_modif_placement": ["post_add"],
+                "grad_modif_params": ["layer_type=v1", f"c={c}", "eps=1e-6"],
+                "tags": [f"lr_{lr_tag}", "std_norm", "post_add_c_lr_grid", "grad_norm", c_tag],
                 "name": f"{NAME_PREFIX}_{config_name}",
                 "grad_modif_type": "std_norm",
                 "learning_rate": 1e-4 * lr_multiplier,
@@ -70,6 +70,28 @@ def main():
         }
 
         configs.append(config)
+    
+    for i, (lr_mul, lr_tag) in enumerate(LR_MULTIPLIERS, start=len(C_GRID)*len(LR_MULTIPLIERS)):
+        config_name = f"exp_{i}_lr_{lr_tag}_baseline"
+
+        config = {
+            "parent": BASELINE_INPUT,
+            "time": "0-04:00:00",
+            "md5_parent_hash": parent_md5_hash,
+            "params": {
+                "grad_modif_placement": [],
+                "grad_modif_params": ["layer_type=v1", "c=99", "eps=1e-6"],
+                "tags": [f"lr_{lr_tag}", "std_norm", "post_add_c_lr_grid", "grad_norm", "true_baseline"],
+                "name": f"{NAME_PREFIX}_{config_name}",
+                "grad_modif_type": "std_norm",
+                "learning_rate": 1e-4 * lr_mul,
+                "n_steps": 2000,
+            },
+        }
+
+        configs.append(config)
+
+
 
     with open(OUTPUT_FILE, "w") as f:
         print(f"Writing to {OUTPUT_FILE}")
