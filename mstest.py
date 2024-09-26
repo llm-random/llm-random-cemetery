8fd20662208c23b6142e000dcc4a0a0322b945e8
madragonse <madragonse@gmail.com>
2024-09-26 17:30:25 +0200

    Versioning code

---
+++ mstest.py
@@ -0,0 +1,50 @@
+946387       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946388       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946389       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946390       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946391       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946392       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946393       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946394       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946395       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946396       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946397       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946398       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946400       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946401       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946402       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946403       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946404       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946405       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946406       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946407       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946408       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946409       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946410       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946411       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946412       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946413       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946414       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946415       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946419       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946420       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946462       base_extr+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946463       base_extr+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946464       base_extr+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946485       base_extr+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946486       base_extr+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946487       base_extr+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946488       relativit+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946490       relativit+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946493       relativit+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946521       relativit+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946523       relativit+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+946525       relativit+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+
++30
+
+946329       base_uplo+ plgrid-gp+ plgllmeff+         32     FAILED      1:0 
+946329.batch      batch            plgllmeff+         32     FAILED      1:0 
+
+946330       base_uplo+ plgrid-gp+ plgllmeff+         32     FAILED      1:0 
+946330.batch      batch            plgllmeff+         32     FAILED      1:0 
