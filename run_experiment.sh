dd0e64c66e0e538a721d4c03c2a330bad4d92877
Maciej Pióro <m@ciekpio.ro>
2024-09-26 14:14:06 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relative_lr/sensitivity/medium_single_rerun.yaml --git_branch=relative_medium_sensitivity_2024-09-26_14-14-06 --skip_copy_code --skip_confirmation
