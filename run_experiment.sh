ebbeec7f6369eb52ef5e5ed6a2eb9f4084305d63
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-09-26 15:51:25 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/medium_validation_baseline.yaml --git_branch=medium_validation_baseline_2024-09-26_15-51-25 --skip_copy_code --skip_confirmation
