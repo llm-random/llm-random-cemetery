80f9db186740bc65c0d6e81a4b31f3da04e754a5
Maciej Stefaniak <madragonse@gmail.com>
2024-09-26 10:56:57 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/local_search/baseline.yaml --git_branch=relativity_medium_baseline_2024-09-26_10-56-57 --skip_copy_code --skip_confirmation
