148e15c4a1f7434cd017b9387eec4b3f32ae73a3
madragonse <madragonse@gmail.com>
2024-09-27 16:22:46 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/49M/debug_chungus.yaml --git_branch=debug_chungization_2024-09-27_16-22-46 --skip_copy_code
