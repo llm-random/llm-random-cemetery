114ed0d22f4cf06e8f50f53ca8edfc8e9cc68917
Janek <jan.malasnicki@gmail.com>
2024-09-29 03:00:23 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/dense/medium_baseline.yaml --git_branch=medium_dense_baseline_2024-09-29_03-00-23 --skip_copy_code --skip_confirmation
