520d2c9433266087d66209eb09e58a4d06a4305d
madragonse <madragonse@gmail.com>
2024-09-30 18:29:39 +0200

    Versioning code

---
+++ mstest.py
@@ -0,0 +1,185 @@
+955125       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955126       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955127       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955128       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955129       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955130       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955131       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955132       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955133       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955134       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955135       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955136       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955137       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955138       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955139       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955140       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955141       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955142       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955143       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955144       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955145       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955146       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955147       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955148       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955150       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955151       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955152       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955153       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955154       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955155       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955156       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955157       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955158       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955159       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955160       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955161       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955162       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955163       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955164       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955165       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955166       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955167       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955168       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955169       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955170       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955171       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955172       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955173       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955174       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955175       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955176       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955177       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955178       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955179       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955181       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955182       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955183       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955184       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955185       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955186       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955187       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955188       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955189       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955190       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955191       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955192       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955193       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955194       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955195       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955196       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955197       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955198       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955199       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955200       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955201       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955202       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955203       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955204       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955205       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955206       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955207       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955208       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955209       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955210       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955212       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955213       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955214       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955215       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955216       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955217       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955218       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955219       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955220       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955221       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955222       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955223       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955224       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955225       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955226       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955227       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955228       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955229       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955230       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955231       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955232       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955233       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955234       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955235       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955236       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955237       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955238       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955239       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955240       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955241       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955242       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955243       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955244       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955245       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955246       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0 
+955247       base_uplo+ plgrid-gp+ plgllmeff+          0    PENDING      0:0
+
+
+955148       base_uplo+ plgrid-gp+ plgllmeff+         32     FAILED      1:0 /net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_expertif_end_2024-09-27_14-09-52 LLMRANDOM-15909
+
+955160       base_uplo+ plgrid-gp+ plgllmeff+         32     FAILED      1:0 /net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-27_14-08-05 LLMRANDOM-15896
+
+
+
+
+946329       base_uplo+ plgrid-gp+ plgllmeff+         32     FAILED      1:0 
+
+
+946330       base_uplo+ plgrid-gp+ plgllmeff+         32     FAILED      1:0 
+edge case - błąd z zapisywaniem wykresu, błąd ścieżki
+
+
+948438       base_uplo+ plgrid-gp+ plgllmeff+         32     FAILED      1:0
+edge case - błąd z zapisywaniem wykresu, błąd ścieżki
+
+Shutting down background jobs, please wait a moment...
+Done!
+Waiting for the remaining 1 operations to synchronize with Neptune. Do not kill this process.
+All 1 operations synced, thanks for waiting!
+Explore the metadata in the Neptune app:
+https://app.neptune.ai/pmtest/llm-random/e/LLMRANDOM-15709/metadata
+Traceback (most recent call last):
+  File "/usr/lib/python3.10/runpy.py", line 196, in _run_module_as_main
+    return _run_code(code, main_globals, None,
+  File "/usr/lib/python3.10/runpy.py", line 86, in _run_code
+    exec(code, run_globals)
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/research/conditional/train/cc_train.py", line 494, in <module>
+    mp.spawn(
+  File "/opt/venv/lib/python3.10/site-packages/torch/multiprocessing/spawn.py", line 246, in spawn
+    return start_processes(fn, args, nprocs, join, daemon, start_method="spawn")
+  File "/opt/venv/lib/python3.10/site-packages/torch/multiprocessing/spawn.py", line 202, in start_processes
+    while not context.join():
+  File "/opt/venv/lib/python3.10/site-packages/torch/multiprocessing/spawn.py", line 163, in join
+    raise ProcessRaisedException(msg, error_index, failed_process.pid)
+torch.multiprocessing.spawn.ProcessRaisedException: 
+
+-- Process 0 terminated with the following error:
+OSError: [Errno 5] Input/output error
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/opt/venv/lib/python3.10/site-packages/torch/multiprocessing/spawn.py", line 74, in _wrap
+    fn(i, *args)
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/research/conditional/train/cc_train.py", line 472, in main
+    trainer.train(args.n_steps)
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/research/conditional/utils/conditional_trainer.py", line 148, in train
+    self._train_step(step)
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/research/conditional/utils/conditional_trainer.py", line 185, in _train_step
+    self.layer_manager.log(step)
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/research/conditional/utils/layer_manager.py", line 89, in log
+    self.logger.report_generic_info(
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/lizrd/support/logging.py", line 416, in report_generic_info
+    logger.report_generic_info(title=title, iteration=iteration, data=data)
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/lizrd/support/logging.py", line 205, in report_generic_info
+    self.report_plotly(figure=data, title=title, iteration=iteration)
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/lizrd/support/logging.py", line 268, in report_plotly
+    self._upload_with_tmp_file(f"{directory}/plot_{filename}", html, "html")
+  File "/net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/llm_random_cemetery/base_uplot_embedding_start_2024-09-26_10-46-05/lizrd/support/logging.py", line 199, in _upload_with_tmp_file
+    with open(tmp_file, "w") as f:
+OSError: [Errno 5] Input/output error
