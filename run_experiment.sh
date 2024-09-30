30672aa238ec2000c5db63424558f1f5fe2fac4e
madragonse <madragonse@gmail.com>
2024-09-30 18:30:34 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/lr_grid/base_relative.yaml --git_branch=base_lr_grid_relative_2024-09-30_18-30-34 --skip_copy_code --skip_confirmation
