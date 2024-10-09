d53c9881a062b63c4a92f206d724e3ad53340ee4
madragonse <madragonse@gmail.com>
2024-10-09 14:25:06 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/u_plots/medium/head_end.yaml --git_branch=medium_uplot_head_end_2024-10-09_14-25-06 --skip_copy_code --skip_confirmation
