8eda43f9054c0fa42a65d53ff96213a0698c93d5
madragonse <madragonse@gmail.com>
2024-10-09 14:15:08 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/removal/medium/head.yaml --git_branch=medium_removal_head_2024-10-09_14-15-08 --skip_copy_code --skip_confirmation
