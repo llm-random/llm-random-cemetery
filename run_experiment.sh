b71cdbdabe19ee3027311e82dfced71a470a90b3
madragonse <madragonse@gmail.com>
2024-10-09 14:23:52 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/u_plots/medium/expert_if_end.yaml --git_branch=medium_uplot_expertif_end_2024-10-09_14-23-52 --skip_copy_code --skip_confirmation
