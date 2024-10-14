c2335fc462da994f3e97aaf8ada00de513f0cf91
Maciej Pióro <m@ciekpio.ro>
2024-10-14 17:12:33 +0200

    Versioning code

--- lizrd/scripts/download_c4/download_c4.sh
+++ lizrd/scripts/download_c4/download_c4.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=plgrid-gpu-a100
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --time=24:00:00
 #SBATCH --mem=125G
 #SBATCH --gres=gpu:0
