edc112f4e428b30fd4ccb6e56abd1307c5ee5c08
Maciej Stefaniak <madragonse@gmail.com>
2024-10-15 00:00:43 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/u_plots/base/projection_start.yaml --git_branch=base_uplot_projection_start_2024-10-15_00-00-43 --skip_copy_code --skip_confirmation
