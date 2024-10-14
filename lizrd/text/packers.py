1e0882e4429c8b896bbd09d008424bd44ad99a05
Maciej Pióro <m@ciekpio.ro>
2024-10-14 17:10:31 +0200

    Versioning code

--- lizrd/text/packers.py
+++ lizrd/text/packers.py
@@ -171,7 +171,7 @@ class BERTPacker(
         )
 
 
-class GPTPacker(
+class LegacyGPTPacker(
     AbstractPacker,
 ):
     def __init__(
@@ -216,3 +216,51 @@ class GPTPacker(
         calculate_loss = [1] * len(target_ids)
 
         return LLMExample(input_ids, target_ids, calculate_loss)
+
+
+class GPTPacker(
+    AbstractPacker,
+):
+    def __init__(
+        self,
+        sequence_length: int,
+        dataset_maker: AbstractDataset,
+        tokenizer_maker: Callable[[], AbstractTokenizer],
+        seed: Optional[int] = None,
+    ):
+        super().__init__(
+            sequence_length,
+            dataset_maker,
+            tokenizer_maker,
+            seed=seed,
+        )
+        self.inputs = None
+        self.targets = None
+
+    def get_sample(self) -> LLMExample:
+        """
+        Sample examples from the dataset until we reach the desired sequence length.
+        """
+        eot_id = self.tokenizer.eot_id
+        assert eot_id is not None
+
+        if self.inputs is None:
+            self.inputs = [eot_id]
+            self.targets = []
+
+        while len(self.targets) < self.sequence_length:
+            document = self.dataset.get_document()
+            tokens = self.tokenizer.text_to_ids(document)
+            self.inputs.extend(tokens)
+            self.targets.extend(tokens)
+
+        res_inputs = self.inputs[: self.sequence_length]
+        res_targets = self.targets[: self.sequence_length]
+        self.inputs = self.inputs[self.sequence_length :]
+        self.targets = self.targets[self.sequence_length :]
+
+        return LLMExample(
+            input_ids=res_inputs,
+            target_ids=res_targets,
+            should_calculate_loss=[1] * len(res_inputs),
+        )
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/baselines/gpt/dense/base.yaml --git_branch=baseline_2024-10-14_17-10-31 --skip_copy_code
