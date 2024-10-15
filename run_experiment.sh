fb1daa47444974f4b6d92830d6732dc9f388b038
Janek <jan.malasnicki@gmail.com>
2024-10-15 11:43:16 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/dense/large_relative.yaml --git_branch=large_dense_relative_2024-10-15_11-43-16 --skip_copy_code --skip_confirmation
