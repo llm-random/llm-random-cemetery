26f8e58fa444d70b3fb9aaf5fc44881f288f7010
Janek <jan.malasnicki@gmail.com>
2024-10-24 10:29:58 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/ablations/u_plots/base/projection_start.yaml --git_branch=base_uplot_projection_start_2024-10-24_10-29-58 --skip_copy_code --skip_confirmation
