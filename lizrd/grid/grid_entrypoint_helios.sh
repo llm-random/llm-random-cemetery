7736ab0c824079512631023eec6c857f579dec79
Maciej Pióro <m@ciekpio.ro>
2024-10-28 10:59:32 +0100

    Versioning code

---
+++ lizrd/grid/grid_entrypoint_helios.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+conda deactivate
+module load ML-bundle/24.06a
+source /net/storage/pr3/plgrid/plggllmeffi/datasets/make_singularity_image/venv/bin/activate
+echo "Will run the following command:"
+echo "$@"
+echo "==============================="
+$@
