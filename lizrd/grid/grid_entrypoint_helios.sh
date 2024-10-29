a3ee7f9165fcb46513c5c92040aa7142ed16efa5
Maciej Pióro <m@ciekpio.ro>
2024-10-29 10:37:41 +0100

    Versioning code

--- lizrd/grid/grid_entrypoint_helios.sh
+++ lizrd/grid/grid_entrypoint_helios.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -l
 
+export OMP_NUM_THREADS=8
 module load ML-bundle/24.06a
+export OMP_NUM_THREADS=8
 source /net/storage/pr3/plgrid/plggllmeffi/datasets/make_singularity_image/venv/bin/activate
 echo "Will run the following command:"
 echo "$@"
