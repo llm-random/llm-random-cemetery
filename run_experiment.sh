45141e675fe83a619944870264756db34c183c5f
Janek <jan.malasnicki@gmail.com>
2024-11-08 15:54:41 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/extrapolations/jm_check_pr.yaml --git_branch=check_relativity_pr_2024-11-08_15-54-41 --skip_copy_code
