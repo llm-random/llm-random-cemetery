fa5169efddf946f1906a35780f21fea96f2bb626
Maciej Pióro <m@ciekpio.ro>
2024-11-14 11:46:02 +0100

    Versioning code

---
+++ research/dummy_backends_project/utils/argparse.py
@@ -0,0 +1,8 @@
+import argparse
+
+
+def introduce_parser_arguments(
+    parser: argparse.ArgumentParser,
+) -> argparse.ArgumentParser:
+    parser.add_argument("rest", nargs=argparse.REMAINDER)
+    return parser
