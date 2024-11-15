45d2975ce06fb1b30b8c664f76c388c49219a376
Maciej Pióro <m@ciekpio.ro>
2024-11-15 11:12:54 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/private/attention_moe/15.11_rewrite_momqa_flash_capacity.yaml --git_branch=rewrite_momqa_2024-11-15_11-12-54 --skip_copy_code
