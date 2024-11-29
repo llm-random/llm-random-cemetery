6b119e66f9d11258006e2a32600eb0773be19c56
madragonse <madragonse@gmail.com>
2024-11-29 13:46:19 +0100

    Versioning code

--- research/conditional/utils/conditional_trainer.py
+++ research/conditional/utils/conditional_trainer.py
@@ -100,20 +100,24 @@ class ConditionalTrainer:
     get_final_eval_dataloader: Optional[Callable[..., DataloaderWrapper]] = None
     final_eval_dataloader_batch_size: Optional[int] = None
     n_final_eval_batches: int = None
+    accumulators: dict = None
 
     def __attrs_post_init__(self):
         if self.mixed_precision_dtype == torch.float16:
             self.scaler = torch.cuda.amp.GradScaler(enabled=self.mixed_precision)
-        self.loss_accumulators = {
-            f"loss_interval/{i}": SN(acc=0.0, interval=i)
-            for i in self.loss_log_intervals
-        }
-        self.loss_accumulators["loss"] = SN(
-            acc=0.0, interval=self.logging_interval_loss
-        )
-        self.correct_tokens_accumulator = 0.0
-        self.total_tokens_accumulator = 0.0
-        self.auxiliary_losses_accumulator = dict()
+        if self.accumulators:
+            ...
+        else:
+            self.loss_accumulators = { #dev
+                f"loss_interval/{i}": SN(acc=0.0, interval=i)
+                for i in self.loss_log_intervals
+            }
+            self.loss_accumulators["loss"] = SN(
+                acc=0.0, interval=self.logging_interval_loss
+            )
+            self.correct_tokens_accumulator = 0.0
+            self.total_tokens_accumulator = 0.0
+            self.auxiliary_losses_accumulator = dict() #dev
         self._calculate_loss_and_gradient = make_loss_and_gradient_function(
             loss_checkpoint_chungs=self.loss_checkpoint_chungs,
         )
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/feature_examples/checkpoint_manager.yaml --git_branch=checkpoint_manager_example_2024-11-29_13-46-19 --skip_copy_code
