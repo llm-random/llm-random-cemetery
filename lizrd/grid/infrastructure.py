b9ff3a2caab49fcd247634ff61947dce996bca18
madragonse <madragonse@gmail.com>
2024-11-29 14:12:12 +0100

    Versioning code

--- lizrd/grid/infrastructure.py
+++ lizrd/grid/infrastructure.py
@@ -255,7 +255,8 @@ class IdeasBackend(MachineBackend):
 
 
 class EntropyBackend(MachineBackend):
-    max_exp_time = 14 * 24 * 60 * 60
+    # max_exp_time = 14 * 24 * 60 * 60 #dev
+    max_exp_time = 30 * 60 #dev
 
     def get_common_directory(self) -> str:
         return "/home/jkrajewski_a100"
@@ -264,7 +265,7 @@ class EntropyBackend(MachineBackend):
         return "/local_storage_2/dataset_cache"
 
     def get_grid_entrypoint(self) -> str:
-        return "lizrd/grid/grid_entrypoint.sh"
+        return "lizrd/grid/grid_entrypoint_entropy.sh"
 
     def get_default_train_dataset_path(self, dataset_type: str):
         if dataset_type == "c4":
@@ -298,14 +299,15 @@ class EntropyBackend(MachineBackend):
             f"--job-name={training_args['name']}",
             f"--time={setup_args['time']}",
             f"{setup_args['grid_entrypoint']}",
-            "singularity",
-            "run",
-            *singularity_env_arguments,
-            make_singularity_mount_paths(setup_args, training_args),
-            "--nv",
-            setup_args["singularity_image"],
+            # "singularity", # dev TODO temporary solution
+            # "run", 
+            # *singularity_env_arguments,
+            # make_singularity_mount_paths(setup_args, training_args),
+            # "--nv",
+            # setup_args["singularity_image"],
             *self.get_runner_command(setup_args["runner"], runner_params),
         ]
+
 
 
 class WriterBackend(MachineBackend):
