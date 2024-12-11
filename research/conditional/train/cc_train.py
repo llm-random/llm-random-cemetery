c5a36ae2748300067bfe5b6de76fabd64eb35cde
madragonse <madragonse@gmail.com>
2024-12-11 14:22:34 +0100

    Versioning code

--- research/conditional/train/cc_train.py
+++ research/conditional/train/cc_train.py
@@ -8,7 +8,7 @@ import socket
 
 import torch
 import torch.multiprocessing as mp
-from torch.distributed import init_process_group, destroy_process_group
+from torch.distributed import init_process_group, destroy_process_group, barrier
 from ast import literal_eval
 
 from lizrd.core import misc
@@ -369,6 +369,8 @@ def main(
             }
             for ff_fun in ff_layer_funs
         ]
+    print("---------------------------------------------------------------------------")
+    print("Main process before checkpoint manager assensment last barrier")
 
     checkpoint_path = args.load_weights_path
     if not args.checkpoint_manager:
@@ -384,6 +386,10 @@ def main(
         checkpoint = (
             get_checkpoint_from_path(checkpoint_path) if checkpoint_path else None
         )
+    print("Main process at last barrier")
+    barrier()
+    print("Main process after last barrier")
+    print("---------------------------------------------------------------------------")
 
     model = get_model(
         max_length=args.cutoff,
@@ -606,6 +612,7 @@ def main(
     )
     trainer.train(args.n_steps)
 
+    barrier()
     if rank is not None:
         destroy_process_group()
 
