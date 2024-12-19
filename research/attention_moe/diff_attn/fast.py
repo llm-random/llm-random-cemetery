0ded983adf0d21da086562ea09b42f9cdd668479
Maciej Pióro <m@ciekpio.ro>
2024-12-19 14:06:18 +0100

    Versioning code

--- research/attention_moe/diff_attn/fast.py
+++ research/attention_moe/diff_attn/fast.py
@@ -108,6 +108,32 @@ class MultiheadFlashDiff1(nn.Module):
                 init_scale,
                 output_dim=2 * self.head_dim * self.num_kv_heads,
             )
+        elif self.adapter_type == "additive":
+            self.k_delta = nn.Parameter(
+                torch.zeros(
+                    2 * self.head_dim * self.num_kv_heads, dtype=torch.float32
+                ).normal_(mean=0, std=0.1)
+            )
+            self.q_delta = nn.Parameter(
+                torch.zeros(self.embed_dim, dtype=torch.float32).normal_(
+                    mean=0, std=0.1
+                )
+            )
+        elif self.adapter_type == "multiplicative":
+            self.k_delta = nn.Parameter(
+                torch.zeros(
+                    2 * self.head_dim * self.num_kv_heads, dtype=torch.float32
+                ).normal_(mean=1, std=0.1)
+            )
+            self.q_delta = nn.Parameter(
+                torch.zeros(self.embed_dim, dtype=torch.float32).normal_(
+                    mean=1, std=0.1
+                )
+            )
+        elif self.adapter_type == "none":
+            pass
+        else:
+            raise NotImplementedError
 
         self.scaling = self.head_dim**-0.5
 
@@ -191,17 +217,25 @@ class MultiheadFlashDiff1(nn.Module):
             k = k.view(bsz, src_len, self.num_kv_heads, 2 * self.head_dim)
             v = v.view(bsz, src_len, self.num_kv_heads, 2 * self.head_dim)
         elif self.adapter_type == "additive":
+            q_negative = (q + self.q_delta).view(
+                bsz, tgt_len, self.num_heads, 2 * self.head_dim
+            )
+            k_negative = (k + self.k_delta).view(
+                bsz, src_len, self.num_kv_heads, 2 * self.head_dim
+            )
             q = q.view(bsz, tgt_len, self.num_heads, 2 * self.head_dim)
             k = k.view(bsz, src_len, self.num_kv_heads, 2 * self.head_dim)
             v = v.view(bsz, src_len, self.num_kv_heads, 2 * self.head_dim)
-            k_negative = k.clone()
-            q_negative = q + self.q_delta
         elif self.adapter_type == "multiplicative":
+            q_negative = (q * self.q_delta).view(
+                bsz, tgt_len, self.num_heads, 2 * self.head_dim
+            )
+            k_negative = (k * self.k_delta).view(
+                bsz, src_len, self.num_kv_heads, 2 * self.head_dim
+            )
             q = q.view(bsz, tgt_len, self.num_heads, 2 * self.head_dim)
             k = k.view(bsz, src_len, self.num_kv_heads, 2 * self.head_dim)
             v = v.view(bsz, src_len, self.num_kv_heads, 2 * self.head_dim)
-            k_negative = k.clone()
-            q_negative = q * self.q_delta
         elif self.adapter_type == "identity":
             q = q.view(bsz, tgt_len, self.num_heads, 2 * self.head_dim)
             k = k.view(bsz, src_len, self.num_kv_heads, 2 * self.head_dim)
