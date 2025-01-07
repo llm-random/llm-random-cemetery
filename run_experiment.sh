4ceb04c34e6588f22008e203f4c89ad541c54d09
Jakub <gim.jakubk@gmail.com>
2025-01-07 10:13:27 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/130M/expansion_rate/8.yaml --git_branch=constrained_scaling_grid_21_11_2025-01-07_10-13-27 --skip_copy_code
