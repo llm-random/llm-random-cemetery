a0bc92cde56a842f4e480d4e77c3d7f7c8352f0b
Maciej Pióro <m@ciekpio.ro>
2025-01-09 19:34:25 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/7B_total/expansion_rate/8.yaml --git_branch=constrained_scaling_grid_21_11_2025-01-09_19-34-25 --skip_copy_code
