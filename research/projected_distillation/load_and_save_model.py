50b62f49120dd77b715ef9b491647590198f1874
madragonse <madragonse@gmail.com>
2025-01-16 15:21:43 +0100

    Versioning code

---
+++ research/projected_distillation/load_and_save_model.py
@@ -0,0 +1,13 @@
+import torch
+
+
+def load_projected_weights(model:torch.nn.Module, projected_weights):
+    print("----------------replace with new values----------------") #dev
+    for name, params in model.named_parameters():
+        prj_params = projected_weights.get(name)
+        if prj_params:
+            print(name) #dev
+            params.data.copy_(prj_params)
+    print("--------------end------------------") #dev
+
+    
