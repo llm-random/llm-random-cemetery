707881491546817210309041ae2b3c24b72ef501
Maciej Pióro <m@ciekpio.ro>
2025-01-17 11:14:36 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/eval/370M/expansion_rate/32.yaml --git_branch=constrained_scaling_grid_21_11_2025-01-17_11-14-36 --skip_copy_code
