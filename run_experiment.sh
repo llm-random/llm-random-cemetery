9b302fd0e80661890f3b1d1708afec0baf417ebf
Jakub <gim.jakubk@gmail.com>
2025-01-19 13:52:50 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/parabola_2/expansion_rate/1.yaml --git_branch=constrained_scaling_grid_21_11_2025-01-19_13-52-50 --skip_copy_code
