aea8b3f95c81bef6832c79694b56a81f564f2baa
Maciej Stefaniak <madragonse@gmail.com>
2025-01-19 12:05:16 +0100

    Versioning code

--- research/projected_distillation/utils.py
+++ research/projected_distillation/utils.py
@@ -6,11 +6,32 @@ FREEZE_PARAMS_REGULES = [
 
     ".block.residual_attention.layer.attention.input_projection.input_projection.weight", #ATT
     ".block.residual_attention.layer.attention.output_projection.output_projection.weight",
+
+    "embedding_layer.layers.0.embedding.weight",
+
+    # "embedding_layer._checkpoint_wrapped_module.layers.0.embedding.weight",
+    # "",
+    # "",
 ]
 
 def freeze_projected_params(model):
     for name, param in model.named_parameters():
+        print(name) #dev
         if any([reg in name for reg in FREEZE_PARAMS_REGULES]):  # Check if the parameter belongs to layer1
             param.requires_grad = False
     return model 
 
+
+# PROJECTIONS_1_1 = [
+#     ""
+# ]
+
+# PROJECTIONS_1_4 = [
+#     ""
+# ]
+
+# PROJECTIONS_1_3 = [
+#     ""
+# ]
+
+# def initialize_projectors(model):
