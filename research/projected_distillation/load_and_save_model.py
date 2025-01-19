d8392d135dfb6293a444a78169de2d7d8fffcea8
Maciej Stefaniak <madragonse@gmail.com>
2025-01-19 12:16:22 +0100

    Versioning code

--- research/projected_distillation/load_and_save_model.py
+++ research/projected_distillation/load_and_save_model.py
@@ -6,25 +6,30 @@ TRANSFER_PARAMS = [
 
     ".block.residual_attention.layer.attention.input_projection.weight", #ATT
     ".block.residual_attention.layer.attention.output_projection.weight",
+
+    "embedding_layer.layers.0.embedding.weight",
+    # "head._checkpoint_wrapped_module.weight",
 ]
 
 CAST_PROJECTED_PARAMS_NAME_PARTS = [
     (".output_projection.output_projection.", ".output_projection."),
-    (".input_projection.input_projection.", ".input_projection.")
+    (".input_projection.input_projection.", ".input_projection."),
+    ("embedding_layer.layers.0.embedding.weight", "embedding_layer.layers.0.weight"),
+    #("head.weight", "#dev"),
 ]
 
 
 def load_projected_weights(model:torch.nn.Module, projected_weights):
     # print(list(projected_weights["model"].keys())) #dev
-    # print("----------------replace with new values----------------") #dev
+    print("------------------------------replace with new values------------------------") #dev
     for name, params in model.named_parameters():
         for e in CAST_PROJECTED_PARAMS_NAME_PARTS:
             if e[0] in name:
                 name = name.replace(e[0], e[1])
-                # print("replaced name", name) #dev
+                print("replaced name", name) #dev
         prj_params = projected_weights.get(name)
         # print(name, type(prj_params))
         if (prj_params is not None) and any([reg in name for reg in TRANSFER_PARAMS]):
             print(name) #dev
             params.data.copy_(prj_params)
-    print("--------------end------------------") #dev
+    print("------------------------------replace with new values end------------------------") #dev
