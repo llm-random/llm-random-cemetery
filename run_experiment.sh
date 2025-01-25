a53894a522c3f28d8ffa2f3ce5cf9f4f39b8b135
Maciej Pióro <m@ciekpio.ro>
2025-01-25 18:55:40 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/eval/parabolae/ad_hoc.yaml --git_branch=constrained_scaling_grid_21_11_2025-01-25_18-55-39 --skip_copy_code
