479923c08d48601e6d52057acd1871ceb6fc454f
Janek <jan.malasnicki@gmail.com>
2025-01-25 17:55:14 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/relativity_paper/local_search/relative.yaml --git_branch=relativity_relative_verify_merge_2025-01-25_17-55-14 --skip_copy_code
