1d877575733aa079dcb5283c855da3d4b3768cec
madragonse <madragonse@gmail.com>
2025-01-30 16:22:56 +0100

    Versioning code

--- research/projected_distillation/utils.py
+++ research/projected_distillation/utils.py
@@ -110,6 +110,12 @@ def initialize_projections(model:torch.nn.Module, dmodel:int, projected_dmodel:i
     projection_3 = torch.concat((projection, projection, projection))
     projection_3 = torch.concat((projection_3, projection_3, projection_3), dim=1)
     projection_3.shape
+    # projection_z = torch.zeros((projected_dmodel, dmodel)) #dev
+    # projection_3 = torch.concat((
+    #     torch.concat((projection, projection_z, projection_z), dim=0),
+    #     torch.concat((projection_z, projection, projection_z), dim=0),
+    #     torch.concat((projection_z, projection_z, projection), dim=0),
+    #     ), dim=1)
 
     if diagonal:
         projection_z = torch.zeros((projected_dmodel, dmodel))
