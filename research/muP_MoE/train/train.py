745fa99aa03f50a60b76c36e1efe56ee75fff341
Janek <jan.malasnicki@gmail.com>
2025-02-24 15:05:50 +0100

    Versioning code

--- research/muP_MoE/train/train.py
+++ research/muP_MoE/train/train.py
@@ -89,12 +89,14 @@ def get_muP_learning_rates(args, model, m_d=1.0):
 
     key_lr_dict = {
         "embedding_layer": 1.0,
-        "input_projection": (1 / m_d),  # Attn Q, K, V
-        "output_projection": (1 / m_d),  # Attn O
+        # "input_projection": (1 / m_d),  # Attn Q, K, V
+        # "output_projection": (1 / m_d),  # Attn O
+        "residual_attention.layer.attention" : (1 / m_d),
         "lin1_weight": (1 / m_d),  # FF in
         "lin2_weight": (1 / m_d),  # FF out
         "pre_relu": (1 / m_d),  # FF in, ver2
         "post_relu": (1 / m_d),  # FF out, ver2
+        "expert_inner_function": (1 / m_d), # FF in MoE
         "head": 1,
         "gating": 1,
     }
@@ -108,49 +110,51 @@ def get_muP_learning_rates(args, model, m_d=1.0):
                 break
         print(f"Assigning lr ratio {ratio} to {name}")
         ratio_to_params[ratio].append(param)
-    param_grops = [
+    param_groups = [
         {"params": params, "lr": ratio * lr, "lr_ratio": ratio}
         for ratio, params in ratio_to_params.items()
     ]
-    return param_grops
+    return param_groups
 
 
+#TODO change init to be applied on module initialization
 def apply_muP_init(args, model, init_base_value=1.0, m_d=1.0, n_blocks=1.0):
+    pass
     # check if the model isn't loaded from checkpoint
-    if args.load_weights_path is not None:
-        return
+    # if args.load_weights_path is not None:
+    #     return
 
-    key_init_dict = {
-        "embedding_layer": 1.0,
-        "input_projection": (1 / m_d),
-        "output_projection": (1 / (m_d * 2 * n_blocks)),
-        "lin1_weight": (1 / m_d),
-        "lin2_weight": (1 / (m_d * 2 * n_blocks)),
-        "pre_relu": (1 / m_d),  # FF in, ver2
-        "post_relu": (1 / (m_d * 2 * n_blocks)),  # FF out, ver2
-        "head": 1.0,
-        "gating": 1,
-    }
-    for name, param in model.named_parameters():
-        # check for not implemented FFs
-        if "expert_inner_function" in name:
-            assert any(
-                substring in name
-                for substring in {"lin1_weight", "lin2_weight", "gate_weight"}
-            )
+    # key_init_dict = {
+    #     "embedding_layer": 1.0,
+    #     "input_projection": (1 / m_d),
+    #     "output_projection": (1 / (m_d * 2 * n_blocks)),
+    #     "lin1_weight": (1 / m_d),
+    #     "lin2_weight": (1 / (m_d * 2 * n_blocks)),
+    #     "pre_relu": (1 / m_d),  # FF in, ver2
+    #     "post_relu": (1 / (m_d * 2 * n_blocks)),  # FF out, ver2
+    #     "head": 1.0,
+    #     "gating": 1,
+    # }
+    # for name, param in model.named_parameters():
+    # #     # check for not implemented FFs
+    # #     if "expert_inner_function" in name:
+    # #         assert any(
+    # #             substring in name
+    # #             for substring in {"lin1_weight", "lin2_weight", "gate_weight"}
+    # #         )
 
-        scale = 1.0
-        for keyword in key_init_dict.keys():
-            if keyword in name:
-                scale = key_init_dict[keyword]
-                break
-        # we don't want to initialize normalization layers, those have their own initialization
-        if "norm" not in name:
-            print(f"Initializing {name} with scale {scale}")
-            print(f"Resulting std: {(init_base_value * scale) ** 0.5}")
-            torch.nn.init.normal_(
-                param.data, mean=0.0, std=(init_base_value * scale) ** 0.5
-            )
+    #     scale = 1.0
+    #     for keyword in key_init_dict.keys():
+    #         if keyword in name:
+    #             scale = key_init_dict[keyword]
+    #             break
+    #     # we don't want to initialize normalization layers, those have their own initialization
+    #     if "norm" not in name:
+    #         print(f"Initializing {name} with scale {scale}")
+    #         print(f"Resulting std: {(init_base_value * scale) ** 0.5}")
+    #         torch.nn.init.normal_(
+    #             param.data, mean=0.0, std=(init_base_value * scale) ** 0.5
+    #         )
 
 
 def main(
@@ -307,8 +311,9 @@ def main(
         model = torch.compile(model)
 
     if args.print_parameter_names:
+        print('----------print_parameter_names----------')
         for name, param in model.named_parameters():
-            print(name, param.shape)
+            print(f'name: {name},\tshape: {param.shape}')
 
     # muP innit
     if args.mup_params is not None:
@@ -323,10 +328,10 @@ def main(
             m_d=m_d,
             n_blocks=args.n_blocks,
         )
-    param_grops = get_muP_learning_rates(args, model, m_d=m_d)
+    param_groups = get_muP_learning_rates(args, model, m_d=m_d)
 
     optimizer = torch.optim.AdamW(
-        param_grops,
+        param_groups,
         lr=args.learning_rate,
         weight_decay=args.weight_decay,
         betas=(args.adam_beta1, args.adam_beta2),
