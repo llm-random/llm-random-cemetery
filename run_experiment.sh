3b469b7498e5126e048d588dbab26ec89a2f297c
Janek <jan.malasnicki@gmail.com>
2025-02-25 13:24:48 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/muP_MoE/configs/10_step_dense.yaml --git_branch=debug_muP_MoE_10_step_new_fix_blog_fsdp_2025-02-25_13-24-48 --skip_copy_code
