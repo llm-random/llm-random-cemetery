7fb4ea99203bc139154e7fa230e420be238fa938
Mikołaj Dziok <m1kush@disroot.org>
2025-03-05 14:30:59 +0100

    Versioning code

--- research/attention_moe/backends.py
+++ research/attention_moe/backends.py
@@ -191,7 +191,7 @@ class EntropyBackend(MachineBackend):
     max_exp_time = 14 * 24 * 60 * 60
 
     def get_common_directory(self) -> str:
-        return "/home/jkrajewski_a100"
+        return "/storage_nvme_1/llm-random/"
 
     def get_cache_path(self) -> str:
         return "/storage_nvme_1/llm-random/cache/"
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/better_differential_transformer_dora1.yaml --git_branch=diff_gda_adapters_grid_fix_2025-03-05_14-30-59 --skip_copy_code --custom_backends_module=research.attention_moe.backends
