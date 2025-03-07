a66372982651e1b5fef15b0cdcb6f323a6cf8c1b
Janek <jan.malasnicki@gmail.com>
2025-03-07 10:27:14 +0100

    Versioning code

--- research/muP_MoE/utils/model_utils.py
+++ research/muP_MoE/utils/model_utils.py
@@ -451,6 +451,7 @@ def get_model(
     vocab_size: int,
     block_modules: dict[str, Callable[[], torch.nn.Module]],
     dm: int,
+    dff_ratio: float,
     n_blocks: int,
     device: torch.device,
     init_type,
@@ -490,11 +491,6 @@ def get_model(
         )
 
     embedding_layer = llm.EmbeddingLayer(*embedding_components).to(first_gpu)
-    if mup_config is not None:
-        print("---Embedding init with muP---")
-        for name, param in embedding_layer.named_parameters():
-            print(f"Initializing {name} with scale {init_scale}")
-            torch.nn.init.normal_(param.data, mean=0.0, std=(init_scale) ** 0.5)
 
     # Python officially preserves dict order since 3.7, so we pass the layer dict
     transformer_tower = llm.TransformerTower(
@@ -505,33 +501,39 @@ def get_model(
         model_fragmentation=model_fragmentation,
         residual_fn=residual_fn,
     )
+
+    head = llm.PredictionHead(
+        dm, vocab_size, init_type=init_type, init_scale=init_scale
+    ).to(last_gpu)
+
     if mup_config is not None:
+        scale = init_scale / mup_config['base_dmodel']  # this is a normal init undercover xd
+        print("---Embedding init with muP---")
+        for name, param in embedding_layer.named_parameters():
+            # if "0" in name:
+            print(f"Initializing {name} with scale {scale}")
+            torch.nn.init.normal_(param.data, mean=0.0, std=(scale) ** 0.5)
+
+        print("---Unembedding init with muP---")
+        for name, param in head.named_parameters():
+            print(f"Initializing {name} with scale {scale}")
+            torch.nn.init.normal_(param.data, mean=0.0, std=(scale) ** 0.5)
+
         print("---TransformerTower init with muP---")
         transformer_init_dict = {
             "input_projection": (1 / mup_config["m_d"]),
             "output_projection": (1 / (mup_config["m_d"] * 2 * n_blocks)),
             "lin1_weight": (1 / mup_config["m_d"]),
-            "lin2_weight": (1 / (mup_config["m_d"] * 2 * n_blocks)),
+            "lin2_weight": (1 / (mup_config["m_d"] * 2 * n_blocks * dff_ratio)),
             "pre_relu": (1 / mup_config["m_d"]),  # FF in, ver2
-            "post_relu": (1 / (mup_config["m_d"] * 2 * n_blocks)),  # FF out, ver2
+            "post_relu": (1 / (mup_config["m_d"] * 2 * n_blocks * dff_ratio)),  # FF out, ver2
         }
         for name, param in transformer_tower.named_parameters():
-            scale = init_scale
             for keyword, value in transformer_init_dict.items():
                 if keyword in name:
-                    scale *= value
-                    print(f"Initializing {name} with scale {scale}")
-                    torch.nn.init.normal_(param.data, mean=0.0, std=(scale) ** 0.5)
+                    print(f"Initializing {name} with scale {scale * value}")
+                    torch.nn.init.normal_(param.data, mean=0.0, std=(scale * value) ** 0.5)
                     break
-
-    head = llm.PredictionHead(
-        dm, vocab_size, init_type=init_type, init_scale=init_scale
-    ).to(last_gpu)
-    if mup_config is not None:
-        print("---Unembedding init with muP---")
-        for name, param in head.named_parameters():
-            print(f"Initializing {name} with scale {init_scale}")
-            torch.nn.init.normal_(param.data, mean=0.0, std=(init_scale) ** 0.5)
 
     model = mup_modules.muP_LLM(embedding_layer, transformer_tower, head, mup_config)
 
---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/muP_MoE/configs/gyang_config.yaml --git_branch=muP_MoE_like_gyang_config_2025-03-07_10-27-14 --skip_copy_code
