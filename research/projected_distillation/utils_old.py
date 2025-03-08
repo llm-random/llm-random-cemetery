b088bacfaaa09cec831c66c75569b60437f568b9
madragonse <madragonse@gmail.com>
2025-03-08 19:21:24 +0100

    Versioning code

---
+++ research/projected_distillation/utils_old.py
@@ -0,0 +1,212 @@
+import numpy as np
+import torch
+
+from lizrd.core.initialization import get_init_weight
+
+
+FREEZE_PARAMS_REGULES = [
+    ".block.residual_feedforward.layer.feedforward.logging_ff_pre_relu.", #FF
+    ".block.residual_feedforward.layer.feedforward.logging_ff_post_relu.",
+
+    ".block.residual_attention.layer.attention.input_projection.input_projection.weight", #ATT
+    ".block.residual_attention.layer.attention.output_projection.output_projection.weight",
+
+    "embedding_layer.layers.0.embedding.weight", #TE
+    "embedding_layer.layers.1.projected_layer.pe_layer.weight", #PE
+
+    "head.head.weight", #Head
+]
+
+FF_PARAMS_BLACKLIST = [
+    ".block.residual_feedforward.layer.feedforward.logging_ff_pre_relu.", #FF
+    ".block.residual_feedforward.layer.feedforward.logging_ff_post_relu.",
+]
+
+def freeze_projected_params(model, unprojected_ff):
+    frozen_modules = []
+    for name, param in model.named_parameters():
+        if unprojected_ff and any([reg in name for reg in FF_PARAMS_BLACKLIST]):  # Check if the parameter belongs to layer1
+            continue
+        if any([reg in name for reg in FREEZE_PARAMS_REGULES]):  # Check if the parameter belongs to layer1
+            param.requires_grad = False
+            frozen_modules.append(param)
+    return frozen_modules
+
+
+FREEZE_LN_REGULES = [
+    ".pre_norm.", # Layer norm
+]
+
+def freeze_ln_params(model):
+    frozen_modules = []
+    for name, param in model.named_parameters():
+        if any([reg in name for reg in FREEZE_LN_REGULES]):  # Check if the parameter belongs to layer1
+            param.requires_grad = False
+            frozen_modules.append(param)
+    return frozen_modules 
+
+PROJECTIONS_1_1 = [
+    ".block.residual_attention.layer.attention.input_projection.input_projection_p11.weight",
+    ".block.residual_attention.layer.attention.output_projection.output_projection_p21.weight",
+    ".block.residual_feedforward.layer.feedforward.logging_ff_pre_relu_p11.weight",
+    "head.head_p.weight",
+    ".block.residual_feedforward.layer.feedforward.logging_ff_post_relu_p21.weight", #FF in - 1ff configuration
+]
+
+PROJECTIONS_1_1_T = [
+    "embedding_layer.layers.0.embedding_p.weight",
+    "embedding_layer.layers.1.projected_layer.pe_layer_p.weight",
+    ".block.residual_attention.layer.attention.output_projection.output_projection_p22.weight",
+    ".block.residual_feedforward.layer.feedforward.logging_ff_post_relu_p22.weight",
+    ".block.residual_feedforward.layer.feedforward.logging_ff_pre_relu_p12.weight", #FF out - 1ff configuration
+    ".block.residual_attention.layer.attention.input_projection_out_projection_q.input_projection_p12_q.weight", #Attention in_projection_out_projection
+    ".block.residual_attention.layer.attention.input_projection_out_projection_k.input_projection_p12_k.weight", 
+    ".block.residual_attention.layer.attention.input_projection_out_projection_v.input_projection_p12_v.weight", 
+]
+
+PROJECTIONS_1_4 = [
+    # ".block.residual_feedforward.layer.feedforward.logging_ff_post_relu_p21.weight",
+]
+
+PROJECTIONS_1_4_T = [
+    # ".block.residual_feedforward.layer.feedforward.logging_ff_pre_relu_p12.weight",
+]
+
+PROJECTIONS_1_3 = [
+
+]
+
+PROJECTIONS_1_3_T = [
+    # ".block.residual_attention.layer.attention.input_projection.input_projection_p12.weight",
+]
+# encoder.blocks.block_7.block.residual_feedforward.layer.feedforward.logging_ff_pre_relu_p11.weight, shape: torch.Size([512, 256]) requires_grad: True, cuda:0
+# encoder.blocks.block_7.block.residual_feedforward.layer.feedforward.logging_ff_pre_relu_p12.weight, shape: torch.Size([256, 512]) requires_grad: True, cuda:0
+# encoder.blocks.block_7.block.residual_feedforward.layer.feedforward.logging_ff_post_relu_p21.weight, shape: torch.Size([512, 256]) requires_grad: True, cuda:0
+# encoder.blocks.block_7.block.residual_feedforward.layer.feedforward.logging_ff_post_relu_p22.weight, shape: torch.Size([256, 512]) requires_grad: True, cuda:0
+MULTIPLY = [
+
+]
+
+MULTIPLY_T = [
+
+]
+
+
+def is_in_partial_list(elemen_name:str, partials_list:list[str]):
+    for weight_name in partials_list:
+        if weight_name in elemen_name:
+            return True
+    return False
+
+def initialize_projections(model:torch.nn.Module, dmodel:int, projected_dmodel:int, projection:torch.Tensor, diagonal=True):
+    if projection is None:
+        print("No projection initialization")
+        return
+    
+
+    # if diagonal:
+    #     projection_z = torch.zeros((projected_dmodel, dmodel))
+    #     projection_4 = torch.concat((
+    #         torch.concat((projection, projection_z, projection_z, projection_z), dim=0),
+    #         torch.concat((projection_z, projection, projection_z, projection_z), dim=0),
+    #         torch.concat((projection_z, projection_z, projection, projection_z), dim=0),
+    #         torch.concat((projection_z, projection_z, projection_z, projection), dim=0),
+    #         ), dim=1)
+    #     projection_4_T = projection_4.T
+    # else:
+    #     print("NO DIAGONAL INIT") #dev
+    #     projection_4 = torch.concat((projection, projection, projection, projection))
+    #     projection_4 = torch.concat((projection_4, projection_4, projection_4, projection_4), dim=1)
+    #     projection_4_T = projection_4.T
+
+    #dev start experimental layer dependent projections
+    # def generate_projection():
+    #     ret_proj = torch.zeros(projected_dmodel, projected_dmodel)
+    #     mask = torch.eye(projected_dmodel).bool()
+    #     ret_proj = ret_proj.masked_fill(mask, 1)
+    #     # projection = projection[:, int(dm):]
+
+    #     columns_to_remove = torch.randperm(projected_dmodel)[:projected_dmodel-dmodel]
+    #     mask = torch.ones(projected_dmodel, dtype=torch.bool)
+    #     mask[columns_to_remove] = False
+    #     # print(mask) #dev
+    #     ret_proj = ret_proj[:, mask]
+    #     return ret_proj
+    # default_projection = generate_projection() 
+    # projections_dict = {
+    #     "block_0": default_projection,
+    #     "block_1": default_projection,
+    #     "block_2": default_projection,
+    #     "block_3": default_projection,
+    #     "block_4": default_projection,
+    #     "block_5": default_projection,
+    #     "block_6": default_projection,
+    #     "block_7": default_projection,
+    # }
+    #dev stop experimental layer dependent projections
+    
+    projection_z = torch.zeros((projected_dmodel, dmodel), device=projection.device)
+
+    print("------------------------------init projections------------------------") #dev
+    for name, params in model.named_parameters():
+
+        # print(f"{name} projection fitting") #dev layer_dependent
+        # projection = default_projection #dev layer_dependent
+        # for k, v in projections_dict.items(): #dev layer_dependent
+        #     if k in name:
+        #         print(f"{k} in this name")
+        #         projection = v
+
+        if is_in_partial_list(name, PROJECTIONS_1_1):
+            # projection
+            print(f"projection: {name}, {params.shape}")
+            params.data.copy_(projection)
+            # params.data = projection #dev coupled 
+        elif is_in_partial_list(name, PROJECTIONS_1_1_T):
+            # projection_T
+            print(f"projection_T: {name}, {params.shape}")
+            params.data.copy_(projection.T)
+            # params.data = projection.T #dev coupled 
+            # params.data.copy_(torch.inverse(projection).T) #dev inverted_test
+            # params.data.copy_(torch.inverse(projection)) #dev inverted_test
+        elif is_in_partial_list(name, PROJECTIONS_1_4):
+            # projection_4
+            print(f"projection_4: {name}, {params.shape}")
+            raise NotImplemented()
+            # params.data.copy_(projection_4)
+        elif is_in_partial_list(name, PROJECTIONS_1_4_T):
+            # projection_4_T
+            print(f"projection_4_T: {name}, {params.shape}")
+            raise NotImplemented()
+            # params.data.copy_(projection_4_T)
+        elif is_in_partial_list(name, PROJECTIONS_1_3):
+            # projection_3
+            print(f"projection_3: {name}, {params.shape}")
+            raise NotImplemented()
+            params.data.copy_(projection_3)
+        elif is_in_partial_list(name, PROJECTIONS_1_3_T):
+            # projection_3_T
+            print(f"projection_3_T: {name}, {params.shape}")
+            projection_c = projection
+            projection_3 = torch.concat((
+                torch.concat((projection_c, projection_z, projection_z), dim=0),
+                torch.concat((projection_z, projection_c, projection_z), dim=0),
+                torch.concat((projection_z, projection_z, projection_c), dim=0),
+            ), dim=1)
+            params.data.copy_(projection_3.T)
+            # params.data = projection_3.T #dev coupled 
+            # params.data.copy_(torch.inverse(projection_3).T) #dev inverted_test
+            # params.data.copy_(torch.inverse(projection_3)) #dev inverted_test
+        elif is_in_partial_list(name, MULTIPLY):
+            # projection
+            print(f"projection: {name}, {params.shape}")
+            raise NotImplemented()
+            params.data.copy_(projection)
+        elif is_in_partial_list(name, MULTIPLY_T):
+            # projection
+            print(f"projection: {name}, {params.shape}")
+            raise NotImplemented()
+            params.data.copy_(projection)
+        else:
+            print(f"Not projection: {name}, {params.shape}, {params.requires_grad}")
+    print("------------------------------init projections end------------------------") #dev
