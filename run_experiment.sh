475f2c8148e881be21d9553c0e4b5d07e0749515
Maciej Pióro <m@ciekpio.ro>
2025-03-08 02:53:09 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/private/diff_attn_final/2025.03.08_save_checkpoint.yaml --git_branch=debug_diff_2025-03-08_02-53-09 --skip_copy_code --custom_backends_module=research.attention_moe.backends
