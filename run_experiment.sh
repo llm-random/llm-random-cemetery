54d1a767ccfc10bb1eaadd2e2dd6949ca0f2494e
Maciej Pióro <m@ciekpio.ro>
2025-03-18 16:47:45 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/eval/helios/ad_hoc.yaml --git_branch=constrained_scaling_grid_21_11_2025-03-18_16-47-44 --skip_copy_code
