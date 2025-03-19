eef46cf1fa4fe57b643ec944b552a10cfd10b9f8
Maciej Pióro <m@ciekpio.ro>
2025-03-19 16:39:42 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/private/attn_scaling/2025.03.19_first.yaml --git_branch=debug_diff_2025-03-19_16-39-42 --skip_copy_code --custom_backends_module=research.context_scaling.backends
