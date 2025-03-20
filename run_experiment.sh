fcbe6c899e908185fb61945abcda3f6fe9172b57
Mikołaj Dziok <m1kush@disroot.org>
2025-03-20 21:35:17 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/better_differential_transformer.yaml --git_branch=diff_no_final_norm_2025-03-20_21-35-17 --skip_copy_code --custom_backends_module=research.attention_moe.backends
