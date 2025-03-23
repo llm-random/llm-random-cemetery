acb408359a6c5c5d445de0296dd7e238f3c9583f
Maciej Stefaniak <madragonse@gmail.com>
2025-03-24 00:15:40 +0100

    Versioning code

--- research/conditional/utils/model_utils.py
+++ research/conditional/utils/model_utils.py
@@ -335,7 +335,7 @@ def calculate_llm_distillation_loss_and_gradient(
             mask_cpu = mask.reshape(-1).requires_grad_(False)
 
             for loss_type_i, method_lam in AVAILABLE_DISTILL_LOSSES:
-                distill_losses[loss_type_i]  = get_distill_loss(model_output_cpu, tutor_output_cpu, mask_cpu, loss_type_i, method_lam)
+                distill_losses[loss_type_i]  = get_distill_loss(model_output_cpu, tutor_output_cpu, mask_cpu, loss_type_i, method_lam).item()
 
         aux_info = {
             "correct_tokens": correct_tokens,
