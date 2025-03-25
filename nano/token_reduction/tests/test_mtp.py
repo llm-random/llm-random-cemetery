48f0f8e89251cd9794503f1683cab2bab35c1370
Janek <jan.malasnicki@gmail.com>
2025-03-25 17:23:03 +0100

    Versioning code

--- nano/token_reduction/tests/test_mtp.py
+++ nano/token_reduction/tests/test_mtp.py
@@ -59,6 +59,47 @@ class TestTrainerMTP(unittest.TestCase):
         print("ok")
         mtp_trainer.train()
 
+def costam(cfg):
+    # with initialize(version_base=None, config_path="configs"):
+    #     cfg = compose(config_name="test_mtp", overrides=[])
+
+    setup_enviroment()
+    metric_logger = get_metric_logger(
+        metric_logger_config=instantiate(cfg.metric_logger, _convert_="all"),
+    )
+
+    model = instantiate(cfg.model, _convert_="all")
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=cfg.training.learning_rate,
+        weight_decay=cfg.training.weight_decay,
+    )
+
+    scheduler = instantiate(cfg.training.scheduler)(optimizer=optimizer)
+
+    dataloaders_factory = instantiate(cfg.dataloaders_factory)
+    train_dataloader, eval_dataloader = dataloaders_factory()
+
+    training_state = {"next_step": 0, "run_id": None, "processed_tokens": 0}
+
+    trainer_factory = instantiate(cfg.trainer_factory)
+    # print(f'cfg.trainer_factory:\n{cfg.trainer_factory}')
+    # print(f'trainer_factory:\n{type(trainer_factory)}')
+    # print(f'model: {type(model)}')
+    mtp_trainer = trainer_factory(
+        model=model,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        training_state=training_state,
+        train_dataloader=train_dataloader,
+        eval_dataloader=eval_dataloader,
+        metric_logger=metric_logger,
+    )
+    # print(f'mtp_trainer: {type(mtp_trainer)}')
+    print("ok")
+    mtp_trainer.train()
+
 
 if __name__ == "__main__":
     unittest.main()
