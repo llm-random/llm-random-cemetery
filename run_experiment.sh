32a155a4616375afb53053fe7109ab3abc41dfc6
Maciej Pióro <m@ciekpio.ro>
2025-03-27 20:58:05 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/experiments/constrained_scaling_laws/harness/16s.yaml --git_branch=constrained_scaling_grid_21_11_2025-03-27_20-58-05 --skip_copy_code
