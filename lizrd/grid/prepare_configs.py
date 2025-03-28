62efec1cadb336ba62cb3e984fc67b6598b95d8b
Janek <jan.malasnicki@gmail.com>
2025-03-28 10:57:05 +0100

    Versioning code

--- lizrd/grid/prepare_configs.py
+++ lizrd/grid/prepare_configs.py
@@ -76,6 +76,9 @@ def prepare_configs(
     configs, all_config_paths = load_with_inheritance(config_path)
 
     for config in configs:
+        print(f'___config___')
+        print(f'{config}')
+        print(f'___config___')
         config["params"]["git_branch"] = git_branch
         config["params"]["path_to_entry_config"] = config_path
         config["params"]["all_config_paths"] = ",".join(all_config_paths)
