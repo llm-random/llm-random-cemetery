f17709cafeee5223694972095a589b0d240fff7f
Janek <jan.malasnicki@gmail.com>
2025-03-28 10:25:53 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/muP_MoE/configs/cnfg.yaml --git_branch=muP_MoE_MFU_fixing_2025-03-28_10-25-53 --skip_copy_code --custom_backends_module=research.muP_MoE.utils.backends
