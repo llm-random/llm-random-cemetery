8cf04a96592f5443dd695e1afec002bc5201d0ae
Janek <jan.malasnicki@gmail.com>
2025-04-01 12:03:48 +0200

    Versioning code

--- research/muP_MoE/utils/trainer.py
+++ research/muP_MoE/utils/trainer.py
@@ -177,9 +177,9 @@ class muP_Trainer:
 
         self.lr_scheduler.set_lr(step=step, optimizer=self.optimizer)
         loss, aux_info = self.calculate_loss_and_gradient(processed_batch)
-        self._apply_gradient()
         # this function needs to check for logging_process itself
         self._log_weights_and_gradients(step)
+        self._apply_gradient()
 
         if self.is_logging_process:
             self._log_train_stats(loss, step)
@@ -227,19 +227,21 @@ class muP_Trainer:
             "losses": losses,
         }
 
+    def clip_gradient(self):
+        if self.gradient_clipping is not None:
+            if isinstance(self.model, FSDP):
+                return self.model.clip_grad_norm_(self.gradient_clipping)
+            else:
+                return torch.nn.utils.clip_grad_norm_(
+                    self.model.parameters(), self.gradient_clipping
+                )
+
     def _apply_gradient(self):
         if self.scaler is None:
-            if self.gradient_clipping is not None:
-                torch.nn.utils.clip_grad_norm_(
-                    self.model.parameters(), self.gradient_clipping
-                )
+            self.clip_gradient()
             self.optimizer.step()
         else:
-            if self.gradient_clipping is not None:
-                self.scaler.unscale_(self.optimizer)
-                torch.nn.utils.clip_grad_norm_(
-                    self.model.parameters(), self.gradient_clipping
-                )
+            self.clip_gradient()
             self.scaler.step(self.optimizer)
             self.scaler.update()
         self.optimizer.zero_grad()
@@ -337,7 +339,9 @@ class muP_Trainer:
     def _log_weights_and_gradients_loop(self, step):
         g_norms, w_norms = {}, {}
         for name, value in self.model.named_parameters():
+            print(f'name: {name}')
             if value.grad is not None:
+                print('\tin grad')
                 norm = torch.linalg.norm(value.grad)
                 g_norms[f"weight_norms/{name.replace('.', '/')}/grad"] = norm
                 if (
@@ -348,6 +352,7 @@ class muP_Trainer:
                         f"spectral_norms/{name.replace('.', '/')}/grad"
                     ] = spectral_norm
             if value.requires_grad:
+                print('\tin weight')
                 norm = torch.linalg.norm(value)
                 variance = torch.var(value)
                 w_norms[f"weight_norms/{name.replace('.', '/')}/weight"] = norm
