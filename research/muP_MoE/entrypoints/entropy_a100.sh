82e5e007fd7d097900a902b0768f42c5b1ad6047
Janek <jan.malasnicki@gmail.com>
2025-04-11 08:52:24 +0200

    Versioning code

--- research/muP_MoE/entrypoints/entropy_a100.sh
+++ research/muP_MoE/entrypoints/entropy_a100.sh
@@ -2,7 +2,7 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 echo "Running Entropy A100 Entrypoint"
-conda activate /storage_ssd_1/llm-random/differential-conda
+conda activate llm-random_main
 echo "Will run the following command:"
 echo "$@"
 echo "==============================="
