8f32958da78645b1cc576ed88e4797ed8446f590
Janek <jan.malasnicki@gmail.com>
2025-04-15 13:10:38 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/muP_MoE/configs/lr_plot_dense.yaml --git_branch=muP_MoE_lr_grid_dense_0704_rerun_2025-04-15_13-10-38 --skip_copy_code --custom_backends_module=research.muP_MoE.utils.backends
