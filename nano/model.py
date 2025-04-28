8f2e3b605899ca5a13e1d9b5d2ec45d62914a0c7
Michał Krutul <crewtoolcompany@gmail.com>
2025-04-28 21:42:42 +0200

    Versioning code

--- nano/model.py
+++ nano/model.py
@@ -121,6 +121,8 @@ def run(cfg):
     if "distributed" in cfg and cfg.distributed is not None:
         distributed_setup()
     training_state = load_training_state(cfg.checkpoint_config)
+    training_state["run_id"] = None #REMOVE
+    # training_state = {"next_step": 0, "run_id": None, "processed_tokens": 0}
     metric_logger = get_metric_logger(
         metric_logger_config=instantiate(cfg.metric_logger, _convert_="all"),
         neptune_run_id=training_state["run_id"],
