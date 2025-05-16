9dd56d7880c75838d4ccf5d7fd8435b61f3fb082
Michał Krutul <crewtoolcompany@gmail.com>
2025-05-16 17:23:50 +0200

    Versioning code

--- nano/model.py
+++ nano/model.py
@@ -221,6 +221,7 @@ class C4Dataset(IterableDataset):
             tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
         self._load_dataset(path, split, seed, tokenizer, eot_str, shuffle)
         self.sequence_length = sequence_length
+        self.seed = seed
         self.rng = random.Random(seed)
 
     def _load_dataset(self, path, split, seed, tokenizer, eot_str, shuffle: bool):
@@ -295,6 +296,7 @@ class C4Dataset(IterableDataset):
                     buffer, document_lengths = [], []
 
     def __iter__(self):
+        self.rng.seed(self.seed)
         if self.world_size_independent:
             return itertools.islice(
                 self.sample_packer(), self.rank, None, self.world_size
@@ -340,6 +342,7 @@ def get_dataloader(
             collate_fn=collate_fn,
             pin_memory=True,
             num_workers=num_workers,
+            prefetch_factor=4,
         )
     else:
         raise ValueError(f"Unsupported dataset type: '{dataset_type}'")
