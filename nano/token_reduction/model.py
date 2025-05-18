ba8457a6978261925965c9e218db34f67d1e3abd
Michał Krutul <crewtoolcompany@gmail.com>
2025-05-18 12:43:30 +0200

    Versioning code

--- nano/token_reduction/model.py
+++ nano/token_reduction/model.py
@@ -28,6 +28,7 @@ from model import (
     PredictionHead,
     RMSNorm,
 )
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -1166,3 +1167,16 @@ class TrainerMTPWithMergingUltimate(Trainer):
                     "steps/train/loss_100", self.step, self.loss_interval_100 / 100.0
                 )
                 self.loss_interval_100 = 0.0
+
+def trunc_collate(batch, seq_len=None, batch_size=None):
+    truncated = [sequence[:seq_len] for sequence in batch[:batch_size]]
+    return torch.from_numpy(np.array(truncated))
+
+def collate_trunc_reduction(batch, trunc_seq_len, trunc_batch_size, result_seq_len, n_dropped_tokens):
+    truncated = [sequence[:trunc_seq_len] for sequence in batch[:trunc_batch_size]]
+    batch = torch.from_numpy(np.array(truncated))
+    batch_size, seq_len = batch.shape
+    return (
+        batch,
+        batched_split_indexes(batch_size, seq_len, result_seq_len, n_dropped_tokens),
+    )
