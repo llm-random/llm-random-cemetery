ae42153dcf55114f017892cf5af50adea637306b
Janek <jan.malasnicki@gmail.com>
2025-05-19 10:52:41 +0200

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=research/muP_MoE/configs/lr_plot_moe_granularity.yaml --git_branch=muP_MoE_lr_plot_moe_exprate_2025-05-19_10-52-40 --skip_copy_code --custom_backends_module=research.muP_MoE.utils.backends
