b813c61a0a6b82198505d9cc3aae56b82e856252
Michał Krutul <crewtoolcompany@gmail.com>
2025-05-28 11:21:05 +0200

    Versioning code

--- nano/token_reduction/tests/test_mtp_deepseek.py
+++ nano/token_reduction/tests/test_mtp_deepseek.py
@@ -50,6 +50,37 @@ class TestSimpleRun(unittest.TestCase):
                 self.assertEqual(expected_step, actual_step)
                 self.assertAlmostEqual(expected_loss, actual_loss, delta=TOLERANCE)
 
+    @patch("model.get_metric_logger", return_value=RecorderLogger())
+    def test_mtp_deepseek_merge(self, get_metric_logger):
+        TOLERANCE = 1e-5
+        target_losses_dropping = [
+            (10.914034843444824, 0),
+            (10.883499145507812, 1),
+            (10.891767501831055, 2),
+            (10.889894485473633, 3),
+            (10.874717712402344, 4),
+            (10.853045463562012, 5),
+            (10.860404968261719, 6),
+            (10.825983047485352, 7),
+            (10.828263282775879, 8),
+            (10.803018569946289, 9),
+        ]
+        with initialize(version_base=None, config_path="configs"):
+            cfg = compose(config_name="test_mtp_deepseek_merge", overrides=[])
+            training_state = load_training_state(cfg.checkpoint_config)
+            metric_logger = get_metric_logger(
+                metric_logger_config=instantiate(cfg.metric_logger, _convert_="all"),
+                neptune_run_id=training_state["run_id"],
+            )
+            run(cfg, metric_logger)
+
+            for (expected_loss, expected_step), (actual_loss, actual_step) in zip(
+                target_losses_dropping, metric_logger.data["steps/train/loss"]
+            ):
+                self.assertEqual(expected_step, actual_step)
+                self.assertAlmostEqual(expected_loss, actual_loss, delta=TOLERANCE)
+
+
 
 if __name__ == "__main__":
     unittest.main()
