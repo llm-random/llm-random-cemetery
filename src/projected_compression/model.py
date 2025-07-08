3648b81e9e41d32c45fe6c724830a716b890a7db
Maciej Stefaniak <madragonse@gmail.com>
2025-07-08 17:54:11 +0200

    Versioning code

---
+++ src/projected_compression/model.py
@@ -0,0 +1,10 @@
+# Add new transformer modules for PC compressor model (with residual weights, projections and base model weights):
+# - embedding
+# - head (unembedding)
+# - feed forward
+# - attention
+# -- each element shoud take into account layer norm weights and bias, layernorm weights and biases should have shared projections as weights they normaizes output from!
+
+# modules for activation based weights importance assesment
+
+
