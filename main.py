737abbea8fe6e0e4a866bc26a01e27e7b53dd565
Maciej Stefaniak <madragonse@gmail.com>
2025-07-30 15:07:38 +0200

    Versioning code

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ import logging
 
 from src.core.checkpointing import load_checkpoint_from_file, load_training_state
 from src.core.metric_loggers import NeptuneLogger, get_metric_logger
-from src.core.model import Residual, wrap_model_distributed
+from src.core.model import Residual
+from src.core.utils import wrap_model_distributed 
 
 logger = logging.getLogger(__name__)
 
