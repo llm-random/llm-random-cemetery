c99d6b8a326bb690d6eaf70979db03904d90d3aa
Maciej Stefaniak <madragonse@gmail.com>
2025-08-12 15:49:20 +0200

    Versioning code

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ import logging
 
 from src.core.checkpointing import load_checkpoint_from_file, load_training_state
 from src.core.metric_loggers import NeptuneLogger, get_metric_logger
-from src.core.model import Residual, wrap_model_distributed
+from src.core.model import Residual
+from src.core.utils import wrap_model_distributed 
 
 logger = logging.getLogger(__name__)
 
@@ -179,9 +180,9 @@ def run(cfg, metric_logger=None):
 
 @hydra.main(version_base=None, config_path="configs", config_name="exp")
 def main(config):
-
+    # print(config) #dev
     if config.get("_run_"):
-        run(config)
+        run(config) 
         return
 
     configs_grid = create_grid_config(config)
