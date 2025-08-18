869d02b455b92444b52b4c366e68517bbe5f1aad
Maciej Stefaniak <madragonse@gmail.com>
2025-08-18 14:56:37 +0200

    Versioning code

--- main.py
+++ main.py
@@ -168,6 +168,12 @@ def run(cfg, metric_logger=None):
         scheduler = instantiate(cfg.trainer.scheduler)(optimizer=optimizer, n_steps=cfg.trainer.n_steps)
         load_checkpoint_from_file(cfg.trainer.checkpoint.load, model, optimizer, scheduler)
 
+    print("model------------------------------------------------------")
+    print(os.environ["RANK"])
+    if os.environ["RANK"] == 0:
+        print(model)
+    print("------------------------------------------------------model")
+
     trainer = instantiate(cfg.trainer)
     trainer(
         model=model,
