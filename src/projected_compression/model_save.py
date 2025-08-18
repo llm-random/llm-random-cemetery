b8f8e0bffa548cfede2280a327355814f665facd
Maciej Stefaniak <madragonse@gmail.com>
2025-08-18 16:20:23 +0200

    Versioning code

--- src/projected_compression/model_save.py
+++ src/projected_compression/model_save.py
@@ -382,6 +382,22 @@ class LLM(nn.Module):
         x = self.head(x)
         return x
 
+# class ParameterModule(nn.Module):
+#     def __init__(self, data):
+#         super().__init__()
+#         self.param = nn.Parameter(data)
+
+#     def forward(self):
+#         return self.param
+
+#     # make module behave like the tensor
+#     def __torch_function__(self, func, types, args=(), kwargs=None):
+#         if kwargs is None:
+#             kwargs = {}
+#         return func(self.param, *args, **kwargs)
+
+#     def __repr__(self):
+#         return repr(self.param)
 
 class ProjectedLinear(nn.Module):
     __constants__ = [
@@ -413,13 +429,15 @@ class ProjectedLinear(nn.Module):
         self.base_out_features = base_out_features
         self.initialized_compression = False
 
+        self.projected_weights = nn.Parameter(
+            torch.zeros((result_out_features, result_in_features), **factory_kwargs)
+        )
         self.weight = nn.Parameter(
-            torch.rand((base_out_features, base_in_features), **factory_kwargs)
+            torch.rand((base_out_features, base_in_features), device="cpu", dtype=factory_kwargs["dtype"])
         )
         self.projection_in_weight = None
         self.projection_out_weight = None
         self.auxiliary_weight = None
-        self.projected_weights = None
 
     def init_projections(
         self,
@@ -474,14 +492,12 @@ class ProjectedLinear(nn.Module):
                 final_out_features, final_in_features, **factory_kwargs
             )
             self.auxiliary_weight = nn.Parameter(weight, requires_grad=True)
-        
-        self.projected_weights = nn.Parameter(
-            torch.rand((self.result_out_features if self.result_out_features else self.base_out_features, self.result_in_features if self.result_in_features else self.base_in_features), **factory_kwargs)
-        )
-        self.project()
+
         self.initialized_compression = True
 
-    def project(self):
+    def project(
+        self
+    ):
         weight = self.weight
 
         if self.result_in_features is not None:
@@ -496,13 +512,12 @@ class ProjectedLinear(nn.Module):
         ):
             weight += self.auxiliary_weight
 
-        self.projected_weights.copy_(weight)
+        self.projected_weights.data.copy_(weight)
 
+    
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        # gradient magic happens here - PC optimization
-        # TODO maybe order of projections matter for speed
-        self.project()
         return F.linear(input, self.projected_weights, bias=None)
+    
 
     def extra_repr(self) -> str:
         if self.result_in_features is not None:
