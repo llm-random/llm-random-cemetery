aaffebb1221a2f3f962545c9526de43cf1d59b78
Mikołaj Dziok <m1kush@disroot.org>
2025-08-20 16:05:39 +0200

    Versioning code

---
+++ src/projected_compression/minitron.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+
+
+
+# Hidden dimension: 4096 → 3072
+# MLP hidden dimension: 14336 → 9216
+
+def calculate_dimension_importances(model: nn.Module, calibration_data, dmodel, dff):
+    """
+    Calculate the importance of each neuron in the model based on the calibration data.
+    Returns a list of importance scores for dmodel and dff dimensions.
+    """
+    dmodel_importance = torch.zeros(dmodel)
+    dff_importance = torch.zeros(dff)
+
+    # Forward pass through the model with calibration data
+    print(calibration_data.shape)
+    print(calibration_data[0])
+    x = model.embedding(calibration_data)
+    for layer in model.encoder.blocks:
+        # TODO sum
+
+        normalized_pre_attn = layer.attention_layer.norm(x)
+        dmodel_importance += torch.sum(normalized_pre_attn, dim=[0, 1])  # Sum across batch and sequence dimensions
+
+        attention_output = layer.attention_layer.layer(normalized_pre_attn)
+        x = x + attention_output  # Residual connection
+
+
+        normalized_pre_ff = layer.ff_layer.norm(x)
+        dmodel_importance += torch.sum(normalized_pre_ff, dim=[0, 1])  # Sum across batch and sequence dimensions
+        ff_layer = layer.ff_layer.layer
+        ff_pre_act = ff_layer.ff_pre_act(normalized_pre_ff)
+        dff_importance += torch.sum(ff_pre_act, dim=[0, 1])
+
+        ff_gated = ff_layer.silu(ff_layer.gate(normalized_pre_ff)) if hasattr(ff_layer, 'gate') else normalized_pre_ff
+        ff_output = ff_layer.ff_post_act(ff_pre_act * ff_gated)
+
+        x = x + ff_output  # Residual connection
+    x = model.head(x)
+
+    assert x == model(calibration_data), "Model output does not match expected output"
+    print("Model output matches expected output")
+
+    return dmodel_importance, dff_importance
+
+def prune(model: nn.Module, dmodel_indices, dff_indices):
+    """
+    Prune the model by selecting the top k neurons based on their importance scores.
+    """
+    # Embedding
+    embedding_weight = model.embedding.embedding.weight.data
+    model.embedding.embedding.weight.data = embedding_weight[:, dmodel_indices]
+
+    # Head
+    head_weight = model.head.linear.weight.data
+    model.head.linear.weight.data = head_weight[:, dmodel_indices]
+
+    for block in model.encoder.blocks:
+        block_state_dict = block.state_dict()
+
+        # Attention layer
+        block.attention_layer.norm.weight.data = block_state_dict['attention_layer.layer.norm.weight'][dmodel_indices]
+        block.attention_layer.layer.q_proj.weight.data = block_state_dict['attention_layer.layer.q_proj.weight'][:, dmodel_indices]
+        block.attention_layer.layer.k_proj.weight.data = block_state_dict['attention_layer.layer.k_proj.weight'][:, dmodel_indices]
+        block.attention_layer.layer.v_proj.weight.data = block_state_dict['attention_layer.layer.v_proj.weight'][:, dmodel_indices]
+        block.attention_layer.layer.o_proj.weight.data = block_state_dict['attention_layer.layer.o_proj.weight'][dmodel_indices, :]
+
+        # Feed-forward layer
+        block.ff_layer.norm.weight.data = block_state_dict['ff_layer.layer.norm.weight'][dmodel_indices]
+        block.ff_layer.layer.ff_pre_act.weight.data = block_state_dict['ff_layer.layer.ff_pre_act.weight'][dff_indices, dmodel_indices]
+        if 'ff_layer.layer.gate.weight' in block_state_dict:
+            block.ff_layer.layer.gate.weight.data = block_state_dict['ff_layer.layer.gate.weight'][dff_indices, dmodel_indices]
+        block.ff_layer.layer.ff_post_act.weight.data = block_state_dict['ff_layer.layer.ff_post_act.weight'][dmodel_indices, dff_indices]
+
+    return model
+
+def minitron_prune(model: nn.Module, dataloader, dmodel, dff, calibration_dataset_size, seq_len):
+    # Freeze all parameters
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # get 1024 samples calibration data
+    calibration_data = torch.zeros(calibration_dataset_size, seq_len + 1)
+    for i, batch in enumerate(dataloader):
+        if i >= calibration_dataset_size:
+            break
+        calibration_data[i] = batch[0]
+
+    importance_neurons, importance_dmodel = calculate_dimension_importances(
+        model, calibration_data, dmodel, dff
+    )
+
+    # select bottom k neurons
+    # bottomk_dmodel = 1024
+    # bottomk_dff = 5120
+    #
+    # dmodel_bottom_indices = torch.topk(importance_neurons, dim=0, largest=False, k=bottomk_dmodel).indices
+    # dff_bottom_indices = torch.topk(importance_dmodel, dim=0, largest=False, k=bottomk_dff).indices
+    #
+    #
+    # model = prune(model, dmodel_bottom_indices, dff_bottom_indices)
+
+    # select top k neurons
+    topk_dmodel = 3072
+    topk_dff = 9216
+
+    dmodel_top_indices = torch.topk(importance_neurons, dim=0, largest=True, k=topk_dmodel).indices
+    dff_top_indices = torch.topk(importance_dmodel, dim=0, largest=True, k=topk_dff).indices
+
+    model = prune(model, dmodel_top_indices, dff_top_indices)
+
