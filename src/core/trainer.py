dd87c43e3a63f193d3807838518b23b500b7b006
Maciej Stefaniak <madragonse@gmail.com>
2025-08-24 19:02:44 +0200

    Versioning code

--- src/core/trainer.py
+++ src/core/trainer.py
@@ -98,15 +98,25 @@ class Trainer:
             self.optimizer.step()
             self.optimizer.zero_grad()
             self.scheduler.step()
-
+            
             if self._should_save_checkpoint:
                 self.save_checkpoint()
 
             if self._should_evaluate:
                 self.eval()
 
+        print("Presave ===============") #dev
+        print(self._should_save_checkpoint)
+        print(self.checkpoint.save.type)
+        print("=+=+++")
         if self._should_save_final_checkpoint:
-            self.save_checkpoint()
+            if self.checkpoint.save.type == "nano":
+                self.save_checkpoint()
+            elif self.checkpoint.save.type == "huggingface":
+                print(f"Rank: {os.environ['RANK']} ------------------------------------")#dev
+                print(self.model.state_dict())
+                print(f"------------------------------------ Rank: {os.environ['RANK']}")
+                # local_model = self.model.to("cpu")
 
     def _preprocess_input(self, batch):  # TODO test it
         input_ids = batch[:, :-1].contiguous()
@@ -202,8 +212,8 @@ class Trainer:
 
     def clip_gradient(self):
         if self.gradient_clipping is not None:
-            if isinstance(self.model, FSDP):
-                return self.model.clip_grad_norm_(self.gradient_clipping)
+            if isinstance(self.model, FSDP) or self.model.__module__ == "torch.distributed.fsdp._fully_shard._fully_shard": 
+                return self.model.clip_grad_norm_(self.gradient_clipping)  #dev TODO does it work with FSDP2
             else:
                 return torch.nn.utils.clip_grad_norm_(
                     self.model.parameters(), self.gradient_clipping
