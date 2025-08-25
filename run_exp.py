8c53f5f26722c1de688ba4f29fcd08a1899b2b8a
Michał Krutul <crewtoolcompany@gmail.com>
2025-08-25 18:51:26 +0200

    Versioning code

--- run_exp.py
+++ run_exp.py
@@ -12,28 +12,30 @@ from fabric import Connection
 import hydra
 from omegaconf import OmegaConf
 import paramiko.ssh_exception
+from hydra import compose, initialize
+from omegaconf import OmegaConf
 
+from grid_generator.generate_configs import create_grid_config
+from grid_generator.sbatch_builder import generate_sbatch_script
+from main import dump_grid_configs
 from resolver import get_cluster_name
 
+from rich.console import Console
+from rich.spinner import Spinner
+from rich.live import Live
+from rich.logging import RichHandler
+console = Console()
 logger = logging.getLogger(__name__)
+# if logger.hasHandlers():
+#     logger.handlers.clear()
+logging.basicConfig(
+    # level=logging.DEBUG,
+    # format="%(message)s",
+    handlers=[RichHandler(console=Console(), rich_tracebacks=True)],
+)
+# logger.addHandler(RichHandler(console=console, rich_tracebacks=True))
 
 _SSH_HOSTS_TO_PASSPHRASES = {}
-
-
-def ensure_remote_config_exist(repo: Repo, remote_name: str, remote_url: str):
-    for remote in repo.remotes:
-        if remote.name == remote_name:
-            if remote.url != remote_url:
-                old_remote_url = remote.url
-                remote.set_url(remote_url)
-                print(
-                    f"Updated url of '{remote_name}' remote from '{old_remote_url}' to '{remote_url}'"
-                )
-            return
-
-    repo.create_remote(remote_name, url=remote_url)
-    print(f"Added remote '{remote_name}' with url '{remote_url}'")
-
 
 def commit_pending_changes(repo: Repo):
     if len(repo.index.diff("HEAD")) > 0:
@@ -52,11 +54,15 @@ def reset_to_original_repo_state(
     repo.head.reset(original_branch_commit_hash, index=True)
     print("Successfully restored working tree to the original state!")
 
+def git_ssh_to_https_tree(ssh_url: str) -> str:
+    without_git = ssh_url.removeprefix("git@").removesuffix(".git")
+    host, path = without_git.split(":", 1)
+    return f"https://{host}/{path}/tree"
 
 def version_code(
-    remote_name: str,
     remote_url: str,
     experiment_config_path: Optional[str] = None,
+    exp_job_path: Optional[str] = None,
     job_name: Optional[str] = None,
 ) -> str:
     repo = Repo(".", search_parent_directories=True)
@@ -68,19 +74,17 @@ def version_code(
     original_branch = repo.active_branch.name
     original_branch_commit_hash = repo.head.object.hexsha
 
-    ensure_remote_config_exist(repo, remote_name, remote_url)
     repo.git.add(experiment_config_path, force=True)
+    repo.git.add(exp_job_path, force=True)
     repo.git.add(all=True)
 
     try:
         commit_pending_changes(repo)
-
         repo.git.checkout(b=experiment_branch_name)
-        print(
-            f"Pushing experiment code to {experiment_branch_name} '{remote_name}' remote..."
-        )
-        repo.git.push(remote_name, experiment_branch_name)
-        print(f"Pushed.")
+        spinner = Spinner("dots", text=f"Pushing experiment code to branch '{experiment_branch_name}' at '{remote_url}'...")
+        with Live(spinner, refresh_per_second=10, console=console):
+            repo.git.push(remote_url, experiment_branch_name)
+        logger.info(f"Experiment pushed to:  '{git_ssh_to_https_tree(remote_url)}/{experiment_branch_name}'")
     finally:
         reset_to_original_repo_state(
             repo, original_branch, original_branch_commit_hash, experiment_branch_name
@@ -132,31 +136,27 @@ def get_experiment_components(
 def submit_experiment(
     cfg: OmegaConf,
 ):
-    hydra_config = hydra.utils.HydraConfig.get()
-    config_path, config_name = get_experiment_components(hydra_config)
-    experiment_config_path = f"{config_path}/{config_name}.yaml"
-
-    experiment_branch_name = version_code(
-        cfg.infrastructure.git.remote_name,
-        cfg.infrastructure.git.remote_url,
-        experiment_config_path,
-        hydra_config.job.name,
-    )
-
     missing_keys: set[str] = OmegaConf.missing_keys(cfg)
     if missing_keys:
         raise RuntimeError(f"Got missing keys in config:\n{missing_keys}")
 
+    configs_grid = create_grid_config(cfg)
+    dump_grid_configs(configs_grid, cfg.infrastructure.generated_configs_path)
+
+    modules_to_add = cfg.infrastructure.get("modules_to_add", None)
+    generate_sbatch_script(
+        cfg.infrastructure.slurm, cfg.infrastructure.generated_configs_path, len(configs_grid), cfg.infrastructure.venv_path, modules_to_add
+    )
+
+    experiment_branch_name = version_code(
+        remote_url=cfg.infrastructure.git.remote_url,
+        experiment_config_path=cfg.infrastructure.generated_configs_path,
+        exp_job_path="exp.job",
+        job_name=cfg.infrastructure.metric_logger.name,
+    )
+
     with ConnectWithPassphrase(host=cfg.infrastructure.server, inline_ssh_env=True) as connection:
-        result = connection.run("uname -n", hide=True)
-        hostname = result.stdout.strip()
-        username = connection.user
-
-        cluster_name = get_cluster_name(hostname, username)
-
-        cluster_config = OmegaConf.load(f"configs/clusters/{cluster_name}.yaml")
-
-        cemetery_dir = cluster_config.cemetery_experiments_dir
+        cemetery_dir = cfg.infrastructure.cemetery_experiments_dir
         connection.run(f"mkdir -p {cemetery_dir}")
 
         if "NEPTUNE_API_TOKEN" in os.environ:
@@ -178,7 +178,7 @@ def submit_experiment(
             print(f"Cloned.")
         else:
             print(
-                f"Experiment {experiment_branch_name} already exists on {hostname}. Skipping."
+                f"Experiment {experiment_branch_name} already exists. Skipping."
             )
 
         try:
