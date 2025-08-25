2f6705884ff1ec0d4cdd2d7ef0015dd305feec18
Maciej Stefaniak <madragonse@gmail.com>
2025-08-25 22:05:32 +0200

    Versioning code

--- src/core/checkpointing.py
+++ src/core/checkpointing.py
@@ -121,6 +121,15 @@ def _find_latest_checkpoint(path: str) -> str:
 
     return max(files, key=os.path.getmtime)
 
+def print_state_dict_info(state_dict): #dev
+    for name, param in state_dict.items():
+        if isinstance(param, torch.Tensor):
+            print(f"{name:60s} {type(param)}, shape={tuple(param.shape)} "
+            # print(f"{name:60s} {param}, shape={tuple(param.shape)} "
+                f"norm={param.norm().item():.4f}")
+        else:
+            # Sometimes buffers / metadata can be non-tensors
+            print(f"{name:60s} NON-TENSOR {type(param)}")
 
 def load_checkpoint_from_file(load_config, model, optimizer, scheduler):
     checkpoint_path = load_config.path
@@ -128,7 +137,7 @@ def load_checkpoint_from_file(load_config, model, optimizer, scheduler):
         return 
 
     if checkpoint_path is not None:
-        if isinstance(model, FSDP):
+        if isinstance(model, FSDP) or model.__module__ == "torch.distributed.fsdp._fully_shard._fully_shard":
             # Sharded load
             state_dict = {"app": TrainingState(model, optimizer, scheduler)}
             dcp.load(state_dict=state_dict, checkpoint_id=checkpoint_path)
