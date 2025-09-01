527d31e39f3e5f3e03ba552c2897ddb1a6d68ebf
Michał Krutul <crewtoolcompany@gmail.com>
2025-09-01 17:15:16 +0200

    Versioning code

--- main.py
+++ main.py
@@ -97,10 +97,14 @@ def distributed_setup():
     rank = int(os.environ["RANK"])
     local_rank = int(os.environ["LOCAL_RANK"])
     world_size = int(os.environ.get("WORLD_SIZE", 1))
+    print(os.environ["CUDA_VISIBLE_DEVICES"])
+    print(f"Distributed setup: rank {rank}, local_rank {local_rank}, world_size {world_size}")
 
     if torch.cuda.is_available():
-        dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
+        print(torch.__version__)
+        dist.init_process_group(backend="nccl", rank=rank, world_size=world_size, device_id=torch.device(f"cuda:{local_rank}"))
         torch.cuda.set_device(local_rank)
+        print(f"World Rank: {dist.get_world_size()}, Group Rank: {rank}")
     else:
         logger.warning("CUDA is not available. Running on CPU and 'gloo' backend.")
         dist.init_process_group(backend="gloo", rank=rank, world_size=world_size)
@@ -186,7 +190,7 @@ def run(cfg, metric_logger=None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     logger.info(f"Creating model...")
-    model = instantiate(cfg.model, _convert_="all").to(device)
+    model = instantiate(cfg.model, _convert_="all") #.to(device)
     logger.info(f"Model {model.__class__.__name__} created with {sum(p.numel() for p in model.parameters() if p.requires_grad)} trainable parameters")
     # Residual layers needs metric_logger for logging update norms
     for _, module in model.named_modules():
@@ -195,6 +199,7 @@ def run(cfg, metric_logger=None):
 
     if cfg.trainer.checkpoint.load.type == "huggingface":
         copy_llama_model_weights_from_HF(model, cfg.trainer.checkpoint.load.path)
+        logger.info(f"Loaded weights from Huggingface model at {cfg.trainer.checkpoint.load.path}")
         if cfg.get("apply_functions", None):
             for fn in instantiate(cfg.apply_functions):
                 fn(model)
@@ -240,6 +245,10 @@ def run(cfg, metric_logger=None):
     else:
         raise Exception(f"Not recognized load checkpoint format: {cfg.trainer.checkpoint.load.type}")
     
+    print(f"JESTEM PRZED DIST BARRIER {os.environ.get('RANK', 'no rank')}")
+    dist.barrier()
+    print(f"JESTEM ZA DIST BARRIER {os.environ.get('RANK', 'no rank')}")
+
     trainer = instantiate(cfg.trainer)
     trainer(
         model=model,
