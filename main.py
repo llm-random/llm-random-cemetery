0c84511b83ccb21aec4581b83d56731a7c42cd88
Maciej Stefaniak <madragonse@gmail.com>
2025-09-02 10:47:19 +0200

    Versioning code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import hydra
 import yaml
+from src.core.conversion_from_finalized_pc import load_finalized_pc_checkpoint
 from src.core.distributed_training import setup_distributed_training
 from src.core.conversion_from_llmrandom import load_llmrandom_checkpoint
 from src.core.llama import copy_llama_model_weights_from_HF
@@ -217,7 +218,40 @@ def run(cfg, metric_logger=None):
             weight_decay=cfg.trainer.weight_decay,
         )
         scheduler = instantiate(cfg.trainer.scheduler)(optimizer=optimizer, n_steps=cfg.trainer.n_steps)
+    elif cfg.trainer.checkpoint.load.type == "finalized_pc":
+        load_finalized_pc_checkpoint(model, cfg.trainer.checkpoint.load.path)
+        if cfg.get("apply_functions", None):
+            for fn in instantiate(cfg.apply_functions):
+                fn(model)
+        model = setup_distributed_training(model, cfg.trainer.distributed)
+        optimizer = torch.optim.AdamW(
+            model.parameters(),
+            lr=cfg.trainer.learning_rate,
+            weight_decay=cfg.trainer.weight_decay,
+        )
+        scheduler = instantiate(cfg.trainer.scheduler)(optimizer=optimizer, n_steps=cfg.trainer.n_steps)
     elif cfg.trainer.checkpoint.load.type == "nano":
+        if cfg.get("apply_functions", None):
+            for fn in instantiate(cfg.apply_functions):
+                fn(model)
+        model = setup_distributed_training(model, cfg.trainer.distributed)
+        optimizer = torch.optim.AdamW(
+            model.parameters(),
+            lr=cfg.trainer.learning_rate,
+            weight_decay=cfg.trainer.weight_decay,
+        )
+        scheduler = instantiate(cfg.trainer.scheduler)(optimizer=optimizer, n_steps=cfg.trainer.n_steps)
+        
+        load_checkpoint_from_file(cfg.trainer.checkpoint.load, model, optimizer, scheduler)
+        if cfg.trainer.checkpoint.load.only_weights:
+            optimizer = torch.optim.AdamW(
+                model.parameters(),
+                lr=cfg.trainer.learning_rate,
+                weight_decay=cfg.trainer.weight_decay,
+            )
+            scheduler = instantiate(cfg.trainer.scheduler)(optimizer=optimizer, n_steps=cfg.trainer.n_steps)
+    elif cfg.trainer.checkpoint.load.type == "nano_local":
+
         if cfg.get("apply_functions", None):
             for fn in instantiate(cfg.apply_functions):
                 fn(model)
