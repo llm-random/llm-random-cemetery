494d20e76d2fec7ac70c426cbd6ccbc31cd8c04e
Maciej Stefaniak <madragonse@gmail.com>
2025-09-06 15:28:41 +0200

    Versioning code

--- src/core/datasets.py
+++ src/core/datasets.py
@@ -42,6 +42,25 @@ def llama_tokenize_fn():
         return batch_encodings
     return tokenize_function
 
+
+# def worker_init_fn(worker_id): #devv
+#     worker_info = torch.utils.data.get_worker_info()
+#     dataset = worker_info.dataset
+
+#     rank = int(os.environ.get("RANK", 0))
+#     base_seed = dataset.seed if dataset.seed is not None else 0
+
+#     # dataset.world_size = int(os.environ.get("WORLD_SIZE"))
+#     # dataset.rank = int(os.environ.get("RANK"))
+
+#     logger.info(f"WORKER init fun base_seed {base_seed},\n rank {rank},\n worker_info.num_workers {worker_info.num_workers},\n worker_id {worker_id},\n dataset {vars(dataset)},\n")
+
+#     # Deterministic: global seed + rank offset + worker offset
+#     unique_seed = base_seed + rank * worker_info.num_workers + worker_id
+#     dataset.rng.seed(unique_seed) 
+#     dataset.world_size = int(os.environ.get("WORLD_SIZE")) * worker_info.num_workers
+#     dataset.rank = int(os.environ.get("RANK")) + worker_id
+
 class AbstractDataset(IterableDataset):
 
     def __init__(
@@ -66,6 +85,7 @@ class AbstractDataset(IterableDataset):
         self.use_new_sampling_method = use_new_sampling_method
         self.shuffle = shuffle
         self.world_size_independent = world_size_independent
+        self.packer = self.sample_packer()
 
     def sample_packer(self):
         buffer: List[int] = []
@@ -101,10 +121,16 @@ class AbstractDataset(IterableDataset):
                     buffer, document_lengths = [], []
 
     def __iter__(self):
+        worker_info = torch.utils.data.get_worker_info()
+        num_workers = worker_info.num_workers
+        worker_id = worker_info.id
+        self.packer = self.sample_packer()
         self.rng.seed(self.seed)
         if self.world_size_independent:
             return itertools.islice(
-                self.sample_packer(), self.rank, None, self.world_size
+                # self.sample_packer(), self.rank, None, self.world_size
+                self.packer, self.rank*num_workers+worker_id, None, self.world_size*num_workers
+                # self.packer, self.rank, None, self.world_size
             )
         else:
             return self.sample_packer()
@@ -150,25 +176,12 @@ class FineWebEduDataset(AbstractDataset):
             batched=True
         )
 
-    def _belongs_to_split(self, document_id: int) -> bool:
-        eval_percentage = 1
-
-        if self.split == "train":
-            return hash(document_id) % 100 >= eval_percentage
-        elif self.split == "validation":
-            return hash(document_id) % 100 < eval_percentage
-        else:
-            raise ValueError("split must be either 'train' or 'validation'")
-
-
     def get_infinite_sampler(self):
         epoch = 0
         while True:
             self.data_generator.set_epoch(epoch)
             for next_sample in self.data_generator:
-                if self._belongs_to_split(next_sample["id"]):
-                    yield next_sample
-
+                yield next_sample
             epoch += 1
 
 class C4Dataset(AbstractDataset):
