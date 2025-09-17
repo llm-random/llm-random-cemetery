b4911c7eea1c6ad2d9cd02820767377917cb1dbf
j321m <jan.malasnicki@gmail.com>
2025-09-17 19:02:47 +0200

    Versioning code

--- research/muP_MoE/entrypoints/entropy_h100.sh
+++ research/muP_MoE/entrypoints/entropy_h100.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-source ~/miniconda/etc/profile.d/conda.sh
+source ~/miniconda3/etc/profile.d/conda.sh
 echo "Running Entropy H100 Entrypoint"
 conda activate llm_random_main
 echo "Will run the following command:"
