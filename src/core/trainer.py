237f975efa28d919151e782d9f14fceb146a1b39
Maciej Stefaniak <madragonse@gmail.com>
2025-09-17 17:35:59 +0200

    Versioning code

--- src/core/trainer.py
+++ src/core/trainer.py
@@ -10,7 +10,6 @@ from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
 from src.projected_compression.compression import finalize_projection_weights
 from src.core.conversion_to_hf import save_to_llama_3_hf
-from old_datasets import LLMBatch
 import torch.distributed.checkpoint as dcp
 from torch.nn.parallel import DistributedDataParallel as DDP
 import logging
@@ -147,6 +146,7 @@ class Trainer:
             """we want to have no reference to model output while backpropagating to allow torch to free memory,
             so we wrap loss calculation in a function"""
             predicted_ids = self.model(input_ids)
+            raise Exception()
 
             # Tensors should be on the same device for loss calculation #TODO check
             target_ids = target_ids.to(predicted_ids.device)
