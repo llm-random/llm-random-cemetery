9c33ad51145077cb25256f55b1222575a44e9a6a
Maciej Stefaniak <madragonse@gmail.com>
2025-09-17 17:28:10 +0200

    Versioning code

--- src/core/trainer.py
+++ src/core/trainer.py
@@ -10,7 +10,6 @@ from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
 from src.projected_compression.compression import finalize_projection_weights
 from src.core.conversion_to_hf import save_to_llama_3_hf
-from old_datasets import LLMBatch
 import torch.distributed.checkpoint as dcp
 from torch.nn.parallel import DistributedDataParallel as DDP
 import logging
@@ -96,6 +95,7 @@ class Trainer:
             self.step = step
             self.metric_logger.set_step(step)
             self.model.train()
+            raise Exception()
             loss = self.calculate_loss(batch)
 
             grad_norm = self.clip_gradient()
