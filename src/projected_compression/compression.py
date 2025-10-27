a5af934063d4a11453805ac381a9607ed67c1862
Maciej Stefaniak <madragonse@gmail.com>
2025-10-27 15:44:54 +0100

    Versioning code

--- src/projected_compression/compression.py
+++ src/projected_compression/compression.py
@@ -100,6 +100,7 @@ def initialize_projection_weights(
     model.head.norm.normalized_shape = tuple(model.head.norm.weight.shape)
 
     for i, block in enumerate(model.encoder.blocks):
+        print(f"Initializing projections, block: {i} ---------------")
         layers_to_init_projections = [
             ("attention_layer.layer.q_proj", dmodel_top_indices, None),
             ("attention_layer.layer.k_proj", dmodel_top_indices, None),
