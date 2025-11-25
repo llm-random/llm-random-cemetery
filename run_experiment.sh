177a3d998e1b71666c995e522986224b2b669ccc
Maciej Pióro <m@ciekpio.ro>
2025-11-25 17:06:56 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=configs/context/2025.11.25_mha_short.yaml --git_branch=context_loss_2025-11-25_17-06-56 --skip_copy_code --custom_backends_module=research.attention_moe.backends
