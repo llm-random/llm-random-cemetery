37b29c6a560e01c31fa59fcf3c2d599cd4cb55fb
Maciej Pióro <m@ciekpio.ro>
2025-12-13 13:06:29 +0100

    Versioning code

---
+++ run_experiment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -m lizrd.grid --config_path=./configs/bdt_mp/2025.12.13_500m_lemtest.yaml --git_branch=diff_500m_vanilla_2025-12-13_13-06-29 --skip_copy_code --custom_backends_module=research.attention_moe.backends
