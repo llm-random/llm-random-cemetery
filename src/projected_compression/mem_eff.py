fd677a681828f09c868562d12080c21467637114
Maciej Stefaniak <madragonse@gmail.com>
2026-01-22 17:04:02 +0100

    Versioning code

--- src/projected_compression/mem_eff.py
+++ src/projected_compression/mem_eff.py
@@ -291,9 +291,10 @@ class MemoryEfficientProjectedCompression(nn.Module):
                             block_proj.parameters(), gradient_clipping, grad_norm_to_use
                         )
                     else:
-                        grad_norm = torch.nn.utils.clip_grad_norm_(
-                            block_proj.parameters(), gradient_clipping
-                        )
+                        pass #dev ###############################################################################################
+                        # grad_norm = torch.nn.utils.clip_grad_norm_(
+                        #     block_proj.parameters(), gradient_clipping
+                        # )
 
                 optimizer.step()
                 optimizer.zero_grad()
