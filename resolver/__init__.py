cb906b071ebd60b89a83425d6cc8802047fc0c49
Maciej Pióro <m@ciekpio.ro>
2026-02-03 16:34:21 +0100

    Versioning code

--- resolver/__init__.py
+++ resolver/__init__.py
@@ -22,9 +22,23 @@ def get_cluster_name(hostname=None, username=None) -> str:
 
     return "default"
 
+def get_common_env_variables_export():
+    variables = ["WANDB_API_KEY", "HF_TOKEN"]
+    res = []
+    for var in variables:
+        if var in os.environ:
+            res.append(f'export {var}="{os.environ[var]}"')
+        else:
+            print(f"Warning: {var} not found in environment variables. This might lead to issues.")
+            res.append(f'# export {var}=... <- potentially missing variable')
+    return '\n'.join(res)
+
+
 
 OmegaConf.register_new_resolver("__llmrandom_cluster_config", get_cluster_name)
 
 OmegaConf.register_new_resolver("random_seed", lambda: random.randint(0, 100000))
 
 OmegaConf.register_new_resolver("eval", eval)
+
+OmegaConf.register_new_resolver("export_env_variables", get_common_env_variables_export)
