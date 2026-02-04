085c53747ce65006220fe775690e111c67089687
ggwozdz2 <gwozdzgg@gmail.com>
2026-02-04 12:43:31 +0100

    Versioning code

---
+++ src/product_keys/finetuning_trainer.py
@@ -0,0 +1,13 @@
+from attr import define, field
+
+from src.core.trainer import Trainer
+
+@define(slots=False)
+class FineTuningTrainer(Trainer):
+
+    def __init__(self):
+        super().__init__()
+
+    
+
+
