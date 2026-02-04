705896e01251b4958f14b1acdbe5f44610f34651
Michał Krutul <crewtoolcompany@gmail.com>
2026-02-04 18:01:43 +0100

    Versioning code

--- run_exp.py
+++ run_exp.py
@@ -223,6 +223,8 @@ def submit_experiment(
 
             try:
                 connection.run(f"tmux new -d -s {experiment_branch_name}")
+                if "WANDB_API_KEY" in os.environ:
+                    connection.run(f"tmux set-environment -t {experiment_branch_name} WANDB_API_KEY {os.environ['WANDB_API_KEY']}")
                 connection.run(
                     f'tmux send -t {experiment_branch_name}.0 "cd {experiment_dir}" ENTER'
                 )
