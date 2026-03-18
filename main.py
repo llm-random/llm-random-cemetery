c4f050782600970dd393b510faae5b052f409ace
Janek <jan.malasnicki@gmail.com>
2026-03-18 10:56:30 +0100

    eval works on tiny local and logs to wandb

--- main.py
+++ main.py
@@ -346,7 +346,7 @@ def run(cfg: OmegaConf, metric_logger=None):
 
     evaluator = instantiate(cfg.evaluator)
     if evaluator is not None:
-        evaluator(metric_logger=metric_logger).eval()
+        evaluator(metric_logger=metric_logger, model=model).eval()
 
     cleanup()
 
