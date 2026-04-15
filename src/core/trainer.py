8901ac42a7373eaebfd2b975fb0ba7727f0d0fd9
Janek <jan.malasnicki@gmail.com>
2026-04-15 18:11:01 +0200

    Versioning code

--- src/core/trainer.py
+++ src/core/trainer.py
@@ -84,6 +84,7 @@ class Trainer:
             self._rewind_train_iterator(self.start_step)
 
         self.loss_averaged_100 = AveMetric(100, "100/train/loss")
+        self.total_loss_averaged_100 = AveMetric(100, "100/train/total_loss")
         self.time_diff_averaged_100 = AveDiffMetric(100, "100/time", time.time())
 
     def _rewind_train_iterator(self, n_batches: int):
